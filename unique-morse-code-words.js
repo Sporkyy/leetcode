0b1d9057fa152d32c9bc8c76cd57557e2835e67e
Todd Sayre <captain.friendly@gmail.com>
2019-02-18 09:40:59 -0500

    Unique Morse Code Words

---
+++ unique-morse-code-words.js
@@ -0,0 +1,74 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function(words) {
+  const codes = [
+    '.-',
+    '-...',
+    '-.-.',
+    '-..',
+    '.',
+    '..-.',
+    '--.',
+    '....',
+    '..',
+    '.---',
+    '-.-',
+    '.-..',
+    '--',
+    '-.',
+    '---',
+    '.--.',
+    '--.-',
+    '.-.',
+    '...',
+    '-',
+    '..-',
+    '...-',
+    '.--',
+    '-..-',
+    '-.--',
+    '--..'
+  ];
+  const toMorseCode = s =>
+    s
+      .split('')
+      .map(l => codes[l.charCodeAt(0) - 97])
+      .join('');
+  return new Set(words.map(word => toMorseCode(word))).size;
+};
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const tests = [
+  {
+    name: 'Example',
+    input: ['gin', 'zen', 'gig', 'msg'],
+    output: 2
+  },
+  {
+    name: 'Empty array',
+    input: [],
+    output: 0
+  },
+  {
+    name: 'One word',
+    input: ['xyzzy'],
+    output: 1
+  },
+  {
+    name: 'Words different lengths',
+    input: ['larry', 'moe', 'curly'],
+    output: 3
+  }
+];
+
+tests.forEach(test => {
+  const result = uniqueMorseRepresentations(test.input);
+  if (test.output === result) {
+    console.log(`${test.name}: okay`);
+  } else {
+    console.log(`${test.name}: got ${result}; expected ${test.output}`);
+  }
+});
