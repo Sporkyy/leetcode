d330c217210f93aeabae6627e151dcfc54f0e467
Todd Sayre <captain.friendly@gmail.com>
2019-02-18 09:07:57 -0500

    To Lower Case

---
+++ to-lower-case.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} str
+ * @return {string}
+ */
+const toLowerCase = str => str.toLowerCase();
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const tests = [
+  {
+    title: 'Example 1',
+    input: 'Hello',
+    output: 'hello'
+  },
+  {
+    title: 'Example 2',
+    input: 'here',
+    output: 'here'
+  },
+  {
+    title: 'Example 3',
+    input: 'LOVELY',
+    output: 'lovely'
+  },
+  {
+    title: 'Empty string',
+    input: '',
+    output: ''
+  },
+  {
+    title: 'Single lowercase',
+    input: 'a',
+    output: 'a'
+  },
+  {
+    title: 'Single Uppercase',
+    input: 'A',
+    output: 'a'
+  },
+  {
+    title: 'A digit',
+    input: '1',
+    output: '1'
+  }
+];
+
+tests.forEach(test => {
+  console.log(`${test.title}: ${test.output === toLowerCase(test.input)}`);
+});
