586455e4b1aefe338dc4db2da8c4332960641774
Todd Sayre <captain.friendly@gmail.com>
2019-03-26 10:36:10 -0400

    Fleshed out test suite

--- 589-n-ary-tree-preorder-traversal.js
+++ 589-n-ary-tree-preorder-traversal.js
@@ -3,9 +3,9 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const nestedArrayAnswer = root => !!!root ? [] : 0 === root.children.length ? [root.val] : [root.val, ...root.children.map(c => nestedArrayAnswer(c))];
+const preorderNested = root => !!!root ? [] : 0 === root.children.length ? [root.val] : [root.val, ...root.children.map(c => preorderNested(c))];
 
-const flat = a => a.reduce((acc, curr) => Array.isArray(curr) ? [...acc, ...flat(curr)] : [...acc, curr], [])
+const flat = a => a.reduce((acc, curr) => Array.isArray(curr) ? [...acc, ...flat(curr)] : [...acc, curr], []);
 
 /**
  * // Definition for a Node.
@@ -18,42 +18,67 @@ const flat = a => a.reduce((acc, curr) => Array.isArray(curr) ? [...acc, ...flat
  * @param {Node} root
  * @return {number[]}
  */
-const preorder = root => flat(nestedArrayAnswer(root));
+const preorder = root => flat(preorderNested(root));
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// const run = {
-//   "$id": "1",
-//   "children": [{
-//     "$id": "2",
-//     "children": [{
-//       "$id": "5",
-//       "children": [],
-//       "val": 5
-//     }, {
-//       "$id": "6",
-//       "children": [],
-//       "val": 6
-//     }],
-//     "val": 3
-//   }, {
-//     "$id": "3",
-//     "children": [],
-//     "val": 2
-//   }, {
-//     "$id": "4",
-//     "children": [],
-//     "val": 4
-//   }],
-//   "val": 1
-// }
+const tests = [{
+    name: 'Example 1',
+    input: {
+      "$id": "1",
+      "children": [{
+        "$id": "2",
+        "children": [{
+          "$id": "5",
+          "children": [],
+          "val": 5
+        }, {
+          "$id": "6",
+          "children": [],
+          "val": 6
+        }],
+        "val": 3
+      }, {
+        "$id": "3",
+        "children": [],
+        "val": 2
+      }, {
+        "$id": "4",
+        "children": [],
+        "val": 4
+      }],
+      "val": 1
+    },
+    expected: [1, 3, 5, 6, 2, 4]
+  },
+  {
+    name: 'No children',
+    input: {
+      "$id": "1",
+      "children": [],
+      "val": 44
+    },
+    expected: [44]
+  },
+  {
+    name: 'Null',
+    input: null,
+    expected: []
+  }
+];
 
-const run = {
-  "$id": "1",
-  "children": [],
-  "val": 44
-}
+const check = (a1, a2) => a1.reduce((acc, curr, i) => acc && curr === a2[i], true);
 
-// const run = null;
-
-console.log(preorder(run));
+tests.forEach(({
+  name,
+  input,
+  expected
+}) => {
+  const result = preorder(input);
+  if (check(result, expected)) {
+    console.log(`✅ ${name}`);
+  } else {
+    console.log(`🔴 ${name}`);
+    console.log(`Expected "${expected}", but got "${result}"`);
+  }
+});
