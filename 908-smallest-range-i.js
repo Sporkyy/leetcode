ddcd6f032d1f125bcea7bfc143a5d35554fba975
Todd Sayre <captain.friendly@gmail.com>
2019-03-29 15:01:21 -0400

    908. Smallest Range I (96%/96%)

---
+++ 908-smallest-range-i.js
@@ -0,0 +1,56 @@
+// 908. Smallest Range I
+// https://leetcode.com/problems/smallest-range-i/
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+const smallestRangeI = (A, K) => Math.max(0, Math.max(...A) - Math.min(...A) - K * 2);
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const tests = [{
+    name: 'Example 1',
+    input: {
+      A: [1],
+      K: 0
+    },
+    expected: 0
+  },
+  {
+    name: 'Example 2',
+    input: {
+      A: [0, 10],
+      K: 2
+    },
+    expected: 6
+  },
+  {
+    name: 'Example 3',
+    input: {
+      A: [1, 3, 6],
+      K: 3
+    },
+    expected: 0
+  }
+];
+
+tests.forEach(({
+  name,
+  input: {
+    A,
+    K
+  },
+  expected
+}) => {
+  const result = smallestRangeI(A, K);
+  if (expected === result) {
+    console.log(`✅ ${name}`);
+  } else {
+    console.log(`🔴 ${name}`);
+    console.log(`Expected "${expected}", but got "${result}"`);
+  }
+});
