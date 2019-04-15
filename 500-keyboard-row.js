cbdc2a449877d0e140eb10bcc044c9c943a2390e
Todd Sayre <captain.friendly@gmail.com>
2019-04-15 09:41:13 -0400

    500. Keyboard Row (53%/6%)

---
+++ 500-keyboard-row.js
@@ -0,0 +1,92 @@
+// 500. Keyboard Row
+// https://leetcode.com/problems/keyboard-row/
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+const findWords = words => {
+  const keyboard = [
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
+  ];
+  const oneLineWords = [];
+  for (let i = 0; i < words.length; i++) {
+    let didMatchRow1 = false;
+    let didMatchRow2 = false;
+    let didMatchRow3 = false;
+    for (let j = 0; j < words[i].length; j++) {
+      for (let k = 0; k < keyboard.length; k++) {
+        for (let l = 0; l < keyboard[k].length; l++) {
+          if (words[i][j].toLowerCase() === keyboard[k][l]) {
+            switch (k) {
+              case 0:
+                didMatchRow1 = true;
+                break;
+              case 1:
+                didMatchRow2 = true;
+                break;
+              case 2:
+                didMatchRow3 = true;
+                break;
+            }
+          }
+          if (
+            (didMatchRow1 && didMatchRow2) ||
+            (didMatchRow2 && didMatchRow3) ||
+            (didMatchRow1 && didMatchRow3)
+          ) {
+            break;
+          }
+        }
+        if (
+          (didMatchRow1 && didMatchRow2) ||
+          (didMatchRow2 && didMatchRow3) ||
+          (didMatchRow1 && didMatchRow3)
+        ) {
+          break;
+        }
+      }
+      if (
+        (didMatchRow1 && didMatchRow2) ||
+        (didMatchRow2 && didMatchRow3) ||
+        (didMatchRow1 && didMatchRow3)
+      ) {
+        break;
+      }
+    }
+    if (
+      (didMatchRow1 && !didMatchRow2 && !didMatchRow3) ||
+      (!didMatchRow1 && didMatchRow2 && !didMatchRow3) ||
+      (!didMatchRow1 && !didMatchRow2 && didMatchRow3)
+    ) {
+      oneLineWords.push(words[i]);
+    }
+  }
+  return oneLineWords;
+};
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const tests = [
+  {
+    name: 'Example',
+    input: ['Hello', 'Alaska', 'Dad', 'Peace'],
+    expected: ['Alaska', 'Dad'],
+  },
+];
+
+const areArraysEqual = (a1, a2) => a1.length === a2.length && a1.join() === a2.join();
+
+tests.forEach(({ name, input, expected }) => {
+  const result = findWords(input);
+  if (areArraysEqual(expected, result)) {
+    console.log(`✅ ${name}`);
+  } else {
+    console.log(`🔴 ${name}`);
+    console.log(`Expected "${expected}", but got "${result}"`);
+  }
+});
