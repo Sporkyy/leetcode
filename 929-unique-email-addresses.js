88ae02241fdaf7effd6927a46912b7a363e749d9
Todd Sayre <captain.friendly@gmail.com>
2019-04-23 14:46:34 -0400

    20%/93%

--- 929-unique-email-addresses.js
+++ 929-unique-email-addresses.js
@@ -3,104 +3,182 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+// Runtime: 88 ms, faster than 78.30% of JavaScript online submissions for Unique Email Addresses.
+// Memory Usage: 42.5 MB, less than 30.46% of JavaScript online submissions for Unique Email Addresses.
+
 /**
  * @param {string[]} emails
  * @return {number}
  */
-const numUniqueEmails = emails =>
-  new Set(
-    emails.map(email => {
-      const [localName, domainName] = email.split('@');
-      const forwardName = localName.replace(/\./g, '').split('+')[0];
-      return `${forwardName}@${domainName}`;
-    })
-  ).size;
+// const numUniqueEmails = emails =>
+//   new Set(
+//     emails.map(email => {
+//       const [localName, domainName] = email.split('@');
+//       const forwardName = localName.replace(/\./g, '').split('+')[0];
+//       return `${forwardName}@${domainName}`;
+//     }),
+//   ).size;
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const testCases = [
+// Runtime: 88 ms, faster than 78.30% of JavaScript online submissions for Unique Email Addresses.
+// Memory Usage: 42.4 MB, less than 32.34% of JavaScript online submissions for Unique Email Addresses.
+
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+// const numUniqueEmails = emails => {
+//   if (emails.length < 2) return emails.length;
+//   let result = 0;
+//   let seenAddresses = '';
+//   for (let i = emails.length - 1; 0 <= i; --i) {
+//     const [localName, domainName] = emails[i].split('@');
+//     const forwardName = localName.split('+')[0].replace(/\./g, '');
+//     const includableAddress = ` ${domainName}${forwardName} `;
+//     if (!seenAddresses.includes(includableAddress)) {
+//       result++;
+//       seenAddresses = `${seenAddresses}${includableAddress}`;
+//     }
+//   }
+//   return result;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 84 ms, faster than 87.68% of JavaScript online submissions for Unique Email Addresses.
+// Memory Usage: 42 MB, less than 39.85% of JavaScript online submissions for Unique Email Addresses.
+
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+// function numUniqueEmails(emails) {
+//   const cleanEmails = new Array(emails.length);
+//   for (let i = emails.length - 1; 0 <= i; --i) {
+//     const [localName, domainName] = emails[i].split('@');
+//     const forwardName = localName.split('+')[0].replace(/\./g, '');
+//     cleanEmails[i] = `${forwardName}@${domainName}`;
+//   }
+//   // console.log(emails);
+//   // console.log(cleanEmails);
+//   return new Set(cleanEmails).size;
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 116 ms, faster than 20.74% of JavaScript online submissions for Unique Email Addresses.
+// Memory Usage: 39 MB, less than 93.23% of JavaScript online submissions for Unique Email Addresses.
+
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+function numUniqueEmails(emails) {
+  const cleanEmails = new Set();
+  for (let i = emails.length - 1; 0 <= i; --i) {
+    const matches = emails[i].match(/^([^\+]+)(?:\+.+)?(@.+)$/);
+    cleanEmails.add(`${matches[1].replace(/\./g, '')}@${matches[2]}`);
+  }
+  return cleanEmails.size;
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const tests = [
   {
+    name: 'Example 1',
     input: [
       'test.email+alex@leetcode.com',
       'test.e.mail+bob.cathy@leetcode.com',
-      'testemail+david@lee.tcode.com'
+      'testemail+david@lee.tcode.com',
     ],
-    output: 2
+    expected: 2,
   },
   {
+    name: 'One address',
     input: ['test@foo.com'],
-    output: 1
+    expected: 1,
   },
   {
+    name: 'All the same domain with plus signs',
     input: [
       'test@foo.com',
       'test+bar@foo.com',
       'test+bar+baz@foo.com',
-      'test+bar+baz+quux@foo.com'
+      'test+bar+baz+quux@foo.com',
     ],
-    output: 1
+    expected: 1,
   },
   {
+    name: 'All the same domain with dots',
     input: [
       'test@foo.com',
       'test.bar@foo.com',
       'test.bar.baz@foo.com',
-      'test.bar.baz.quux@foo.com'
+      'test.bar.baz.quux@foo.com',
     ],
-    output: 4
+    expected: 4,
   },
   {
+    name: 'All the same domain with dots and pluses',
     input: [
       'test@foo.com',
       'test+bar@foo.com',
       'test.bar@foo.com',
       'test+bar+baz@foo.com',
-      'test.bar.baz@foo.com'
+      'test.bar.baz@foo.com',
     ],
-    output: 3
+    expected: 3,
   },
   {
+    name: 'All the same local name with different domains',
     input: ['test@foo.com', 'test@bar.com', 'test@baz.com', 'test@quux.com'],
-    output: 4
+    expected: 4,
   },
   {
+    name: 'Domains with dots',
     input: [
       'test@foo.com',
       'test@foo.bar.com',
       'test@foo.bar.baz.com',
-      'test@foo.bar.baz.quux.com'
+      'test@foo.bar.baz.quux.com',
     ],
-    output: 4
+    expected: 4,
   },
   {
+    name: 'Domains with pluses',
     input: [
       'test@foo.com',
       'test@foo+bar.com',
       'test@foo+bar+baz.com',
-      'test@foo+bar+baz+quux.com'
+      'test@foo+bar+baz+quux.com',
     ],
-    output: 4
+    expected: 4,
   },
   {
+    name: 'Domains with pluses and dots',
     input: ['test@foo.com', 'test@foo.bar.com', 'test@foo+bar.com', 'test@foo+bar+baz.com'],
-    output: 4
+    expected: 4,
   },
   {
+    name: 'All on same domain',
     input: ['test@foo.com', 'test@foo.com', 'test@foo.com'],
-    output: 1
+    expected: 1,
   },
   {
+    name: 'No addresses',
     input: [],
-    output: 0
-  }
+    expected: 0,
+  },
 ];
 
-testCases.forEach(testCase => {
-  console.log(testCase);
-  const returnedValue = numUniqueEmails(testCase.input);
-  if (returnedValue === testCase.output) {
-    console.log('✅');
+tests.forEach(({ name, input, expected }) => {
+  const output = numUniqueEmails(input);
+  if (output === expected) {
+    console.log(`✅ ${name}`);
   } else {
-    console.log(`❗Returned ${returnedValue} where ${testCase.output} expected ❗`);
+    console.log(`🔴 ${name}`);
+    console.log(`Expected ${expected} but got ${output}`);
   }
 });
