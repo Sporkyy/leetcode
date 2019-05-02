a3f2a9ede949c927f726b4fc70687ac62776ae7c
Todd Sayre <captain.friendly@gmail.com>
2019-05-02 10:09:01 -0400

    14. Longest Common Prefix (80%/50%)

---
+++ 14-longest-common-prefix.js
@@ -0,0 +1,76 @@
+// 14. Longest Common Prefix
+// https://leetcode.com/problems/longest-common-prefix/
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 64 ms, faster than 80.12% of JavaScript online submissions for Longest Common Prefix.
+// Memory Usage: 35 MB, less than 50.54% of JavaScript online submissions for Longest Common Prefix.
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+const longestCommonPrefix = strs => {
+  let prefix = '';
+
+  const charAtInStrs = n => strs.map(s => s.slice(n, n + 1));
+
+  const isArrayHomogeneous = a =>
+    a.reduce((acc, curr, i) => acc && (curr === a[i + 1] || 'undefined' === typeof a[i + 1]), true);
+
+  const isArrayFull = a => a.length === a.join('').length;
+
+  let slicePoint = 0;
+
+  while (true) {
+    const chars = charAtInStrs(slicePoint);
+    if (0 < chars.length && isArrayFull(chars) && isArrayHomogeneous(chars)) {
+      prefix += chars[0];
+      slicePoint++;
+    } else {
+      break;
+    }
+  }
+
+  return prefix;
+};
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const tests = [
+  {
+    name: 'Example 1',
+    input: ['flower', 'flow', 'flight'],
+    expected: 'fl',
+  },
+  {
+    name: 'Example 2',
+    input: ['dog', 'racecar', 'car'],
+    expected: '',
+  },
+  {
+    name: 'Many repeats',
+    input: ['wwwwwww', 'wwwwww', 'wwwww', 'wwww', 'www'],
+    expected: 'www',
+  },
+  {
+    name: 'Empty array',
+    input: [],
+    expected: '',
+  },
+  {
+    name: 'abca, abc',
+    input: ['abca', 'abc'],
+    expected: 'abc',
+  },
+];
+
+tests.forEach(({ name, input, expected }) => {
+  const output = longestCommonPrefix(input);
+  if (expected === output) {
+    console.log(`✅ ${name}`);
+  } else {
+    console.log(`🔴 ${name}`);
+    console.log(`Expected "${expected}", but got "${output}"`);
+  }
+});
