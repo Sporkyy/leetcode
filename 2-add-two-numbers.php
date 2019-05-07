933371835b90429bddfb7c8c2e4dd34f620fb348
Todd Sayre <captain.friendly@gmail.com>
2019-05-07 10:09:49 -0400

    2. Add Two Numbers (94%/18%)

--- 2-add-two-numbers.php
+++ 2-add-two-numbers.php
@@ -2,6 +2,9 @@
 /**
  * PHP version 7.2
  *
+ * 2. Add Two Numbers
+ * https://leetcode.com/problems/add-two-numbers/submissions/
+ *
  * You are given two non-empty linked lists representing two non-negative integers.
  * The digits are stored in reverse order and each of their nodes contain
  * a single digit. Add the two numbers and return it as a linked list.
@@ -26,6 +29,10 @@
  *     function __construct($val) { $this->val = $val; }
  * }
  *
+ * Runtime: 28 ms, faster than 94.80% of PHP online submissions for Add Two Numbers.
+ * Memory Usage: 14.9 MB, less than 18.60% of PHP online submissions
+ * for Add Two Numbers.
+ *
  * @category Professional_Development
  * @package  Leetcode_Problems
  * @author   Todd Sayre <captain.friendly+doccomment@gmail.com>
@@ -45,55 +52,32 @@ class Solution
      */
     public function addTwoNumbers($l1, $l2)
     {
-        // print('<pre>');
-        // print_r($l1);
-        // print_r($l2);
-        // print('</pre>');
-
-        $answerHead = new ListNode(0);
+        $dummyHead = new ListNode(0);
+        $resultCurr = $dummyHead;
+        $l1Curr = $l1;
+        $l2Curr = $l2;
         $carry = 0;
-        $p = $l1;
-        $q = $l2;
 
-        while (null !== $p || null !== $q) {
-            $x = $p->val ?? 0;
-            $y = $q->val ?? 0;
-            $sum = $x + $y + $carry;
+        while (null !== $l1Curr || null !== $l2Curr) {
+            $l1Val = $l1Curr->val ?? 0;
+            $l2Val = $l2Curr->val ?? 0;
+            $sum = $l1Val + $l2Val + $carry;
+            $resultCurr = $resultCurr->next = new ListNode($sum % 10);
             $carry = intdiv($sum, 10);
-            // print_r("$carry<br>");
-            $answerHead->next = new ListNode($sum % 10);
 
-            print('<pre>');
-            print_r($answerHead);
-            print('</pre>');
-
-            $answerHead = $answerHead->next;
-
-            // print('<pre>');
-            // print_r($answerHead);
-            // print('</pre>');
-
-            if (null !== $p) {
-                $p = $p->next;
+            if (null !== $l1Curr) {
+                $l1Curr = $l1Curr->next;
             }
-            if (null !== $q) {
-                $q = $q->next;
+            if (null !== $l2Curr) {
+                $l2Curr = $l2Curr->next;
             }
         }
-
-        // print('<pre>');
-        // print_r($answerHead);
-        // print('</pre>');
 
         if (0 < $carry) {
-            $answerHead->next = new ListNode($carry);
+            $resultCurr->next = new ListNode($carry);
         }
 
-        // print('<pre>');
-        // print_r($answerHead);
-        // print('</pre>');
-
-        return $answerHead->next;
+        return $dummyHead->next;
     }
 }
 
@@ -124,52 +108,68 @@ class ListNode
     }
 }
 
-
 /**
  * Construct the node
  *
+ * @param n $n The number from which to construct the nodes
+ *
  * @return ListNode the first ListNode
  */
-function createNodeList()
+function createNodeList($n)
 {
-    $args = func_get_args();
-    $listNodes = [];
+    $listNodes = array_reverse(str_split($n));
 
-    for ($i = 0; $i < count($args); $i++) {
-        $listNodes[$i] = new ListNode($args[$i]);
+    for ($i = 0; $i < count($listNodes); $i++) {
+        $listNodes[$i] = new ListNode($listNodes[$i]);
     }
-
-    // print('<pre>');
-    // print_r($listNodes);
-    // print('</pre>');
 
     for ($i = 0; $i < count($listNodes) - 1; $i++) {
         $listNodes[$i]->next = $listNodes[$i + 1];
     }
 
-    // print('<pre>');
-    // print_r($listNodes);
-    // print('</pre>');
-
-    // print('<pre>');
-    // print_r($listNodes[0]);
-    // print('</pre>');
-
     return $listNodes[0];
+}
+
+/**
+ * Turn a node list into an integer
+ *
+ * @param head $head The head node
+ *
+ * @return number The numerical representation of the node list
+ */
+function serializeNodeList($head)
+{
+    $curr = $head;
+    $s = '';
+    while (null !== $curr) {
+        $s = $curr->val.$s;
+        $curr = $curr->next;
+    }
+    return intval($s);
 }
 
 $tests = [
     [
-        'name' => '(2 -> 4 -> 3) + (5 -> 6 -> 4)',
-        'input' => [createNodeList(2, 4, 3), createNodeList(5, 6, 4)],
-        'expected' => createNodeList(7, 0, 8),
-        // Explanation: 342 + 465 = 807.
+        'name' => '342 + 465 = 807',
+        'input' => [createNodeList(342), createNodeList(465)],
+        'expected' => createNodeList(807),
     ]
 ];
 
 $solution = new Solution();
 
 foreach ($tests as $test) {
-    // echo $test['name'];
-    $output = $solution->addTwoNumbers($test['input'][0], $test['input'][1]);
+    $expected = serializeNodeList($test['expected']);
+    $output = serializeNodeList(
+        $solution->addTwoNumbers($test['input'][0], $test['input'][1])
+    );
+    if ($expected === $output) {
+        print("✅ {$test['name']}<br>");
+    } else {
+        print("🔴 {$test['name']}<br>");
+        print('Expected the below<br>');
+        print_r($expected);
+        print('But got the below instead<br>');
+        print_r($output);
+    }
 }
