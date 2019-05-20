8b7ce94a00f9332ebe2df160796f85be0ab65d5a
Todd Sayre <captain.friendly@gmail.com>
2019-05-20 15:01:17 -0400

    With and without a priority queue

---
+++ 1046-last-stone-weight.php
@@ -0,0 +1,189 @@
+<?php
+
+// 1046. Last Stone Weight
+// https://leetcode.com/problems/last-stone-weight/
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 16 ms, faster than 50.00% of PHP online submissions
+// for Last Stone Weight.
+// Memory Usage: 14.8 MB, less than 100.00% of PHP online submissions
+// for Last Stone Weight.
+
+// class Solution
+// {
+
+//     /**
+//      * @param Integer[] $stones
+//      * @return Integer
+//      */
+//     function lastStoneWeight($stones)
+//     {
+//         $count = count($stones);
+//         while (1 < $count) {
+//             sort($stones, SORT_NUMERIC);
+//             $stones[--$count - 1] = array_pop($stones) - $stones[$count - 1];
+//         }
+//         return max($stones);
+//     }
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 8 ms, faster than 50.00% of PHP online submissions
+// for Last Stone Weight.
+// Memory Usage: 14.8 MB, less than 100.00% of PHP online submissions
+// for Last Stone Weight.
+
+// class Solution
+// {
+
+//     /**
+//      * @param Integer[] $stones
+//      * @return Integer
+//      */
+//     function lastStoneWeight($stones)
+//     {
+//         while (1 < count($stones)) {
+//             rsort($stones, SORT_NUMERIC);
+//             if ($stones[0] === $stones[1]) {
+//                 $stones = array_slice($stones, 2, count($stones));
+//             } else if ($stones[0] < $stones[1]) {
+//                 $stones[0] = $stones[1] - array_shift($stones);
+//             } else {
+//                 $stones[0] = array_shift($stones) - $stones[0];
+//             }
+//         }
+//         return $stones[0] ?? 0;
+//     }
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 8 ms, faster than 50.00% of PHP online submissions
+// for Last Stone Weight.
+// Memory Usage: 15 MB, less than 100.00% of PHP online submissions
+// for Last Stone Weight.
+
+// class Solution
+// {
+
+//     /**
+//      * @param Integer[] $stones
+//      * @return Integer
+//      */
+//     function lastStoneWeight($stones)
+//     {
+//         while (1 < count($stones)) {
+//             rsort($stones, SORT_NUMERIC);
+//             $stones[0] = $stones[0] - $stones[1];
+//             unset($stones[1]);
+//         }
+//         return max($stones);
+//     }
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 4 ms, faster than 75.00% of PHP online submissions
+// for Last Stone Weight.
+// Memory Usage: 14.6 MB, less than 100.00% of PHP online submissions
+// for Last Stone Weight.
+
+// class Solution
+// {
+
+//     /**
+//      * @param Integer[] $stones
+//      * @return Integer
+//      */
+//     function lastStoneWeight($stones)
+//     {
+//         while (1 < count($stones)) {
+//             rsort($stones, SORT_NUMERIC);
+//             $stones[0] = -$stones[1] + array_shift($stones);
+//         }
+//         return max($stones);
+//     }
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 4 ms, faster than 75.00% of PHP online submissions
+// for Last Stone Weight.
+// Memory Usage: 14.8 MB, less than 100.00% of PHP online submissions
+// for Last Stone Weight.
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $stones
+     * @return Integer
+     */
+    function lastStoneWeight($stones)
+    {
+        $pq = new SplPriorityQueue();
+        $pq->setExtractFlags(SplPriorityQueue::EXTR_DATA);
+        foreach ($stones as $stone) $pq->insert($stone, $stone);
+        while (1 < $pq->count()) {
+            $outcome = $pq->extract() - $pq->extract();
+            if (0 === $outcome) continue;
+            $pq->insert($outcome, $outcome);
+        }
+        return $pq->current() ?? 0;
+    }
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+$tests = [
+    [
+        'name' => '2, 7, 4, 1, 8, 1',
+        'input' => [2, 7, 4, 1, 8, 1],
+        'expected' => 1,
+        // Explanation:
+        // We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+        // we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+        // we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+        // we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+    ],
+    [
+        'name' => '1',
+        'input' => [1],
+        'expected' => 1,
+    ],
+    [
+        'name' => '1, 1',
+        'input' => [1, 1],
+        'expected' => 0,
+    ],
+    [
+        'name' => '1, 2',
+        'input' => [1, 2],
+        'expected' => 1,
+    ],
+    [
+        'name' => '1, 3',
+        'input' => [1, 3],
+        'expected' => 2,
+    ],
+];
+
+$s = new Solution();
+
+foreach ($tests as $test) {
+    $name = $test['name'];
+    $input = $test['input'];
+    $expected = $test['expected'];
+    $output = $s->lastStoneWeight($input);
+
+    if ($expected === $output) {
+        echo '✅ ', $name, '<br>';
+    } else {
+        echo '🔴 ', $name, '<br>';
+        echo 'Expected "', $expected, '", but got "', $output, '"', '<br>';
+    }
+
+    echo '<hr>';
+}
