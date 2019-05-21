0d6348147eb1ff5c5f846f200098671d77921779
Todd Sayre <captain.friendly@gmail.com>
2019-05-21 09:51:20 -0400

    No loops

--- 1037-valid-boomerang.php
+++ 1037-valid-boomerang.php
@@ -53,6 +53,56 @@
 // Memory Usage: 14.8 MB, less than 100.00% of PHP online submissions
 // for Valid Boomerang.
 
+// class Solution
+// {
+
+//     /**
+//      * @param Integer[][] $points
+//      * @return Boolean
+//      */
+//     function isBoomerang($points)
+//     {
+//         // If all points are in a line vertially
+//         if (
+//             $points[0][0] === $points[1][0]
+//             && $points[1][0] === $points[2][0]
+//             && $points[2][0] === $points[0][0]
+//         ) return false;
+//         // If all points are in a line horizontally
+//         if (
+//             $points[0][1] === $points[1][1]
+//             && $points[1][1] === $points[2][1]
+//             && $points[2][1] === $points[0][1]
+//         ) return false;
+//         // If any of the points are the same
+//         if (
+//             ($points[0][0] === $points[1][0]
+//                 && $points[0][1] === $points[1][1])
+//             || ($points[1][0] === $points[2][0]
+//                 && $points[1][1] === $points[2][1])
+//             || ($points[2][0] === $points[0][0]
+//                 && $points[2][1] === $points[0][1])
+//         ) return false;
+//         $isDiagonal = true;
+//         for ($i = 0; $i < count($points) - 1; $i++) {
+//             $colDistance = $points[$i][0] - $points[$i + 1][0];
+//             $rowDistance = $points[$i][1] - $points[$i + 1][1];
+//             $isDiagonal = $isDiagonal && $colDistance === $rowDistance;
+//         }
+//         // If the points are in a line diagonally
+//         if ($isDiagonal) return false;
+//         // If all the tests are passed, assume it is valid
+//         return true;
+//     }
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 8 ms, faster than 100.00% of PHP online submissions
+// for Valid Boomerang.
+// Memory Usage: 14.8 MB, less than 100.00% of PHP online submissions
+// for Valid Boomerang.
+
 class Solution
 {
 
@@ -83,14 +133,12 @@ class Solution
             || ($points[2][0] === $points[0][0]
                 && $points[2][1] === $points[0][1])
         ) return false;
-        $isDiagonal = true;
-        for ($i = 0; $i < count($points) - 1; $i++) {
-            $colDistance = $points[$i][0] - $points[$i + 1][0];
-            $rowDistance = $points[$i][1] - $points[$i + 1][1];
-            $isDiagonal = $isDiagonal && $colDistance === $rowDistance;
-        }
         // If the points are in a line diagonally
-        if ($isDiagonal) return false;
+        if (
+            ($points[0][0] - $points[1][0] === $points[0][1] - $points[1][1])
+            && ($points[1][0] - $points[2][0] === $points[1][1] - $points[2][1])
+            && ($points[2][0] - $points[0][0] === $points[2][1] - $points[0][1])
+        ) return false;
         // If all the tests are passed, assume it is valid
         return true;
     }
