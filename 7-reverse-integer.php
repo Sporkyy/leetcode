f3d62d7c456178b4103e5bdc4889e4ef5defc36e
Todd Sayre <captain.friendly@gmail.com>
2019-06-07 09:38:08 -0400

    3-line solution

--- 7-reverse-integer.php
+++ 7-reverse-integer.php
@@ -10,13 +10,38 @@
 // Memory Usage: 14.9 MB, less than 20.64% of PHP online submissions
 // for Reverse Integer.
 
+// class Solution
+// {
+
+//     function is32BitInteger($x)
+//     {
+//         return pow(-2, 31) <= $x && $x <= pow(2, 31) - 1;
+//     }
+
+//     /**
+//      * @param Integer $x
+//      * @return Integer
+//      */
+//     function reverse($x)
+//     {
+//         if (!$this->is32BitInteger($x)) {
+//             return 0;
+//         } else {
+//             $rx = ($x <=> 0) * strrev(abs($x));
+//             return $this->is32BitInteger($rx) ? $rx : 0;
+//         }
+//     }
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 8 ms, faster than 90.25% of PHP online submissions
+// for Reverse Integer.
+// Memory Usage: 14.9 MB, less than 36.61% of PHP online submissions
+// for Reverse Integer.
+
 class Solution
 {
-
-    function is32BitInteger($x)
-    {
-        return pow(-2, 31) <= $x && $x <= pow(2, 31) - 1;
-    }
 
     /**
      * @param Integer $x
@@ -24,16 +49,17 @@ class Solution
      */
     function reverse($x)
     {
-        if (!$this->is32BitInteger($x)) {
-            return 0;
-        } else {
-            $rx = ($x <=> 0) * strrev(abs($x));
-            return $this->is32BitInteger($rx) ? $rx : 0;
-        }
+        if ($x < -2147483648 || 2147483647 < $x) return 0;
+        $rx = ($x <=> 0) * strrev(abs($x));
+        return $rx < -2147483648 || 2147483647 < $rx ? 0 : $rx;
     }
 }
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+echo '32-bit min = ', pow(-2, 31), '<br>';
+echo '32-bit max = ', pow(2, 31) - 1, '<br>';
+echo '<hr>';
 
 $tests = [
     [
