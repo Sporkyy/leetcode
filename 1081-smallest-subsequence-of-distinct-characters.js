2e46dfb391c8b1a6a2a2e882f7b76fda8d13009c
Todd Sayre <captain.friendly@gmail.com>
2019-06-13 16:48:38 -0400

    Still working on it

--- 1081-smallest-subsequence-of-distinct-characters.js
+++ 1081-smallest-subsequence-of-distinct-characters.js
@@ -7,46 +7,220 @@
  * @param {string} text
  * @return {string}
  */
-const smallestSubsequence = text => {
-  const uniques = Array.from(new Set(text.split('')));
-  console.log(uniques);
-  const weights = text.split('').map((c, i) => [
-    i,
-    c,
-    uniques
-      .filter(x => x !== c)
-      .map(x => {
-        console.log(text.substring(i, text.length));
-        return text.substring(i, text.length).indexOf(x);
-      })
-      .reduce((a, c) => a + c, 0),
-  ]);
-  console.log(weights);
-  const answer = uniques.map(u =>
-    weights.filter(([, c]) => c === u).sort(([, , a], [, , b]) => a - b),
-  );
-  console.log(answer);
-};
+// const smallestSubsequence = text => {
+//   // Find all the unique chars in the text
+//   const dedupe = s => Array.from(new Set(s.split(''))).join('');
+//   const uniques = dedupe(text).split('');
+//   // console.log(uniques);
+
+//   // Find the last index of each char in the array
+//   const lastIndexOfEach = (s, a) => a.map(c => s.lastIndexOf(c));
+
+//   // Stop at the point where not every char appears in the string
+//   const stopAt = Math.min(...lastIndexOfEach(text, uniques));
+//   // console.log(stopAt);
+
+//   const truncatedText = text.substring(0, stopAt);
+//   // console.log(truncatedText);
+
+//   // Find the last index of each char in the array
+//   const indexOfEach = (s, a) => a.map(c => s.indexOf(c));
+
+//   const distances = truncatedText.split('').map((c, i) => indexOfEach(text.substring(i), uniques));
+//   // console.log(distances);
+
+//   const longestDistances = distances.map(d => Math.max(...d));
+//   // console.log(longestDistances);
+
+//   const shortestDistance = Math.min(...longestDistances);
+
+//   const startAt = longestDistances.indexOf(shortestDistance);
+
+//   const answer = dedupe(text.substring(startAt));
+//   // console.log(answer);
+
+//   return answer;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/**
+ * @param {string} text
+ * @return {string}
+ */
+// const smallestSubsequence = text => {
+//   const uniques = Array.from(new Set(text.split('')));
+// console.log(uniques);
+// const withLastIndexOf = uniques.map(c => [c, text.lastIndexOf(c)]);
+// console.log(withLastIndexOf);
+// const lastIndexOfEach = (s, a) => a.map(c => s.lastIndexOf(c));
+// const withIndices = text.split('').map(u => [u, lastIndexOfEach(text, uniques)]);
+// console.log(withIndices);
+// const indicesOf = (s, c) => s.split('').reduce((a, d, i) => (c === d ? [...a, i] : a), []);
+// const withIndices = text.split('').map((c, i) => [c, indicesOf(text.substring(i), c)]);
+// console.log(withIndices);
+// let temp = text.split('').map((c, i) => text.substring(i));
+// console.log(temp);
+// const count = (s, c) => ;
+// const hasOnlyOneOf = (s, a) => {s };
+// temp = temp.filter()
+// for (let i = text.length; 0 < i; i--) {
+//   const test = text.substring(0, i);
+//   console.log(test);
+//   const chars = new Array(26).fill(0);
+//   for (let j = 0; j < test.length; j++) chars[test[j].charCodeAt(0) - 97]++;
+//   console.log(chars);
+// }
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/**
+ * @param {string} text
+ * @return {string}
+ */
+// const smallestSubsequence = text => {
+//   let cnt = new Array(26);
+//   let nibble = 'l';
+//   let test = text;
+//   let indexOfOne = -1;
+//   let lastIndexOfOne = -1;
+//   while (lastIndexOfOne === indexOfOne || indexOfOne * lastIndexOfOne < 0) {
+//     console.log(test);
+//     cnt.fill(0);
+//     for (let j = 0; j < test.length; j++) cnt[test[j].charCodeAt(0) - 97]++;
+//     // console.log(count);
+//     indexOfOne = cnt.indexOf(1);
+//     // console.log(indexOfOne);
+//     lastIndexOfOne = cnt.lastIndexOf(1);
+//     // console.log(lastIndexOfOne);
+//     nibble = 'l' === nibble ? 'r' : 'l';
+//     test = 'l' === nibble ? text.substring(1) : text.substring(0, test.length - 1);
+//   }
+//   console.log(test);
+//   const answerLength = cnt.join('').replace(/0/g, '').length;
+//   console.log(answerLength);
+//   let answer = '';
+//   for (let i = 0; i < test.length; i++) {
+//     console.log(test[i]);
+//     // if (-1 === answer.indexOf(test[i])) answer += test[i];
+//     // if (i === answerLength - 1) break;
+//   }
+//   return answer;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/**
+ * @param {string} text
+ * @return {string}
+ */
+// const smallestSubsequence = text => {
+//   const stack = [text];
+//   while (0 < stack.length) {
+//     const test = stack.pop();
+//     console.log(test);
+//     if (2 < test.length) stack.push(test.substring(1));
+//   }
+//   stack.push(text);
+//   while (0 < stack.length) {
+//     const test = stack.pop();
+//     console.log(test);
+//     if (2 < test.length) stack.push(test.substring(0, test.length - 1));
+//   }
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/**
+ * @param {string} text
+ * @return {string}
+ */
+// const smallestSubsequence = text => {
+//   const stack = [text];
+//   const possiblities = [];
+//   const countChars = s => {
+//     let counts = new Array(26).fill(0);
+//     for (let j = 0; j < s.length; j++) counts[s[j].charCodeAt(0) - 97]++;
+//     return counts;
+//   };
+//   const answerLength = countChars(text).filter(c => 0 < c).length;
+//   console.log(answerLength);
+//   while (0 < stack.length) {
+//     const test = stack.pop();
+//     console.log(test);
+//     const charCounts = countChars(test);
+//     const hasEveryChar = answerLength === charCounts.filter(c => 0 < c).length;
+//     if (hasEveryChar) possiblities.push(test);
+//     if (answerLength < test.length) stack.push(test.substring(1));
+//   }
+//   stack.push(text);
+//   while (0 < stack.length) {
+//     const test = stack.pop();
+//     console.log(test);
+//     const charCounts = countChars(test);
+//     const hasEveryChar = answerLength === charCounts.filter(c => 0 < c).length;
+//     if (hasEveryChar) possiblities.push(test);
+//     if (answerLength < test.length) stack.push(test.substring(0, test.length - 1));
+//   }
+//   console.log(possiblities.sort((a, b) => b.length - a.length));
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/**
+ * @param {string} text
+ * @return {string}
+ */
+// const smallestSubsequence = text => {
+//   const dedupeA2A = a => Array.from(new Set(a));
+//   const dedupeA2S = a => dedupeA2A(a).join('');
+//   const dedupeS2S = s => dedupeA2S(s.split(''));
+//   const uniques = dedupeS2S(text);
+//   const unsub = (s, b, e = s.length) => dedupeS2S(s.substring(b, e));
+//   const max = text.length - uniques.length;
+//   let begin = 0;
+//   let end = text.length;
+//   const compareInAnyOrder = (a, b) => {
+//     const c = new Array(26).fill(0);
+//     const d = new Array(26).fill(0);
+//     for (let i = 0; i < a.length; i++) c[a[i].charCodeAt(0) - 97]++;
+//     for (let i = 0; i < b.length; i++) d[b[i].charCodeAt(0) - 97]++;
+//     return c.join('') === d.join('');
+//   };
+//   // console.log(compareInAnyOrder('abc', 'cba'));
+//   while (compareInAnyOrder(uniques, unsub(text, begin))) begin++;
+//   console.log(begin);
+//   // while (compareInAnyOrder(uniques, revSubS(text, end--))) end++;
+//   // console.log(end);
+//   // console.log(text.substring(begin, end));
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/**
+ * @param {string} text
+ * @return {string}
+ */
+const smallestSubsequence = text => {};
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 const tests = [
-  /*
-   *          |---|---|---|---|---|---|---|---|
-   *      i = | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
-   *          |---|---|---|---|---|---|---|---|
-   *  input = | c | d | a | d | a | b | c | c |
-   *          |---|---|---|---|---|---|---|---|
-   *      a = |   |   |~a~|   | a |   |   |   |
-   *      b = |   |   |   |   |   |~b~|   |   |
-   *      c = | c |   |   |   |   |   |~c~| c |
-   *      d = |   | d |   |~d~|   |   |   |   |
-   *          |---|---|---|---|---|---|---|---|
-   * answer = | - | - | a | d | - | b | c | - |
-   *          |---|---|---|---|---|---|---|---|
-   *      i = | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
-   *          |---|---|---|---|---|---|---|---|
-   */
+  //          |---|---|---|---|---|---|---|---|
+  //      i = | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
+  //          |---|---|---|---|---|---|---|---|
+  //  input = | c | d | a | d | a | b | c | c |
+  //          |---|---|---|---|---|---|---|---|
+  //      a = |   |   |~a~|   | a |   |   |   |
+  //      b = |   |   |   |   |   |~b~|   |   |
+  //      c = | c |   |   |   |   |   |~c~| c |
+  //      d = |   | d |   |~d~|   |   |   |   |
+  //          |---|---|---|---|---|---|---|---|
+  // answer = |   |   | a | d |   | b |   | c |
+  //          |---|---|---|---|---|---|---|---|
+  //      i = | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |
+  //          |---|---|---|---|---|---|---|---|
   {
     input: 'cdadabcc',
     expected: 'adbc',
@@ -55,6 +229,20 @@ const tests = [
   //   input: 'abcd',
   //   expected: 'abcd',
   // },
+  //          |---|---|---|---|---|---|---|
+  //      i = | 0 | 1 | 2 | 3 | 4 | 5 | 6 |
+  //          |---|---|---|---|---|---|---|
+  //  input = | e | c | b | a | c | b | a |
+  //          |---|---|---|---|---|---|---|
+  //      a = |   |   |   |~a~|   |   | a |
+  //      b = |   |   | b |   |   |~b~|   |
+  //      c = |   | c |   |   |~c~|   |   |
+  //      e = |~e~|   |   |   |   |   |   |
+  //          |---|---|---|---|---|---|---|
+  // answer = | e |   |   | a | c | b |   |
+  //          |---|---|---|---|---|---|---|
+  //      i = | 0 | 1 | 2 | 3 | 4 | 5 | 6 |
+  //          |---|---|---|---|---|---|---|
   // {
   //   input: 'ecbacba',
   //   expected: 'eacb',
