6bba6a7a1d99103256231604c6a225b3ed0ed5f7
Todd Sayre <captain.friendly@gmail.com>
2019-07-10 08:29:43 -0400

    More short circuiting

--- 1005-maximize-sum-of-array-after-k-negations.js
+++ 1005-maximize-sum-of-array-after-k-negations.js
@@ -13,14 +13,197 @@
  * @param {number} K
  * @return {number}
  */
+// const largestSumAfterKNegations = (A, K) => {
+//   for (let i = 0; i < K; i++) {
+//     const mi = A.reduce((acc, curr, i) => (curr < A[acc] ? i : acc), 0);
+//     const mv = A[mi];
+//     if (0 === mv) break;
+//     A[mi] = -mv;
+//   }
+//   return A.reduce((acc, curr) => acc + curr);
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 68 ms, faster than 66.50% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+// Memory Usage: 35.5 MB, less than 39.43% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+// const largestSumAfterKNegations = (A, K) => {
+//   A.sort((a, b) => a - b);
+//   for (let i = 0; i < A.length && 0 < K; i++) if (A[i] < 0) (A[i] = -A[i]) && K--;
+//   if (0 < K && !A.includes(0) && 1 === K % 2) {
+//     const mi = A.reduce((acc, curr, i) => (curr < A[acc] ? i : acc), 0);
+//     A[mi] = -A[mi];
+//   }
+//   return A.reduce((acc, curr) => acc + curr);
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 96 ms, faster than 22.50% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+// Memory Usage: 34.8 MB, less than 98.59% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+// const largestSumAfterKNegations = (A, K) => {
+//   const getMindex = () => A.reduce((a, c, i) => (c < A[a] ? i : a), 0);
+//   let mindex = getMindex();
+//   while (0 !== A[mindex] && 0 < K) {
+//     if (A[mindex] < 0) {
+//       A[mindex] = -A[mindex];
+//       mindex = getMindex();
+//       K--;
+//     } else {
+//       if (1 === K % 2) A[mindex] = -A[mindex];
+//       K = 0;
+//     }
+//   }
+//   return A.reduce((acc, curr) => acc + curr);
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 64 ms, faster than 84.50% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+// Memory Usage: 36.8 MB, less than 26.76% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+// const largestSumAfterKNegations = (A, K) => {
+//   const pos = [];
+//   const neg = [];
+//   for (let i = 0; i < A.length; i++)
+//     if (A[i] < 0) neg.push(A[i]);
+//     else pos.push(A[i]);
+//   neg.sort((a, b) => b - a);
+//   while (neg.length && 0 < K) pos.push(-neg.pop()) && K--;
+//   const combo = [...neg, ...pos];
+//   let sum = combo.reduce((acc, curr) => acc + curr);
+//   if (0 === K % 2 || pos.includes(0)) return sum;
+//   return sum - Math.min(...combo) * 2;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 116 ms, faster than 16.50% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+// Memory Usage: 36.7 MB, less than 26.76% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+// const largestSumAfterKNegations = (A, K) => {
+//   while (K--) {
+//     const mindex = A.reduce((a, c, i) => (c < A[a] ? i : a), 0);
+//     A[mindex] = -A[mindex];
+//   }
+//   return A.reduce((acc, curr) => acc + curr);
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 84 ms, faster than 37.00% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+// Memory Usage: 34.8 MB, less than 98.59% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+// const largestSumAfterKNegations = (A, K) => {
+//   while (K--) {
+//     const mindex = A.reduce((a, c, i) => (c < A[a] ? i : a), 0);
+//     const minVal = A[mindex];
+//     if (0 === minVal) break;
+//     if (0 < minVal && 1 === K % 2) break;
+//     A[mindex] = -minVal;
+//     if (0 < minVal && 0 === K % 2) break;
+//   }
+//   return A.reduce((acc, curr) => acc + curr);
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 64 ms, faster than 84.50% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+// Memory Usage: 35.9 MB, less than 32.39% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+// const largestSumAfterKNegations = (A, K) => {
+//   A.sort((a, b) => a - b);
+//   let sum = 0;
+//   for (let i = 0; i < A.length; i++) {
+//     let val = A[i];
+//     if (0 < K) {
+//       if (val < 0) {
+//         let next = A[i + 1];
+//         if (next && 0 < next && Math.abs(val) < next && 0 === K % 2) {
+//           K = 0;
+//         } else {
+//           [val, K] = [-val, K - 1];
+//         }
+//       } else if (0 === val) K = 0;
+//       else if (0 < val && 1 === K % 2) [val, K] = [-val, 0];
+//     }
+//     sum += val;
+//   }
+//   return sum;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 52 ms, faster than 99.50% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+// Memory Usage: 35.9 MB, less than 33.80% of JavaScript online submissions
+// for Maximize Sum Of Array After K Negations.
+
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
 const largestSumAfterKNegations = (A, K) => {
-  for (let i = 0; i < K; i++) {
-    const mi = A.reduce((acc, curr, i) => (curr < A[acc] ? i : acc), 0);
-    const mv = A[mi];
-    if (0 === mv) break;
-    A[mi] = -mv;
+  A.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < A.length; i++) {
+    let val = A[i];
+    if (0 < K) {
+      if (val < 0) {
+        let next = A[i + 1];
+        if (Math.abs(val) < next && 0 === K % 2) K = 0;
+        else [val, K] = [-val, K - 1];
+      } else if (0 !== val && 1 === K % 2) [val, K] = [-val, 0];
+      else K = 0;
+    }
+    sum += val;
   }
-  return A.reduce((acc, curr) => acc + curr);
+  return sum;
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -52,6 +235,38 @@ const tests = [
     expected: 13,
   },
   // Explanation: Choose indices (1, 4) and A becomes [2,3,-1,5,4].
+
+  {
+    input: {
+      A: [-8, 3, -5, -3, -5, -2],
+      K: 6,
+    },
+    expected: 22,
+  },
+
+  {
+    input: {
+      A: [8, 3, 5, 3, 5, 2],
+      K: 6,
+    },
+    expected: 26,
+  },
+
+  {
+    input: {
+      A: [3, 5, 3, 5, 2],
+      K: 5,
+    },
+    expected: 14,
+  },
+
+  {
+    input: {
+      A: [-2, 5, 0, 2, -2],
+      K: 3,
+    },
+    expected: 11,
+  },
 ];
 
 tests.forEach(({ input: { A, K }, expected }) => {
--- 1005-maximize-sum-of-array-after-k-negations.php
+++ 1005-maximize-sum-of-array-after-k-negations.php
@@ -10,6 +10,72 @@
 // Memory Usage: 14.9 MB, less than 100.00% of PHP online submissions
 // for Maximize Sum Of Array After K Negations.
 
+// class Solution
+// {
+
+//     /**
+//      * @param Integer[] $A
+//      * @param Integer $K
+//      * @return Integer
+//      */
+//     function largestSumAfterKNegations($A, $K)
+//     {
+//         for ($i = 0; $i < $K; $i++) {
+//             $mi = 0;
+//             foreach ($A as $key => $value) {
+//                 if ($value < $A[$mi]) $mi = $key;
+//             }
+//             $mv = $A[$mi];
+//             if (0 === $mv) break;
+//             $A[$mi] = -$mv;
+//         }
+//         return array_reduce($A, function ($acc, $curr) {
+//             return $acc + $curr;
+//         });
+//     }
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 88 ms, faster than 33.33% of PHP online submissions
+// for Maximize Sum Of Array After K Negations.
+// Memory Usage: 14.9 MB, less than 100.00% of PHP online submissions
+// for Maximize Sum Of Array After K Negations.
+
+// class Solution
+// {
+
+//     /**
+//      * @param Integer[] $A
+//      * @param Integer $K
+//      * @return Integer
+//      */
+//     function largestSumAfterKNegations($A, $K)
+//     {
+//         while ($K--) {
+//             $mindex = 0;
+//             foreach ($A as $key => $value) {
+//                 if ($value < $A[$mindex]) $mindex = $key;
+//             }
+//             $minVal = $A[$mindex];
+//             if (0 === $minVal) break;
+//             if (0 < $minVal && 1 === $K % 2) break;
+//             $A[$mindex] = -$minVal;
+//             if (0 < $minVal && 0 === $K % 2) break;
+//         }
+//         return array_reduce($A, function ($acc, $curr) {
+//             return $acc + $curr;
+//         });
+//     }
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 16 ms, faster than 66.67% of PHP online submissions
+// for Maximize Sum Of Array After K Negations.
+// Memory Usage: 14.9 MB, less than 100.00% of PHP online submissions
+// for Maximize Sum Of Array After K Negations.
+
 class Solution
 {
 
@@ -20,18 +86,21 @@ class Solution
      */
     function largestSumAfterKNegations($A, $K)
     {
-        for ($i = 0; $i < $K; $i++) {
-            $mi = 0;
-            foreach ($A as $key => $value) {
-                if ($value < $A[$mi]) $mi = $key;
+        sort($A, SORT_NUMERIC);
+        $sum = 0;
+        for ($i = 0; $i < count($A); $i++) {
+            $val = $A[$i];
+            if (0 < $K) {
+                if ($val < 0) {
+                    $next = $A[$i + 1];
+                    if (abs($val) < $next && 0 === $K % 2) $K = 0;
+                    else list($val, $K) = [-$val, $K - 1];
+                } else if (0 !== val && 1 === $K % 2) list($val, $K) = [-$val, 0];
+                else $K = 0;
             }
-            $mv = $A[$mi];
-            if (0 === $mv) break;
-            $A[$mi] = -$mv;
+            $sum += $val;
         }
-        return array_reduce($A, function ($acc, $curr) {
-            return $acc + $curr;
-        });
+        return $sum;
     }
 }
 
@@ -64,11 +133,42 @@ $tests = [
         'expected' => 13,
     ],
     // Explanation: Choose indices (1, 4) and A becomes [2,3,-1,5,4].
+
+    [
+        'input' => [
+            'A' => [-8, 3, -5, -3, -5, -2],
+            'K' => 6,
+        ],
+        'expected' => 22,
+    ],
+
+    [
+        'input' => [
+            'A' => [8, 3, 5, 3, 5, 2],
+            'K' => 6,
+        ],
+        'expected' => 26,
+    ],
+
+    [
+        'input' => [
+            'A' => [3, 5, 3, 5, 2],
+            'K' => 5,
+        ],
+        'expected' => 14,
+    ],
+
+    [
+        'input' => [
+            'A' => [-2, 5, 0, 2, -2],
+            'K' => 3,
+        ],
+        'expected' => 11,
+    ],
 ];
 
 $s = new Solution();
 
-// tests.forEach(({ input: { A, K }, expected }) => {
 foreach ($tests as $test) {
     extract($test);
     extract($input);
