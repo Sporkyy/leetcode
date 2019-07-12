9fd7a51222b136f3d9d39ffc53f39c30969eba17
Todd Sayre <captain.friendly@gmail.com>
2019-07-12 10:53:08 -0400

    Working math solution

--- 836-rectangle-overlap.js
+++ 836-rectangle-overlap.js
@@ -10,39 +10,32 @@
  * @param {number[]} rec2
  * @return {boolean}
  */
-// var isRectangleOverlap = function([r1x1, r1y1, r1x2, r1y2], [r2x1, r2y1, r2x2, r2y2]) {
-//   // console.log([r1x1, r1y1, r1x2, r1y2], [r2x1, r2y1, r2x2, r2y2]);
-//   const xMin = Math.min(r1x1, r1x2, r2x1, r2x2);
-//   // console.log(xMin);
+// var isRectangleOverlap = function([aX1, aY1, aX2, aY2], [bX1, bY1, bX2, bY2]) {
+//   if (aX2 < aX1 && aY2 < aY1) [aX1, aY1, aX2, aY2] = [aX2, aY2, aX1, aY1];
+//   if (bX2 < bX1 && bY2 < bY1) [bX1, bY1, bX2, bY2] = [bX2, bY2, bX1, bY1];
+//   const xMin = Math.min(aX1, aX2, bX1, bX2);
 //   if (xMin < 0)
-//     [r1x1, r1x2, r2x1, r2x2] = [
-//       r1x1 + Math.abs(xMin),
-//       r1x2 + Math.abs(xMin),
-//       r2x1 + Math.abs(xMin),
-//       r2x2 + Math.abs(xMin),
+//     [aX1, aX2, bX1, bX2] = [
+//       aX1 + Math.abs(xMin),
+//       aX2 + Math.abs(xMin),
+//       bX1 + Math.abs(xMin),
+//       bX2 + Math.abs(xMin),
 //     ];
-//   const yMin = Math.min(r1y1, r1y2, r2y1, r2y2);
-//   // console.log(yMin);
+//   const yMin = Math.min(aY1, aY2, bY1, bY2);
 //   if (yMin < 0)
-//     [r1y1, r1y2, r2y1, r2y2] = [
-//       r1y1 + Math.abs(yMin),
-//       r1y2 + Math.abs(yMin),
-//       r2y1 + Math.abs(yMin),
-//       r2y2 + Math.abs(yMin),
+//     [aY1, aY2, bY1, bY2] = [
+//       aY1 + Math.abs(yMin),
+//       aY2 + Math.abs(yMin),
+//       bY1 + Math.abs(yMin),
+//       bY2 + Math.abs(yMin),
 //     ];
-//   if (r1x2 < r1x1 && r1y2 < r1y1) [r1x1, r1y1, r1x2, r1y2] = [r1x2, r1y2, r1x1, r1y1];
-//   if (r2x2 < r2x1 && r2y2 < r2y1) [r2x1, r2y1, r2x2, r2y2] = [r2x2, r2y2, r2x1, r2y1];
-//   // console.log([r1x1, r1y1, r1x2, r1y2], [r2x1, r2y1, r2x2, r2y2]);
-//   const xMax = Math.max(r1x1, r1x2, r2x1, r2x2);
-//   // console.log(xMax);
-//   const yMax = Math.max(r1y1, r1y2, r2y1, r2y2);
-//   // console.log(yMax);
+//   const xMax = Math.max(aX1, aX2, bX1, bX2);
+//   const yMax = Math.max(aY1, aY2, bY1, bY2);
 //   const canvas = new Array(yMax + 1).fill(0).map(n => new Array(xMax + 1).fill(0));
-//   // for (let row of canvas) console.log(row);
 //   for (let i = 0; i < canvas.length; i++) {
 //     for (let j = 0; j < canvas[i].length; j++) {
-//       if (r1y1 <= i && i <= r1y2 && r1x1 <= j && j <= r1x2) canvas[i][j] += 1;
-//       if (r2y1 <= i && i <= r2y2 && r2x1 <= j && j <= r2x2) canvas[i][j] += 2;
+//       if (aY1 <= i && i <= aY2 && aX1 <= j && j <= aX2) canvas[i][j] += 1;
+//       if (bY1 <= i && i <= bY2 && bX1 <= j && j <= bX2) canvas[i][j] += 2;
 //     }
 //   }
 //   for (let row of canvas) console.log(row);
@@ -51,9 +44,7 @@
 //       if (3 !== canvas[i][j]) continue;
 //       if (canvas.length - 2 < i) continue;
 //       if (canvas[0].length - 2 < j) continue;
-//       // console.log(i, j);
 //       const [r, dr, d] = [canvas[i][j + 1], canvas[i + 1][j + 1], canvas[i + 1][j]];
-//       // console.log(r, dr, d);
 //       if (3 === r && 3 === dr && 3 === d) return true;
 //     }
 //   }
@@ -62,25 +53,36 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+// Runtime: 60 ms, faster than 21.17% of JavaScript online submissions
+// for Rectangle Overlap.
+// Memory Usage: 33.8 MB, less than 39.29% of JavaScript online submissions
+// for Rectangle Overlap.
+
 /**
  * @param {number[]} rec1
  * @param {number[]} rec2
  * @return {boolean}
  */
-const isRectangleOverlap = ([ax1, ay1, ax2, ay2], [bx1, by1, bx2, by2]) => {
-  // console.log([ax1, ay1, ax2, ay2], [bx1, by1, bx2, by2]);
-  // if (ax2 < ax1 && ay2 < ay1) [ax1, ay1, ax2, ay2] = [ax2, ay2, ax1, ay1];
-  // if (bx2 < bx1 && by2 < by1) [bx1, by1, bx2, by2] = [bx2, by2, bx1, by1];
-  // console.log([ax1, ay1, ax2, ay2], [bx1, by1, bx2, by2]);
-  if (bx1 < ax2 && by1 < ay2) return true;
-  if (ax1 < bx2 && ay2 < by2) return true;
-  return false;
+const isRectangleOverlap = ([aX1, aY1, aX2, aY2], [bX1, bY1, bX2, bY2]) => {
+  let [aT, aR, aB, aL, bT, bR, bB, bL] = [
+    Math.min(aY1, aY2),
+    Math.max(aX1, aX2),
+    Math.max(aY1, aY2),
+    Math.min(aX1, aX2),
+    Math.min(bY1, bY2),
+    Math.max(bX1, bX2),
+    Math.max(bY1, bY2),
+    Math.min(bX1, bX2),
+  ];
+  if ((bT <= aT && bR <= aR) || (bL <= aL && bB <= aB))
+    [aT, aR, aB, aL, bT, bR, bB, bL] = [bT, bR, bB, bL, aT, aR, aB, aL];
+  return !(aR <= bL || aB <= bT);
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 const tests = [
-  //   | 0 | 1 | 2 | 3 |
+  //     0   1   2   3
   //   +===============+
   // 0 | 1 | 1 | 1 | 0 |
   // 1 | 1 | 3 | 3 | 2 |
@@ -96,12 +98,12 @@ const tests = [
     expected: true,
   },
 
-  //    | -1 | 0 | 1 | 2 |
+  //     -1   0   1   2
   //    +================+
-  // -1 |  1 | 1 | 1 | 0 |
-  //  0 |  1 | 3 | 3 | 2 |
-  //  1 |  1 | 3 | 3 | 2 |
-  //  2 |  0 | 2 | 2 | 2 |
+  // -1 | 1 | 1 | 1 | 0 |
+  //  0 | 1 | 3 | 3 | 2 |
+  //  1 | 1 | 3 | 3 | 2 |
+  //  2 | 0 | 2 | 2 | 2 |
   //    +================+
 
   {
@@ -112,7 +114,7 @@ const tests = [
     expected: true,
   },
 
-  //   | 0 | 1 | 2 |
+  //     0   1   2
   //   +===========+
   // 0 | 1 | 3 | 2 |
   // 1 | 1 | 3 | 2 |
@@ -126,20 +128,20 @@ const tests = [
     expected: false,
   },
 
-  //    | -4 | -3 | -2 | -1 |
-  //    +===================+
-  // -5 |  2 |  2 |  2 |  0 |
-  // -4 |  2 |  2 |  2 |  0 |
-  // -3 |  2 |  2 |  2 |  0 |
-  // -2 |  2 |  2 |  2 |  0 |
-  // -1 |  0 |  0 |  0 |  0 |
-  //  0 |  0 |  0 |  0 |  0 |
-  //  1 |  0 |  0 |  0 |  0 |
-  //  2 |  0 |  0 |  0 |  0 |
-  //  3 |  1 |  1 |  1 |  1 |
-  //  4 |  1 |  1 |  1 |  1 |
-  //  5 |  1 |  1 |  1 |  1 |​​
-  //    +===================+
+  //     -4  -3   2  -1
+  //    +===============+
+  // -5 | 1 | 1 | 1 | 0 |
+  // -4 | 1 | 1 | 1 | 0 |
+  // -3 | 1 | 1 | 1 | 0 |
+  // -2 | 1 | 1 | 1 | 0 |
+  // -1 | 0 | 0 | 0 | 0 |
+  //  0 | 0 | 0 | 0 | 0 |
+  //  1 | 0 | 0 | 0 | 0 |
+  //  2 | 0 | 0 | 0 | 0 |
+  //  3 | 2 | 2 | 2 | 2 |
+  //  4 | 2 | 2 | 2 | 2 |
+  //  5 | 2 | 2 | 2 | 2 |​​
+  //    +===============+
 
   {
     input: {
@@ -149,21 +151,110 @@ const tests = [
     expected: false,
   },
 
-  // {
-  //   input: {
-  //     rec1: [-9, 6, -3, 10],
-  //     rec2: [-8, -10, -5, -4],
-  //   },
-  //   expected: false,
-  // },
+  //     0   1   2   3
+  //   +===============+
+  // 0 | 1 | 1 | 0 | 0 |
+  // 1 | 1 | 1 | 0 | 0 |
+  // 2 | 0 | 0 | 2 | 2 |
+  // 3 | 0 | 0 | 2 | 2 |
+  //   +===============+
 
-  // {
-  //   input: {
-  //     rec1: [-687153884, -854669644, -368882013, -788694078],
-  //     rec2: [540420176, -909203694, 655002739, -577226067],
-  //   },
-  //   expected: false,
-  // },
+  {
+    input: {
+      rec1: [0, 0, 1, 1],
+      rec2: [2, 2, 3, 3],
+    },
+    expected: false,
+  },
+
+  //    -5  -4  -3  -2  -1   0   1   2   3   4   5
+  //   +===========================================+
+  // 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
+  // 1 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
+  // 2 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
+  // 3 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
+  // 4 | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 |
+  // 5 | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 |
+  // 6 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
+  // 7 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
+  // 8 | 2 | 2 | 2 | 2 | 2 | 2 | 0 | 0 | 0 | 0 | 0 |
+  //   +===========================================+
+
+  {
+    input: {
+      rec1: [-5, 8, 0, 8],
+      rec2: [-5, 4, 5, 5],
+    },
+    expected: false,
+  },
+
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 3, 3, 3, 1 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 3, 3, 3, 1 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 3, 3, 3, 1 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 3, 3, 3, 1 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 3, 3, 3, 1 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 3, 3, 3, 1 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 3, 3, 3, 1 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 3, 3, 3, 1 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0 ]
+
+  {
+    input: {
+      rec1: [7, 8, 13, 15],
+      rec2: [10, 8, 12, 20],
+    },
+    expected: true,
+  },
+
+  // [ 0, 1, 1, 1, 1, 0, 0 ]
+  // [ 0, 1, 1, 1, 1, 0, 0 ]
+  // [ 0, 1, 1, 1, 1, 0, 0 ]
+  // [ 0, 1, 1, 1, 1, 0, 0 ]
+  // [ 0, 1, 1, 1, 1, 0, 0 ]
+  // [ 0, 1, 1, 1, 1, 0, 0 ]
+  // [ 0, 1, 1, 1, 1, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 0, 0, 0, 0, 0, 0, 0 ]
+  // [ 2, 2, 2, 2, 2, 2, 2 ]
+  // [ 2, 2, 2, 2, 2, 2, 2 ]
+  // [ 2, 2, 2, 2, 2, 2, 2 ]
+  // [ 2, 2, 2, 2, 2, 2, 2 ]
+  // [ 2, 2, 2, 2, 2, 2, 2 ]
+
+  {
+    input: {
+      rec1: [-9, 6, -3, 10],
+      rec2: [-8, -10, -5, -4],
+    },
+    expected: false,
+  },
+
+  {
+    input: {
+      rec1: [-687153884, -854669644, -368882013, -788694078],
+      rec2: [540420176, -909203694, 655002739, -577226067],
+    },
+    expected: false, // Maybe
+  },
 ];
 
 for (let {
