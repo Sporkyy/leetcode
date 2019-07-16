e3259d3ee88f6873fe3dfe30f5504bcbfbc0a1e6
Todd Sayre <captain.friendly@gmail.com>
2019-07-16 16:07:59 -0400

    It runs

--- 840-magic-squares-in-grid.js
+++ 840-magic-squares-in-grid.js
@@ -34,30 +34,83 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+// Runtime: 112 ms, faster than 6.76% of JavaScript online submissions
+// for Magic Squares In Grid.
+// Memory Usage: 39.1 MB, less than 5.88% of JavaScript online submissions
+// for Magic Squares In Grid.
+
 const sum = a => a.reduce((acc, curr) => acc + curr);
-// const getRowRemainder = (grid, r, c) => {};
-// const getColRemainder = (grid, c, r) => {};
-// const getDiagonalRemainder = (grid, d, s) => {};
-// const getMax = grid => sum(grid[0]) - Math.min(...grid[0]);
-// const getMin = grid => sum(grid[0]) - Math.max(...grid[0]);
-// const getPermutation = (a, p) => {};
-// const isMagic = grid => {};
+const getRow = (g, r) => g[r];
+const getCol = (g, c) => g.map(r => r[c]);
+const getDia = (g, d) => g.reduce((a, c) => [...a, c[1 === d ? 2 - a.length : a.length]], []);
+const isMagic = g => {
+  const target = sum(getRow(g, 1));
+  for (let i = 1; i < 3; i++) if (target !== sum(getRow(g, i))) return false;
+  for (let i = 0; i < 3; i++) if (target !== sum(getCol(g, i))) return false;
+  for (let i = 0; i < 2; i++) if (target !== sum(getDia(g, i))) return false;
+  return true;
+};
+const isValid = g => {
+  const uniques = new Set();
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      const v = g[i][j];
+      if (v < 1 || 9 < v) return false;
+      uniques.add(v);
+    }
+  }
+  return 9 === uniques.size;
+};
+
+// +---+---+---+
+// | 5 | 1 | 9 |
+// | 7 | 6 | 2 |
+// | 3 | 8 | 4 |
+// +---+---+---+
+
+// console.log(getRow([[4, 3, 8], [9, 5, 1], [2, 7, 6]], 0));
+// console.log(getCol([[4, 3, 8], [9, 5, 1], [2, 7, 6]], 0));
+// console.log(getDia([[4, 3, 8], [9, 5, 1], [2, 7, 6]], 0));
+// console.log(getDia([[4, 3, 8], [9, 5, 1], [2, 7, 6]], 1));
+// console.log(isMagic([[4, 3, 8], [9, 5, 1], [2, 7, 6]]));
+
+// console.log(getRow([[3, 8, 4], [5, 1, 9], [7, 6, 2]], 0));
+// console.log(getCol([[3, 8, 4], [5, 1, 9], [7, 6, 2]], 0));
+// console.log(getDia([[3, 8, 4], [5, 1, 9], [7, 6, 2]], 0));
+// console.log(getDia([[3, 8, 4], [5, 1, 9], [7, 6, 2]], 1));
+// console.log(isUnique([[3, 8, 4], [5, 1, 9], [7, 6, 2]]));
+// console.log(isUnique([[3, 8, 4], [5, 1, 9], [7, 6, 3]]));
+// console.log(isMagic([[3, 8, 4], [5, 1, 9], [7, 6, 2]]));
 
 /**
  * @param {number[][]} grid
  * @return {number}
  */
 const numMagicSquaresInside = grid => {
-  const sums = new Set();
-  for (let i = 0; i < 4; i++) sums.add(sum(grid[0].filter((n, j) => i !== j)));
-  console.log(sums);
-  let possibilities = new Array(12);
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 4; j++) {
-      const possibility = [[i, 0 + j], [i, 1 + j]];
+  let [cols, rows] = [grid[0].length, grid.length];
+  if (cols < 3 || rows < 3) return 0;
+  let result = 0;
+  for (let i = 0, z = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++, z++) {
+      const [a0, b0, c0, a1, b1, c1, a2, b2, c2] = [
+        grid[(i + 1) % rows][(j + 1) % cols],
+        grid[(i + 1) % rows][(j + 2) % cols],
+        grid[(i + 1) % rows][(j + 3) % cols],
+        grid[(i + 2) % rows][(j + 1) % cols],
+        grid[(i + 2) % rows][(j + 2) % cols],
+        grid[(i + 2) % rows][(j + 3) % cols],
+        grid[(i + 3) % rows][(j + 1) % cols],
+        grid[(i + 3) % rows][(j + 2) % cols],
+        grid[(i + 3) % rows][(j + 3) % cols],
+      ];
+      // console.log([[a0, b0, c0], [a1, b1, c1], [a2, b2, c2]]);
+      // console.log(isMagic([[a0, b0, c0], [a1, b1, c1], [a2, b2, c2]]));
+      const g = [[a0, b0, c0], [a1, b1, c1], [a2, b2, c2]];
+      if (!isValid(g)) continue;
+      if (isMagic(g)) result++;
     }
   }
-  console.log(possibilities);
+  return result;
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -86,6 +139,26 @@ const tests = [
   {
     input: [[4, 3, 8, 4], [9, 5, 1, 9], [2, 7, 6, 2]],
     expected: 1,
+  },
+
+  {
+    input: [[8]],
+    expected: 0,
+  },
+
+  {
+    input: [[5, 5, 5], [5, 5, 5], [5, 5, 5]],
+    expected: 0,
+  },
+
+  {
+    input: [[10, 3, 5], [1, 6, 11], [7, 9, 2]],
+    expected: 0,
+  },
+
+  {
+    input: [[7, 0, 5], [2, 4, 6], [3, 8, 1]],
+    expected: 0,
   },
 ];
 
