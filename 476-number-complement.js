e0cb8325371b6dcd74b08f6f6d571c63f20b89f8
Todd Sayre <captain.friendly@gmail.com>
2019-07-22 11:58:30 -0400

    Added one-line solution

--- 476-number-complement.js
+++ 476-number-complement.js
@@ -3,68 +3,91 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const toBinary = n => {
-  let b = '';
-  do {
-    b = (n % 2) + b;
-    n = parseInt(n / 2, 10);
-  } while (0 < n);
-  return b;
-};
+// Runtime: 60 ms, faster than 50.94% of JavaScript online submissions
+// for Number Complement.
+// Memory Usage: 34 MB, less than 17.02% of JavaScript online submissions
+// for Number Complement.
 
-const flipBits = b =>
-  b
-    .split('')
-    .map(d => ('0' === d ? '1' : '0'))
-    .join('');
+// const toBinary = n => {
+//   let b = '';
+//   do {
+//     b = (n % 2) + b;
+//     n = parseInt(n / 2, 10);
+//   } while (0 < n);
+//   return b;
+// };
 
-const toDecimal = n =>
-  n
-    .split('')
-    .reverse()
-    .reduce((acc, curr, i) => (acc += '1' === curr ? Math.pow(2, i) : 0), 0);
+// const flipBits = b =>
+//   b
+//     .split('')
+//     .map(d => ('0' === d ? '1' : '0'))
+//     .join('');
+
+// const toDecimal = n =>
+//   n
+//     .split('')
+//     .reverse()
+//     .reduce((acc, curr, i) => (acc += '1' === curr ? Math.pow(2, i) : 0), 0);
+
+// /**
+//  * @param {number} num
+//  * @return {number}
+//  */
+// const findComplement = num => {
+//   // console.log(num);
+//   const b = toBinary(num);
+//   // console.log(b);
+//   const f = flipBits(b);
+//   // console.log(f);
+//   const c = toDecimal(f);
+//   // console.log(c);
+//   return c;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 44 ms, faster than 98.82% of JavaScript online submissions
+// for Number Complement.
+// Memory Usage: 33.8 MB, less than 44.68% of JavaScript online submissions
+// for Number Complement.
 
 /**
  * @param {number} num
  * @return {number}
  */
-const findComplement = num => {
-  // console.log(num);
-  const b = toBinary(num);
-  // console.log(b);
-  const f = flipBits(b);
-  // console.log(f);
-  const c = toDecimal(f);
-  // console.log(c);
-  return c;
-};
+const findComplement = num =>
+  parseInt(
+    num
+      .toString(2)
+      .split('')
+      .map(b => ('0' === b ? 1 : 0))
+      .join(''),
+    2,
+  );
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 const tests = [
   {
-    name: 'Example 1',
     input: 5,
     expected: 2,
   },
   {
-    name: 'Example 2',
     input: 1,
     expected: 0,
   },
   {
-    name: '2',
     input: 2,
     expected: 1,
   },
 ];
 
-tests.forEach(({ name, input, expected }) => {
+for (let { input, expected } of tests) {
   const result = findComplement(input);
   if (result === expected) {
-    console.log(`✅ ${name}`);
+    console.log(`✅ ${input}`);
   } else {
-    console.log(`🔴 ${name}`);
+    console.log(`🔴 ${input}`);
     console.log(`Expected "${expected}", but got "${result}"`);
   }
-});
+}
