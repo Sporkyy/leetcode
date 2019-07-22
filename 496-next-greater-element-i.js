a45bb7c113c46bbf03889010034eb77dc844762f
Todd Sayre <captain.friendly@gmail.com>
2019-07-22 16:21:25 -0400

    1-liner

--- 496-next-greater-element-i.js
+++ 496-next-greater-element-i.js
@@ -3,8 +3,10 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// Runtime: 92 ms, faster than 9.03% of JavaScript online submissions for Next Greater Element I.
-// Memory Usage: 39.3 MB, less than 6.45% of JavaScript online submissions for Next Greater Element I.
+// Runtime: 92 ms, faster than 9.03% of JavaScript online submissions
+// for Next Greater Element I.
+// Memory Usage: 39.3 MB, less than 6.45% of JavaScript online submissions
+// for Next Greater Element I.
 
 // const nextNumbers = (num, arr) => arr.slice(arr.indexOf(num) + 1);
 
@@ -21,33 +23,10 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// Runtime: 68 ms, faster than 81.06% of JavaScript online submissions for Next Greater Element I.
-// Memory Usage: 35.3 MB, less than 58.06% of JavaScript online submissions for Next Greater Element I.
-
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
- */
-const nextGreaterElement = (nums1, nums2) => {
-  const result = new Array(nums1.length).fill(-1);
-  for (let i = 0; i < nums1.length; i++) {
-    for (let j = 0, ffwd = true; j < nums2.length; j++) {
-      if (!ffwd && nums1[i] < nums2[j]) {
-        result[i] = nums2[j];
-        break;
-      } else if (nums1[i] === nums2[j]) {
-        ffwd = false;
-      }
-    }
-  }
-  return result;
-};
-
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-// Runtime: 128 ms, faster than 5.51% of JavaScript online submissions for Next Greater Element I.
-// Memory Usage: 42.4 MB, less than 6.45% of JavaScript online submissions for Next Greater Element I.
+// Runtime: 128 ms, faster than 5.51% of JavaScript online submissions
+// for Next Greater Element I.
+// Memory Usage: 42.4 MB, less than 6.45% of JavaScript online submissions
+// for Next Greater Element I.
 
 /**
  * @param {number[]} nums1
@@ -70,29 +49,74 @@ const nextGreaterElement = (nums1, nums2) => {
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+// Runtime: 68 ms, faster than 81.06% of JavaScript online submissions
+// for Next Greater Element I.
+// Memory Usage: 35.3 MB, less than 58.06% of JavaScript online submissions
+// for Next Greater Element I.
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+// const nextGreaterElement = (nums1, nums2) => {
+//   const result = new Array(nums1.length).fill(-1);
+//   for (let i = 0; i < nums1.length; i++) {
+//     for (let j = 0, ffwd = true; j < nums2.length; j++) {
+//       if (!ffwd && nums1[i] < nums2[j]) {
+//         result[i] = nums2[j];
+//         break;
+//       } else if (nums1[i] === nums2[j]) {
+//         ffwd = false;
+//       }
+//     }
+//   }
+//   return result;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 92 ms, faster than 10.85% of JavaScript online submissions
+// for Next Greater Element I.
+// Memory Usage: 40.9 MB, less than 16.05% of JavaScript online submissions
+// for Next Greater Element I.
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const nextGreaterElement = (nums1, nums2) =>
+  nums1.map(n => nums2.slice(nums2.indexOf(n) + 1).filter(m => n < m)[0] || -1);
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 const tests = [
   {
-    name: 'Example 1',
     input: {
       nums1: [4, 1, 2],
       nums2: [1, 3, 4, 2],
     },
     expected: [-1, 3, -1],
   },
-  // {
-  //   name: 'Example 2',
-  //   input: {
-  //     nums1: [2, 4],
-  //     nums2: [1, 2, 3, 4],
-  //   },
-  //   expected: [3, -1],
-  // },
+
+  {
+    input: {
+      nums1: [2, 4],
+      nums2: [1, 2, 3, 4],
+    },
+    expected: [3, -1],
+  },
 ];
 
-const areArraysEqual = (a, b) => a.length === b.length && a.join() === b.join();
+const areArraysEqual = (a, b) => JSON.stringify(a) === JSON.stringify(b);
 
-tests.forEach(({ name, input: { nums1, nums2 }, expected }) => {
+for ({
+  input: { nums1, nums2 },
+  expected,
+} of tests) {
   const output = nextGreaterElement(nums1, nums2);
+  const name = `${JSON.stringify(nums1)} ${JSON.stringify(nums2)}`;
   if (areArraysEqual(expected, output)) {
     console.log(`✅ ${name}`);
   } else {
@@ -102,4 +126,4 @@ tests.forEach(({ name, input: { nums1, nums2 }, expected }) => {
     console.log('But got the below instead');
     console.log(output);
   }
-});
+}
---
+++ 496-next-greater-element-i.php
@@ -0,0 +1,92 @@
+<?php
+
+// 496. Next Greater Element I
+// https://leetcode.com/problems/next-greater-element-i/
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 92 ms, faster than 100.00% of PHP online submissions
+// for Next Greater Element I.
+// Memory Usage: 15.3 MB, less than 100.00% of PHP online submissions
+// for Next Greater Element I.
+
+// class Solution
+// {
+
+//     /**
+//      * @param Integer[] $nums1
+//      * @param Integer[] $nums2
+//      * @return Integer[]
+//      */
+//     function nextGreaterElement($nums1, $nums2)
+//     {
+//         $result = array_fill(0, count($nums1), -1);
+//         for ($i = 0; $i < count($nums1); $i++) {
+//             for ($j = 0, $ffwd = true; $j < count($nums2); $j++) {
+//                 if (!$ffwd && $nums1[$i] < $nums2[$j]) {
+//                     $result[$i] = $nums2[$j];
+//                     break;
+//                 } else if ($nums1[$i] === $nums2[$j]) {
+//                     $ffwd = false;
+//                 }
+//             }
+//         }
+//         return $result;
+//     }
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+class Solution
+{
+
+    /**
+     * @param Integer[] $nums1
+     * @param Integer[] $nums2
+     * @return Integer[]
+     */
+    function nextGreaterElement($nums1, $nums2)
+    {
+        $result = array_fill(0, count($nums1), -1);
+        for ($i = count($nums1); $i <= 0; $i--) {
+            for ($j = count(nums2); $j <= i; $j--) { }
+        }
+    }
+}
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+$tests = [
+    [
+        'input' => [
+            'nums1' => [4, 1, 2],
+            'nums2' => [1, 3, 4, 2],
+        ],
+        'expected' => [-1, 3, -1],
+    ],
+    [
+        'input' => [
+            'nums1' => [2, 4],
+            'nums2' => [1, 2, 3, 4],
+        ],
+        'expected' => [3, -1],
+    ],
+];
+
+$s = new Solution();
+
+foreach ($tests as $test) {
+    extract($test);
+    extract($input);
+    $output = $s->nextGreaterElement($nums1, $nums2);
+    $name = json_encode($nums1) . ' ' . json_encode($nums2);
+    if ($expected === $output) {
+        echo "✅ {$name}<br>";
+    } else {
+        echo "🔴 {$name}<br>";
+        echo 'Expected the below<br>';
+        echo '<pre>', print_r($expected, true), '</pre>';
+        echo 'But got the below instead<br>';
+        echo '<pre>', print_r($output, true), '</pre>';
+    }
+}
