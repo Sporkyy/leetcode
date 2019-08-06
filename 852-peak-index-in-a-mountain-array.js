d3f598436421d38fd4a0b096719e9f9bb9aa338e
Todd Sayre <captain.friendly@gmail.com>
2019-08-06 09:07:26 -0400

    1-liner

--- 852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array.js
@@ -3,57 +3,67 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+// Runtime: 64 ms, faster than 22.20% of JavaScript online submissions
+// for Peak Index in a Mountain Array.
+// Memory Usage: 34.9 MB, less than 60.98% of JavaScript online submissions
+// for Peak Index in a Mountain Array.
+
 /**
  * @param {number[]} A
  * @return {number}
  */
-const peakIndexInMountainArray = A => A.reduce((acc, curr, i) => ('undefined' === typeof acc || A[acc] < curr ? i : acc), undefined);
+// const peakIndexInMountainArray = A =>
+//   A.reduce((acc, curr, i) => ('undefined' === typeof acc || A[acc] < curr ? i : acc), undefined);
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [{
-    name: 'Example 1',
+// Runtime: 56 ms, faster than 67.36% of JavaScript online submissions
+// for Peak Index in a Mountain Array.
+// Memory Usage: 35.1 MB, less than 46.15% of JavaScript online submissions
+// for Peak Index in a Mountain Array.
+
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+const peakIndexInMountainArray = A => A.reduce((acc, curr, i) => (A[acc] < curr ? i : acc), 0);
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const tests = [
+  {
     input: [0, 1, 0],
-    expected: 1
+    expected: 1,
   },
   {
-    name: 'Example 2',
     input: [0, 2, 1, 0],
-    expected: 1
+    expected: 1,
   },
   {
-    name: 'Example 2 Modified A Little',
     input: [0, 1, 2, 0],
-    expected: 2
+    expected: 2,
   },
   {
-    name: 'Peak First',
     input: [3, 1, 2, 0],
-    expected: 0
+    expected: 0,
   },
   {
-    name: 'Peak Last',
     input: [1, 2, 0, 3],
-    expected: 3
+    expected: 3,
   },
   {
-    name: 'One Number',
     input: [999],
-    expected: 0
-  }
+    expected: 0,
+  },
 ];
 
-tests.forEach(({
-  name,
-  input,
-  expected
-}) => {
+tests.forEach(({ input, expected }) => {
   const result = peakIndexInMountainArray(input);
-  console.log(result);
+  const name = JSON.stringify(input);
   if (expected === result) {
     console.log(`✅ "${name}"`);
   } else {
     console.log(`🔴 "${name}"`);
     console.log(`Expected "${expected}", but got "${result}"`);
   }
-})
+});
