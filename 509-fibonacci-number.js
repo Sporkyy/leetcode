631f6c959b43abd2897ce4215960dd1285a06427
Todd Sayre <captain.friendly@gmail.com>
2019-12-10 15:39:13 -0500

    Better testing

--- 509-fibonacci-number.js
+++ 509-fibonacci-number.js
@@ -4,22 +4,20 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 84 ms, faster than 13.95% of JavaScript online submissions
-// for Fibonacci Number.
 // Memory Usage: 34.2 MB, less than 32.36% of JavaScript online submissions
-// for Fibonacci Number.
 
 /**
- * @param {number} N
+ * @param {number} n
  * @return {number}
  */
-const fib = N => (N <= 1 ? N : fib(N - 1) + fib(N - 2));
+const fib = n => (n <= 1 ? n : fib(n - 1) + fib(n - 2));
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 import { strictEqual } from 'assert';
 
+strictEqual(fib(0), 0);
+strictEqual(fib(1), 1);
 strictEqual(fib(2), 1);
 strictEqual(fib(3), 2);
 strictEqual(fib(4), 3);
-strictEqual(fib(0), 0);
-strictEqual(fib(1), 1);
