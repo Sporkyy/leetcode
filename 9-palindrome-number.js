4912fdf34b709b8324e6c87b9e10003636473a61
Todd Sayre <captain.friendly@gmail.com>
2020-01-16 09:09:58 -0500

    Mathematical solution

--- 9-palindrome-number.js
+++ 9-palindrome-number.js
@@ -130,24 +130,84 @@
 // Runtime: 180 ms, faster than 86.73% of JavaScript online submissions
 // Memory Usage: 44.9 MB, less than 96.55% of JavaScript online submissions
 
-/**
- * Reverse an integer
- *
- * @param {number} x The number in obverse
- * @param {number} [y=0] The number in reverse
- * @return {number} The reverse of x
- */
-const rev = (x, y = 0) => (0 === x ? y : rev(~~(x / 10), y * 10 + (x % 10)));
+// /**
+//  * Reverse an integer
+//  *
+//  * @param {number} x The number in obverse
+//  * @param {number} [y=0] The number in reverse
+//  * @return {number} The reverse of x
+//  */
+// const rev = (x, y = 0) => (0 === x ? y : rev(~~(x / 10), y * 10 + (x % 10)));
+
+// /**
+//  * @param {number} x
+//  * @return {boolean}
+//  */
+// const isPalindrome = x => (x < 0 ? false : x === rev(x));
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 212 ms, faster than 37.91% of JavaScript online submissions
+// Memory Usage: 45 MB, less than 91.38% of JavaScript online submissions
+
+// const digitsCnt = (x, n = 0) => (0 === x ? n : digitsCnt(~~(x / 10), n + 1));
+
+// console.log(digitsCnt(1));
+// console.log(digitsCnt(12));
+// console.log(digitsCnt(123));
+// console.log(digitsCnt(123456789));
+
+// /**
+//  * @param {number} x
+//  * @return {boolean}
+//  */
+// const isPalindrome = x => {
+//   if (x < 0) return false;
+//   let left = 10 ** (digitsCnt(x) - 1);
+//   // console.log(left);
+//   while (0 < x) {
+//     // console.log(left);
+//     // console.log(x);
+//     // console.log(Math.trunc(x / left), x % 10);
+//     if (Math.trunc(x / left) !== x % 10) return false;
+//     x = Math.trunc((x % left) / 10);
+//     left /= 100;
+//   }
+//   return true;
+// };
+
+// console.log(12345 / 10000);
+// console.log(12345 % 10);
+// console.log((12345 % 10000) / 10);
+// console.log(234 / 100);
+// console.log(234 % 10);
+// console.log((234 % 100) / 10);
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 176 ms, faster than 91.32% of JavaScript online submissions
+// Memory Usage: 45 MB, less than 92.24% of JavaScript online submissions
 
 /**
  * @param {number} x
  * @return {boolean}
  */
-const isPalindrome = x => (x < 0 ? false : x === rev(x));
+const isPalindrome = x => {
+  if (x < 0) return false;
+  let left = 10 ** ((Math.log10((x ^ (x >> 31)) - (x >> 31)) | 0) + 1 - 1);
+  while (0 < x) {
+    if (Math.trunc(x / left) !== x % 10) return false;
+    x = Math.trunc((x % left) / 10);
+    left /= 100;
+  }
+  return true;
+};
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 import { ok } from 'assert';
+
+ok(isPalindrome(0));
 
 ok(isPalindrome(121));
 
