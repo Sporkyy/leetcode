06976a1d5c27fd2fca2882ea7cbc6eb8ef6b6ef5
Todd Sayre <captain.friendly@gmail.com>
2020-01-17 08:55:54 -0500

    Added less abusive 2-function solution

--- 7-reverse-integer.js
+++ 7-reverse-integer.js
@@ -266,13 +266,34 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+// Runtime: 76 ms, faster than 67.09% of JavaScript online submissions
+// Memory Usage: 35.5 MB, less than 92.71% of JavaScript online submissions
+
+// /**
+//  * @param {number} x
+//  * @param {number} [y=0]
+//  * @returns {number}
+//  */
+// const rev = (x, y = 0) => (0 === x ? y : rev(~~(x / 10), y * 10 + (x % 10)));
+
+// /**
+//  * @param {number} x
+//  * @return {number}
+//  */
+// const reverse = x => {
+//   const revX = rev(x);
+//   return -(2 ** 31) <= revX && revX <= 2 ** 31 - 1 ? revX : 0;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 // Runtime: 64 ms, faster than 97.69% of JavaScript online submissions
 // Memory Usage: 35.6 MB, less than 91.67% of JavaScript online submissions
 
 /**
  * @param {number} x
  * @param {number} [revX=0]
- * @return {number}
+ * @return {number} Reverse of x or 0 if int32 overflow
  */
 const reverse = (x, revX = 0) =>
   0 === x
