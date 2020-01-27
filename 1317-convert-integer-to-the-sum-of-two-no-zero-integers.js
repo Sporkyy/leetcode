241f5c4274ebc5c863424aecbfecc1777c980576
Todd Sayre <captain.friendly@gmail.com>
2020-01-27 14:14:03 -0500

    Good enough to post

--- 1317-convert-integer-to-the-sum-of-two-no-zero-integers.js
+++ 1317-convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -108,12 +108,38 @@
 // Runtime: 56 ms, faster than 73.40% of JavaScript online submissions
 // Memory Usage: 33.3 MB, less than 100.00% of JavaScript online submissions
 
+// /**
+//  * @param {number} x
+//  * @returns {boolean}
+//  */
+// const hasZeros = x =>
+//   0 === x % 10 ? true : 10 < Math.abs(x) ? hasZeros(Math.trunc(x / 10)) : false;
+
+// /**
+//  * @param {number} x
+//  * @param {number} [y=0]
+//  * @return {number[]}
+//  */
+// const getNoZeroIntegers = (x, y = 0) =>
+//   !hasZeros(x) && !hasZeros(y)
+//     ? [Math.min(x, y), Math.max(x, y)]
+//     : getNoZeroIntegers(x - 1, y + 1);
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 136 ms, faster than 7.37% of JavaScript online submissions
+// Memory Usage: 33.5 MB, less than 100.00% of JavaScript online submissions
+
 /**
  * @param {number} x
  * @returns {boolean}
  */
 const hasZeros = x =>
-  0 === x % 10 ? true : 10 < Math.abs(x) ? hasZeros(Math.trunc(x / 10)) : false;
+  0 === x % 10
+    ? true
+    : Math.abs(x) <= 100
+    ? false
+    : hasZeros(Math.trunc(x / 10));
 
 /**
  * @param {number} x
@@ -121,21 +147,20 @@ const hasZeros = x =>
  * @return {number[]}
  */
 const getNoZeroIntegers = (x, y = 0) =>
-  !hasZeros(x) && !hasZeros(y)
-    ? [Math.min(x, y), Math.max(x, y)]
-    : getNoZeroIntegers(x - 1, y + 1);
+  !hasZeros(x) && !hasZeros(y) ? [y, x] : getNoZeroIntegers(x - 1, y + 1);
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 import { deepStrictEqual } from 'assert';
 
-deepStrictEqual(getNoZeroIntegers(2), [1, 1]);
-// // Explanation: A = 1, B = 1. A + B = n and both A and B don't contain any 0 in their decimal representation.
+// deepStrictEqual(getNoZeroIntegers(2), [1, 1]);
 
-deepStrictEqual(getNoZeroIntegers(11), [2, 9]);
+// deepStrictEqual(getNoZeroIntegers(11), [2, 9]);
 
-deepStrictEqual(getNoZeroIntegers(10000), [1, 9999]);
+// deepStrictEqual(getNoZeroIntegers(10000), [1, 9999]);
 
-deepStrictEqual(getNoZeroIntegers(69), [1, 68]);
+// deepStrictEqual(getNoZeroIntegers(69), [1, 68]);
 
-deepStrictEqual(getNoZeroIntegers(1010), [11, 999]);
+// deepStrictEqual(getNoZeroIntegers(1010), [11, 999]);
+
+deepStrictEqual(getNoZeroIntegers(1403), [4, 1399]);
