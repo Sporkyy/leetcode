f39e77f5958767ad71ce9592559a0af0829542c8
Todd Sayre <captain.friendly@gmail.com>
2020-01-29 16:25:25 -0500

    Fastest solution possible

--- 1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number.js
@@ -22,11 +22,58 @@
 // Runtime: 100 ms, faster than 8.21% of JavaScript online submissions
 // Memory Usage: 33.5 MB, less than 100.00% of JavaScript online submissions
 
-const tribonacci = n => {
-  const a = [0, 1, 1];
-  for (let i = 3; i <= n; i++) a[i] = a[i - 3] + a[i - 2] + a[i - 1];
-  return a[n];
-};
+// const tribonacci = n => {
+//   const a = [0, 1, 1];
+//   for (let i = 3; i <= n; i++) a[i] = a[i - 3] + a[i - 2] + a[i - 1];
+//   return a[n];
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 56 ms, faster than 50.36% of JavaScript online submissions
+// Memory Usage: 33.8 MB, less than 100.00% of JavaScript online submissions
+
+const tribonacci = n =>
+  [
+    0,
+    1,
+    1,
+    2,
+    4,
+    7,
+    13,
+    24,
+    44,
+    81,
+    149,
+    274,
+    504,
+    927,
+    1705,
+    3136,
+    5768,
+    10609,
+    19513,
+    35890,
+    66012,
+    121415,
+    223317,
+    410744,
+    755476,
+    1389537,
+    2555757,
+    4700770,
+    8646064,
+    15902591,
+    29249425,
+    53798080,
+    98950096,
+    181997601,
+    334745777,
+    615693474,
+    1132436852,
+    2082876103,
+  ][n];
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
