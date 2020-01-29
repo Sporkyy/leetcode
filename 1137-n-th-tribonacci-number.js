0fcd76dba1253f50c19b4042ccd9a67ab2cac2d7
Todd Sayre <captain.friendly@gmail.com>
2020-01-29 15:41:18 -0500

    Rewrote tests using Node.js assert

--- 1137-n-th-tribonacci-number.js
+++ 1137-n-th-tribonacci-number.js
@@ -4,9 +4,7 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 52 ms, faster than 63.26% of JavaScript online submissions
-// for N-th Tribonacci Number.
 // Memory Usage: 34 MB, less than 100.00% of JavaScript online submissions
-// for N-th Tribonacci Number.
 
 /**
  * @param {number} n
@@ -21,75 +19,43 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// Runtime: 56 ms, faster than 34.47% of JavaScript online submissions
-// for N-th Tribonacci Number.
-// Memory Usage: 33.8 MB, less than 100.00% of JavaScript online submissions
-// for N-th Tribonacci Number.
+// Runtime: 100 ms, faster than 8.21% of JavaScript online submissions
+// Memory Usage: 33.5 MB, less than 100.00% of JavaScript online submissions
 
 const tribonacci = n => {
-  a = [0, 1, 1];
-  for (i = 3; i <= n; i++) a[i] = a[i - 3] + a[i - 2] + a[i - 1];
+  const a = [0, 1, 1];
+  for (let i = 3; i <= n; i++) a[i] = a[i - 3] + a[i - 2] + a[i - 1];
   return a[n];
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// t0 = 0 + 1 + 1 = 0
-// t1 = 0 + 1 + 1 = 1
-// t2 = 0 + 1 + 1 = 1
-// t3 = 0 + 1 + 1 = 2
-// t4 = 1 + 1 + 2 = 4
-// t5 = 1 + 2 + 4 = 7
-// t6 = 2 + 4 + 7 = 13
+import { strictEqual } from 'assert';
 
-const tests = [
-  {
-    input: 0,
-    expected: 0,
-  },
+strictEqual(tribonacci(0), 0);
 
-  {
-    input: 1,
-    expected: 1,
-  },
+strictEqual(tribonacci(1), 1);
 
-  {
-    input: 2,
-    expected: 1,
-  },
+strictEqual(tribonacci(2), 1);
 
-  {
-    input: 3,
-    expected: 2,
-  },
+strictEqual(tribonacci(3), 2);
 
-  {
-    input: 4,
-    expected: 4,
-  },
+strictEqual(tribonacci(4), 4);
 
-  {
-    input: 5,
-    expected: 7,
-  },
+strictEqual(tribonacci(5), 7);
 
-  {
-    input: 6,
-    expected: 13,
-  },
+strictEqual(tribonacci(6), 13);
 
-  {
-    input: 25,
-    expected: 1389537,
-  },
-];
+strictEqual(tribonacci(25), 1389537);
 
-for ({ input, expected } of tests) {
-  const result = tribonacci(input);
-  if (expected === result) {
-    console.log(`✅ ${input}`);
-  } else {
-    console.log(`🔴 ${input}`);
-    console.log(`Expected "${expected}", but got "${result}"`);
-  }
-}
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/*
+t0 = 0 + 1 + 1 = 0
+t1 = 0 + 1 + 1 = 1
+t2 = 0 + 1 + 1 = 1
+t3 = 0 + 1 + 1 = 2
+t4 = 1 + 1 + 2 = 4
+t5 = 1 + 2 + 4 = 7
+t6 = 2 + 4 + 7 = 13
+*/
