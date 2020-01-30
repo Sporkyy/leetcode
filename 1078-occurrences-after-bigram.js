20e93f27e48470017e523244529526ea6b176e38
Todd Sayre <captain.friendly@gmail.com>
2020-01-30 11:00:48 -0500

    Converted tests to Node.js assert and added better solutions

--- 1078-occurrences-after-bigram.js
+++ 1078-occurrences-after-bigram.js
@@ -4,9 +4,47 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 48 ms, faster than 91.64% of JavaScript online submissions
-// for Occurrences After Bigram.
 // Memory Usage: 33.8 MB, less than 100.00% of JavaScript online submissions
-// for Occurrences After Bigram.
+
+// /**
+//  * @param {string} text
+//  * @param {string} first
+//  * @param {string} second
+//  * @return {string[]}
+//  */
+// const findOcurrences = (text, first, second) =>
+//   text.split(' ').reduce((acc, curr, i, t) => {
+//     if (i < 2) return acc;
+//     if (first === t[i - 2] && second === t[i - 1]) acc.push(curr);
+//     return acc;
+//   }, []);
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 60 ms, faster than 30.87% of JavaScript online submissions
+// Memory Usage: 33.1 MB, less than 100.00% of JavaScript online submissions
+
+// /**
+//  * @param {string} text
+//  * @param {string} first
+//  * @param {string} second
+//  * @return {string[]}
+//  */
+// const findOcurrences = (text, first, second) =>
+//   text
+//     .split(' ')
+//     .reduce(
+//       (acc, curr, idx, src) =>
+//         first === src[idx - 2] && second === src[idx - 1]
+//           ? acc.concat(curr)
+//           : acc,
+//       [],
+//     );
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 48 ms, faster than 91.28% of JavaScript online submissions
+// Memory Usage: 33.7 MB, less than 100.00% of JavaScript online submissions
 
 /**
  * @param {string} text
@@ -15,44 +53,20 @@
  * @return {string[]}
  */
 const findOcurrences = (text, first, second) =>
-  text.split(' ').reduce((acc, curr, i, t) => {
-    if (i < 2) return acc;
-    if (first === t[i - 2] && second === t[i - 1]) acc.push(curr);
-    return acc;
-  }, []);
+  text
+    .split(' ')
+    .filter((_, idx, src) => first === src[idx - 2] && second === src[idx - 1]);
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    input: {
-      text: 'alice is a good girl she is a good student',
-      first: 'a',
-      second: 'good',
-    },
-    expected: ['girl', 'student'],
-  },
-  {
-    input: {
-      text: 'we will we will rock you',
-      first: 'we',
-      second: 'will',
-    },
-    expected: ['we', 'rock'],
-  },
-];
+import { deepStrictEqual } from 'assert';
 
-const areArraysEqual = (a, b) => a.length === b.length && a.join() === b.join();
+deepStrictEqual(
+  findOcurrences('alice is a good girl she is a good student', 'a', 'good'),
+  ['girl', 'student'],
+);
 
-tests.forEach(({ input: { text, first, second }, expected }) => {
-  const output = findOcurrences(text, first, second);
-  if (areArraysEqual(expected, output)) {
-    console.log(`✅ ${text}`);
-  } else {
-    console.log(`🔴 ${text}`);
-    console.log('Expected the below');
-    console.log(expected);
-    console.log('But got the below instead');
-    console.log(output);
-  }
-});
+deepStrictEqual(findOcurrences('we will we will rock you', 'we', 'will'), [
+  'we',
+  'rock',
+]);
