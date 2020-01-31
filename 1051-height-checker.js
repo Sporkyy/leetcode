90a823f10883453566824c75d5ad230c05a7853f
Todd Sayre <captain.friendly@gmail.com>
2020-01-31 15:11:29 -0500

    Added 1-liner solution

--- 1051-height-checker.js
+++ 1051-height-checker.js
@@ -3,205 +3,226 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// Runtime: 52 ms, faster than 96.50% of JavaScript online submissions
-// for Height Checker.
-// Memory Usage: 34.9 MB, less than 100.00% of JavaScript online submissions
-// for Height Checker.
+// Runtime: 60 ms, faster than 73.44% of JavaScript online submissions
+// Memory Usage: 35.1 MB, less than 100.00% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} heights
+//  * @return {number}
+//  */
+// const heightChecker = heights =>
+//   heights
+//     .slice()
+//     .sort((a, b) => a - b)
+//     .reduce((acc, curr, idx) => (curr !== heights[idx] ? acc + 1 : acc), 0);
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 56 ms, faster than 86.84% of JavaScript online submissions
+// Memory Usage: 35.1 MB, less than 100.00% of JavaScript online submissions
 
 /**
  * @param {number[]} heights
  * @return {number}
  */
 const heightChecker = heights => {
-  let id10ts = 0;
+  let moves = 0;
   const counts = new Array(101).fill(0);
-  for (h of heights) counts[h]++;
+  for (const h of heights) counts[h]++;
   for (let i = 0, z = 0; i < 101; i++)
-    for (let j = counts[i]; 0 < j; j--, z++) if (heights[z] !== i) id10ts++;
-  return id10ts;
+    for (let j = counts[i]; 0 < j; j--, z++) if (heights[z] !== i) moves++;
+  return moves;
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    input: [1, 1, 4, 2, 1, 3],
-    expected: 3,
-  },
+import { strictEqual } from 'assert';
 
-  // Explanation: Students with heights 4, 3 and the last 1 are not
-  // standing in the right positions.
+strictEqual(heightChecker([1, 1, 4, 2, 1, 3]), 3);
+// Explanation: Students with heights 4, 3 and the last 1 are not
+// standing in the right positions.
 
-  {
-    input: [1, 2, 3, 4, 5],
-    expected: 0,
-  },
+strictEqual(heightChecker([1, 2, 3, 4, 5]), 0);
 
-  {
-    input: [5, 4, 3, 2, 1],
-    expected: 4,
-  },
+strictEqual(heightChecker([5, 4, 3, 2, 1]), 4);
 
-  {
-    input: [1, 1, 1, 1, 1],
-    expected: 0,
-  },
+strictEqual(heightChecker([1, 1, 1, 1, 1]), 0);
 
-  {
-    input: [1, 1, 2, 2],
-    expected: 0,
-  },
+strictEqual(heightChecker([1, 1, 2, 2]), 0);
 
-  {
-    input: [2, 2, 1, 1],
-    expected: 4,
-  },
+strictEqual(heightChecker([2, 2, 1, 1]), 4);
 
-  {
-    input: [2, 1, 1, 2],
-    expected: 2,
-  },
+strictEqual(heightChecker([2, 1, 1, 2]), 2);
 
-  {
-    input: [1, 2, 2, 1],
-    expected: 2,
-  },
+strictEqual(heightChecker([1, 2, 2, 1]), 2);
 
-  {
-    input: [1, 2, 1, 2],
-    expected: 2,
-  },
+strictEqual(heightChecker([1, 2, 1, 2]), 2);
 
-  {
-    input: [2, 1, 2, 1],
-    expected: 2,
-  },
+strictEqual(heightChecker([2, 1, 2, 1]), 2);
 
-  {
-    input: [2, 1, 1, 1],
-    expected: 2,
-  },
+strictEqual(heightChecker([2, 1, 1, 1]), 2);
 
-  {
-    input: [2, 2, 2, 1],
-    expected: 2,
-  },
+strictEqual(heightChecker([2, 2, 2, 1]), 2);
 
-  {
-    input: [2, 1, 2, 1, 1, 2, 2, 1],
-    expected: 4,
-  },
+strictEqual(heightChecker([2, 1, 2, 1, 1, 2, 2, 1]), 4);
 
-  {
-    input: [
-      31,
-      81,
-      41,
-      78,
-      48,
-      2,
-      83,
-      48,
-      21,
-      20,
-      43,
-      15,
-      26,
-      78,
-      96,
-      55,
-      5,
-      46,
-      35,
-      89,
-      85,
-      54,
-      76,
-      64,
-      71,
-      36,
-      98,
-      94,
-      100,
-      7,
-      88,
-      92,
-      80,
-      43,
-      24,
-      89,
-      50,
-      61,
-      59,
-      20,
-      94,
-      57,
-      99,
-      62,
-      82,
-      46,
-      28,
-      57,
-      66,
-      62,
-      56,
-      15,
-      12,
-      63,
-      19,
-      35,
-      12,
-      26,
-      15,
-      59,
-      8,
-      44,
-      46,
-      45,
-      33,
-      20,
-      27,
-      31,
-      85,
-      15,
-      92,
-      63,
-      63,
-      40,
-      35,
-      95,
-      91,
-      1,
-      4,
-      57,
-      55,
-      68,
-      53,
-      28,
-      15,
-      94,
-      74,
-      89,
-      77,
-      7,
-      25,
-      63,
-      77,
-      24,
-      76,
-      44,
-    ],
-    expected: 95,
-  },
-];
+strictEqual(
+  heightChecker([
+    31,
+    81,
+    41,
+    78,
+    48,
+    2,
+    83,
+    48,
+    21,
+    20,
+    43,
+    15,
+    26,
+    78,
+    96,
+    55,
+    5,
+    46,
+    35,
+    89,
+    85,
+    54,
+    76,
+    64,
+    71,
+    36,
+    98,
+    94,
+    100,
+    7,
+    88,
+    92,
+    80,
+    43,
+    24,
+    89,
+    50,
+    61,
+    59,
+    20,
+    94,
+    57,
+    99,
+    62,
+    82,
+    46,
+    28,
+    57,
+    66,
+    62,
+    56,
+    15,
+    12,
+    63,
+    19,
+    35,
+    12,
+    26,
+    15,
+    59,
+    8,
+    44,
+    46,
+    45,
+    33,
+    20,
+    27,
+    31,
+    85,
+    15,
+    92,
+    63,
+    63,
+    40,
+    35,
+    95,
+    91,
+    1,
+    4,
+    57,
+    55,
+    68,
+    53,
+    28,
+    15,
+    94,
+    74,
+    89,
+    77,
+    7,
+    25,
+    63,
+    77,
+    24,
+    76,
+    44,
+  ]),
+  95,
+);
 
-for ({ input, expected } of tests) {
-  const name = input.join(', ');
-  const output = heightChecker(input);
-  if (expected === output) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected "${expected}", but got "${output}"`);
-  }
-}
+strictEqual(
+  heightChecker([
+    10,
+    6,
+    6,
+    10,
+    10,
+    9,
+    8,
+    8,
+    3,
+    3,
+    8,
+    2,
+    1,
+    5,
+    1,
+    9,
+    5,
+    2,
+    7,
+    4,
+    7,
+    7,
+  ]),
+  22,
+);
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+/*
+
+1, 1, 4, 2, 1, 3
+
+|  x | min | max | wrong |
+| -: | --: | --: | ----: |
+|  1 |   1 |   4 |       |
+|  1 |   1 |   4 |       |
+|  4 |   1 |   4 |     x |
+|  2 |   1 |   4 |       |
+|  1 |   1 |   4 |     x |
+|  3 |   1 |   4 |     x |
+
+=-=-=-=-=-=-=-=-=-=-=-=
+
+2, 2, 1, 1
+
+| 2 | max = 2
+| 2 | max = 2
+| 1 | max = 2
+| 1 | max = 2
+
+| 1 | min = 1
+| 1 | min = 1
+| 2 | min = 1
+| 2 | min = 1
+
+*/
