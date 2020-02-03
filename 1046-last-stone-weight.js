afa2a5630cb9b0e8cce757e560edbae80c44669b
Todd Sayre <captain.friendly@gmail.com>
2020-02-03 09:32:46 -0500

    One-line recursive solution is good enough

--- 1046-last-stone-weight.js
+++ 1046-last-stone-weight.js
@@ -40,14 +40,14 @@
 // Runtime: 52 ms, faster than 92.22% of JavaScript online submissions
 // Memory Usage: 35 MB, less than 100.00% of JavaScript online submissions
 
-/**
- * @param {number[]} s
- * @return {number}
- */
-const lastStoneWeight = s => {
-  while (1 < s.sort((a, b) => a - b).length) s.push(s.pop() - s.pop());
-  return s[0];
-};
+// /**
+//  * @param {number[]} s
+//  * @return {number}
+//  */
+// const lastStoneWeight = s => {
+//   while (1 < s.sort((a, b) => a - b).length) s.push(s.pop() - s.pop());
+//   return s[0];
+// };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -84,14 +84,57 @@ const lastStoneWeight = s => {
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+// Runtime: 76 ms, faster than 29.01% of JavaScript online submissions
+// Memory Usage: 34.9 MB, less than 100.00% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} s
+//  * @return {number}
+//  */
+// const lastStoneWeight = s =>
+//   1 === s.length
+//     ? s[0]
+//     : lastStoneWeight(s.sort((a, b) => a - b).concat(s.pop() - s.pop()));
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 80 ms, faster than 25.47% of JavaScript online submissions
+// Memory Usage: 34.8 MB, less than 100.00% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} s
+//  * @return {number}
+//  */
+// const lastStoneWeight = s => {
+//   while (1 < s.sort((a, b) => a - b).length) s.unshift(s.pop() - s.pop());
+//   return s[0];
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 64 ms, faster than 52.83% of JavaScript online submissions
+// Memory Usage: 35.2 MB, less than 100.00% of JavaScript online submissions
+
+/**
+ * @param {number[]} s
+ * @return {number}
+ */
+const lastStoneWeight = s =>
+  1 === s.length
+    ? s[0]
+    : lastStoneWeight(s.sort((a, b) => a - b).concat(s.pop() - s.pop()));
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 import { strictEqual } from 'assert';
 
 strictEqual(lastStoneWeight([2, 7, 4, 1, 8, 1]), 1);
 // Explanation:
-// We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
-// we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
-// we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
-// we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+//     start: [2, 7, 4, 1, 8, 1]
+// 7 + 8 = 1: [2, 4, 1, 1, 1]
+// 2 + 4 = 2: [2, 1, 1, 1]
+// 2 + 1 = 1: [1, 1, 1]
+// 1 + 1 = 0: [1]
 
 strictEqual(lastStoneWeight([1]), 1);
 
