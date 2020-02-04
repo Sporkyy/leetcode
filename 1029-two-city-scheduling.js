2a15b570c95b1e6faae1ca437e4ae13a612a8dda
Todd Sayre <captain.friendly@gmail.com>
2020-02-04 15:25:23 -0500

    Converted test suite to Node.js assert

--- 1029-two-city-scheduling.js
+++ 1029-two-city-scheduling.js
@@ -4,9 +4,7 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 60 ms, faster than 67.97% of JavaScript online submissions
-// for Two City Scheduling.
 // Memory Usage: 34.8 MB, less than 41.67% of JavaScript online submissions
-// for Two City Scheduling.
 
 /**
  * @param {number[][]} costs
@@ -19,37 +17,36 @@ const twoCitySchedCost = costs =>
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  // | 10 |  30  | 400 | 30
-  // | 20 | 200  |  50 | 20
-  // Explanation:
-  // The first person goes to city A for a cost of 10.
-  // The second person goes to city A for a cost of 30.
-  // The third person goes to city B for a cost of 50.
-  // The fourth person goes to city B for a cost of 20.
+import { strictEqual } from 'assert';
 
-  {
-    input: [[10, 20], [30, 200], [400, 50], [30, 20]],
-    expected: 110,
-  },
+strictEqual(
+  twoCitySchedCost([
+    [10, 20],
+    [30, 200],
+    [400, 50],
+    [30, 20],
+  ]),
+  110,
+);
 
-  {
-    input: [[10, 20], [30, 200], [50, 400], [20, 30]],
-    expected: 130,
-  },
+strictEqual(
+  twoCitySchedCost([
+    [10, 20],
+    [30, 200],
+    [50, 400],
+    [20, 30],
+  ]),
+  130,
+);
 
-  {
-    input: [[259, 770], [448, 54], [926, 667], [184, 139], [840, 118], [577, 469]],
-    expected: 1859,
-  },
-];
-
-for (let { input, expected } of tests) {
-  const output = twoCitySchedCost(input);
-  if (output === expected) {
-    console.log(`✅ ${input}`);
-  } else {
-    console.log(`🔴 ${input}`);
-    console.log(`Expected "${expected}" but got "${output}"`);
-  }
-}
+strictEqual(
+  twoCitySchedCost([
+    [259, 770],
+    [448, 54],
+    [926, 667],
+    [184, 139],
+    [840, 118],
+    [577, 469],
+  ]),
+  1859,
+);
