da2b4c0c1a0fc639cf2f2f5d5a14b1e25d266ea0
Todd Sayre <captain.friendly@gmail.com>
2020-02-04 14:39:08 -0500

    Best solution is a boring solution

--- 1030-matrix-cells-in-distance-order.js
+++ 1030-matrix-cells-in-distance-order.js
@@ -1,6 +1,21 @@
 // 1030. Matrix Cells in Distance Order
 // https://leetcode.com/problems/matrix-cells-in-distance-order/
 
+/*
+
+We are given a matrix with R rows and C columns has cells with
+integer coordinates (r, c), where 0 <= r < R and 0 <= c < C.
+
+Additionally, we are given a cell in that matrix with coordinates (r0, c0).
+
+Return the coordinates of all cells in the matrix, sorted by their distance
+from (r0, c0) from smallest distance to largest distance.  Here, the distance
+between two cells (r1, c1) and (r2, c2) is the Manhattan distance,
+|r1 - r2| + |c1 - c2|.  (You may return the answer in any order that satisfies
+this condition.)
+
+*/
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 176 ms, faster than 71.19% of JavaScript online submissions
@@ -19,13 +34,13 @@
 //   return result;
 // };
 
-/**
- * @param {number} r
- * @param {number} c
- * @param {number} r0
- * @param {number} c0
- * @return {number[][]}
- */
+// /**
+//  * @param {number} rowCnt
+//  * @param {number} colCnt
+//  * @param {number} r0
+//  * @param {number} c0
+//  * @return {number[][]}
+//  */
 // const allCellsDistOrder = (R, C, r0, c0) => {
 //   const cells = cellsWithDistance(R, C, r0, c0);
 //   const sorted = cells.sort((a, b) => (a[2] < b[2] ? -1 : b[2] < a[2] ? 1 : 0));
@@ -38,13 +53,13 @@
 // Runtime: 376 ms, faster than 5.90% of JavaScript online submissions
 // Memory Usage: 52.5 MB, less than 42.36% of JavaScript online submissions
 
-/**
- * @param {number} r
- * @param {number} c
- * @param {number} r0
- * @param {number} c0
- * @return {number[][]}
- */
+// /**
+//  * @param {number} rowCnt
+//  * @param {number} colCnt
+//  * @param {number} r0
+//  * @param {number} c0
+//  * @return {number[][]}
+//  */
 // const allCellsDistOrder = (R, C, r0, c0) => {
 //   result = [];
 //   for (rI = i = 0; rI <= R - 1; rI++) {
@@ -61,13 +76,13 @@
 // Runtime: 168 ms, faster than 84.59% of JavaScript online submissions
 // Memory Usage: 52.1 MB, less than 45.85% of JavaScript online submissions
 
-/**
- * @param {number} r
- * @param {number} c
- * @param {number} r0
- * @param {number} c0
- * @return {number[][]}
- */
+// /**
+//  * @param {number} rowCnt
+//  * @param {number} colCnt
+//  * @param {number} r0
+//  * @param {number} c0
+//  * @return {number[][]}
+//  */
 // const allCellsDistOrder = (R, C, r0, c0) => {
 //   const o = {};
 //   for (let rI = (i = 0); rI <= R - 1; rI++) {
@@ -84,13 +99,13 @@
 // Runtime: 196 ms, faster than 33.11% of JavaScript online submissions
 // Memory Usage: 68.4 MB, less than 5.24% of JavaScript online submissions
 
-/**
- * @param {number} r
- * @param {number} c
- * @param {number} r0
- * @param {number} c0
- * @return {number[][]}
- */
+// /**
+//  * @param {number} rowCnt
+//  * @param {number} colCnt
+//  * @param {number} r0
+//  * @param {number} c0
+//  * @return {number[][]}
+//  */
 // const allCellsDistOrder = (R, C, r0, c0) => {
 //   const byDistance = [];
 //   for (let rI = 0; rI <= R - 1; rI++) {
@@ -136,42 +151,48 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
+// Runtime: 156 ms, faster than 81.48% of JavaScript online submissions
+// Memory Usage: 46.3 MB, less than 100.00% of JavaScript online submissions
+
 /**
- * @param {number} r
- * @param {number} c
+ * @param {number} R
+ * @param {number} C
  * @param {number} r0
  * @param {number} c0
  * @return {number[][]}
  */
-const allCellsDistOrder = (r, c, r0, c0) => {
-  const byDistance = new Array(r * c).fill(0).map(x => []);
-  for (let rI = 0; rI <= r - 1; rI++) {
-    for (let cI = 0; cI <= c - 1; cI++) {
-      const distance = Math.abs(r0 - rI) + Math.abs(c0 - cI);
-      byDistance[distance].push([rI, cI]);
-    }
-  }
-  const result = [];
-  for (let i = 0; i < byDistance.length; i++) {
-    for (let j = 0; j < byDistance[i].length; j++) {
-      result.push(byDistance[i][j]);
-    }
-  }
-  return result;
+const allCellsDistOrder = (R, C, r0, c0) => {
+  const byDistance = new Array(R + C).fill().map(_ => []);
+  for (let i = 0; i <= R - 1; i++)
+    for (let j = 0; j <= C - 1; j++)
+      byDistance[Math.abs(r0 - i) + Math.abs(c0 - j)].push([i, j]);
+  const flattened = [];
+  for (let i = 0; i < byDistance.length; i++)
+    for (let j = 0; j < byDistance[i].length; j++)
+      flattened.push(byDistance[i][j]);
+  return flattened;
 };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// console.log(allCellsDistOrder(1, 1, 0, 0));
+// console.log(allCellsDistOrder(1, 2, 0, 0));
+// console.log(allCellsDistOrder(2, 1, 0, 0));
+// console.log(allCellsDistOrder(2, 2, 0, 0));
+// console.log(allCellsDistOrder(3, 3, 0, 0));
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 import { deepStrictEqual } from 'assert';
 
-// [0, 0], [0, 1]
+// |(0, 0)| 0, 1 |
 deepStrictEqual(allCellsDistOrder(1, 2, 0, 0), [
   [0, 0],
   [0, 1],
 ]);
 
-// [0, 0], [0, 1]
-// [1, 0], [1, 1]
+// // | 0, 0 |(0, 1)|
+// // | 1, 0 | 1, 1 |
 deepStrictEqual(allCellsDistOrder(2, 2, 0, 1), [
   [0, 1],
   [0, 0],
@@ -179,12 +200,57 @@ deepStrictEqual(allCellsDistOrder(2, 2, 0, 1), [
   [1, 0],
 ]);
 
-// [0, 0], [0, 1], [0, 2]
-// [1, 0], [1, 1], [1, 2]
+// // | 0, 0 | 0, 1 | 0, 2 |
+// // | 1, 0 | 1, 1 |(1, 2)|
 deepStrictEqual(allCellsDistOrder(2, 3, 1, 2), [
   [1, 2],
   [0, 2],
   [1, 1],
+  [0, 1],
+  [1, 0],
+  [0, 0],
+]);
+
+// // |(0, 0)| 0, 1 | 0, 2 |
+// // | 1, 0 | 1, 1 | 1, 2 |
+// // | 2, 0 | 2, 1 | 2, 2 |
+deepStrictEqual(allCellsDistOrder(3, 3, 0, 0), [
+  [0, 0],
+  [0, 1],
+  [1, 0],
+  [0, 2],
+  [1, 1],
+  [2, 0],
+  [1, 2],
+  [2, 1],
+  [2, 2],
+]);
+
+// // | 0, 0 | 0, 1 | 0, 2 |
+// // | 1, 0 |(1, 1)| 1, 2 |
+// // | 2, 0 | 2, 1 | 2, 2 |
+deepStrictEqual(allCellsDistOrder(3, 3, 1, 1), [
+  [1, 1],
+  [0, 1],
+  [1, 0],
+  [1, 2],
+  [2, 1],
+  [0, 0],
+  [0, 2],
+  [2, 0],
+  [2, 2],
+]);
+
+// // | 0, 0 | 0, 1 | 0, 2 |
+// // | 1, 0 | 1, 1 | 1, 2 |
+// // | 2, 0 | 2, 1 |(2, 2)|
+deepStrictEqual(allCellsDistOrder(3, 3, 2, 2), [
+  [2, 2],
+  [1, 2],
+  [2, 1],
+  [0, 2],
+  [1, 1],
+  [2, 0],
   [0, 1],
   [1, 0],
   [0, 0],
