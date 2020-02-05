8191c2a7be49ffb270900e1ee62a5f1514c639ec
Todd Sayre <captain.friendly@gmail.com>
2020-02-05 16:05:33 -0500

    Converted test suite to Node.js assert

--- 1009-complement-of-base-10-integer.js
+++ 1009-complement-of-base-10-integer.js
@@ -4,9 +4,7 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 56 ms, faster than 79.18% of JavaScript online submissions
-// for Complement of Base 10 Integer.
 // Memory Usage: 33.9 MB, less than 37.17% of JavaScript online submissions
-// for Complement of Base 10 Integer.
 
 /**
  * @param {number} N
@@ -23,32 +21,13 @@ const bitwiseComplement = N =>
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    input: 5,
-    expected: 2,
-  },
-  // Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
+import { strictEqual } from 'assert';
 
-  {
-    input: 7,
-    expected: 0,
-  },
-  // Explanation: 7 is "111" in binary, with complement "000" in binary, which is 0 in base-10.
+strictEqual(bitwiseComplement(5), 2);
+// Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
 
-  {
-    input: 10,
-    expected: 5,
-  },
-  // Explanation: 10 is "1010" in binary, with complement "0101" in binary, which is 5 in base-10.
-];
+strictEqual(bitwiseComplement(7), 0);
+// Explanation: 7 is "111" in binary, with complement "000" in binary, which is 0 in base-10.
 
-tests.forEach(({ input, expected }) => {
-  const output = bitwiseComplement(input);
-  if (output === expected) {
-    console.log(`✅ ${input}`);
-  } else {
-    console.log(`🔴 ${input}`);
-    console.log(`Expected "${expected}", but got "${output}"`);
-  }
-});
+strictEqual(bitwiseComplement(10), 5);
+// Explanation: 10 is "1010" in binary, with complement "0101" in binary, which is 5 in base-10.
