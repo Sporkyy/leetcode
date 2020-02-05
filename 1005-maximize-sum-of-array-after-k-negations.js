ab67a49ca6980d2906e8f14717d5fa00fbd9c242
Todd Sayre <captain.friendly@gmail.com>
2020-02-05 16:57:48 -0500

    Added problem descriptions

--- 1005-maximize-sum-of-array-after-k-negations.js
+++ 1005-maximize-sum-of-array-after-k-negations.js
@@ -1,6 +1,16 @@
 // 1005. Maximize Sum Of Array After K Negations
 // https://leetcode.com/problems/maximize-sum-of-array-after-k-negations/
 
+/*
+
+Given an array A of integers, we must modify the array in the following way:
+we choose an i and replace A[i] with -A[i], and we repeat this process
+K times in total.  (We may choose the same index i multiple times.)
+
+Return the largest possible sum of the array after modifying it in this way.
+
+*/
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 92 ms, faster than 27.50% of JavaScript online submissions
