c1510645501f690e8c5d0faf39280daeefe4bd25
Todd Sayre <captain.friendly@gmail.com>
2020-02-06 15:44:00 -0500

    Added problem description

--- 1002-find-common-characters.js
+++ 1002-find-common-characters.js
@@ -1,6 +1,16 @@
 // 1002. Find Common Characters
 // https://leetcode.com/problems/find-common-characters/
 
+/*
+Given an array A of strings made only from lowercase letters, return a list
+of all characters that show up in all strings within the list
+(including duplicates).  For example, if a character occurs 3 times in all
+strings but not 4 times, you need to include that character three times in
+the final answer.
+
+You may return the answer in any order.
+*/
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 84 ms, faster than 51.34% of JavaScript online submissions
