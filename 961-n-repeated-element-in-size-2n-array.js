cd75bf2460b045fe723069bf8ee3fd7805d10ee3
Todd Sayre <captain.friendly@gmail.com>
2020-02-12 13:33:29 -0500

    Converted test suite to Node.js assert

--- 961-n-repeated-element-in-size-2n-array.js
+++ 961-n-repeated-element-in-size-2n-array.js
@@ -4,14 +4,12 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 116 ms, faster than 16.78% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 // Memory Usage: 43.3 MB, less than 5.38% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 
-/**
- * @param {number[]} A
- * @return {number}
- */
+// /**
+//  * @param {number[]} A
+//  * @return {number}
+//  */
 // const repeatedNTimes = A =>
 //   Object.entries(
 //     A.reduce((acc, curr) => {
@@ -28,14 +26,12 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 96 ms, faster than 37.14% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 // Memory Usage: 42.3 MB, less than 5.38% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 
-/**
- * @param {number[]} A
- * @return {number}
- */
+// /**
+//  * @param {number[]} A
+//  * @return {number}
+//  */
 // const repeatedNTimes = A => {
 //   const counts = new Map(Array.from(new Set(A)).map(v => [v, 0]));
 //   for (let i = A.length - 1; 0 <= i; --i) {
@@ -49,14 +45,12 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 64 ms, faster than 100.00% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 // Memory Usage: 36.7 MB, less than 50.00% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 
-/**
- * @param {number[]} A
- * @return {number}
- */
+// /**
+//  * @param {number[]} A
+//  * @return {number}
+//  */
 // const repeatedNTimes = A => {
 //   const map = new Map();
 //   for (let i = 0; i < A.length; i++) {
@@ -73,14 +67,12 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 80 ms, faster than 41.63% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 // Memory Usage: 36.4 MB, less than 50.00% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 
-/**
- * @param {number[]} A
- * @return {number}
- */
+// /**
+//  * @param {number[]} A
+//  * @return {number}
+//  */
 // const repeatedNTimes = A => {
 //   for (let i = 0; i < A.length; i += 4) {
 //     if (A[i] === A[i + 1]) return A[i];
@@ -95,14 +87,12 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 56 ms, faster than 92.71% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 // Memory Usage: 36.5 MB, less than 43.75% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 
-/**
- * @param {number[]} A
- * @return {number}
- */
+// /**
+//  * @param {number[]} A
+//  * @return {number}
+//  */
 // const repeatedNTimes = A => {
 //   for (let i = 0; i < A.length; i += 4) {
 //     const [a, b, c, d] = [A[i], A[i + 1], A[i + 2], A[i + 3]];
@@ -118,9 +108,7 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 72 ms, faster than 53.53% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 // Memory Usage: 36.5 MB, less than 50.00% of JavaScript online submissions
-// for N-Repeated Element in Size 2N Array.
 
 /**
  * @param {number[]} A
@@ -143,40 +131,17 @@ const repeatedNTimes = A => {
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    input: [1, 2, 3, 3],
-    expected: 3,
-  },
+import { strictEqual } from 'assert';
 
-  {
-    input: [2, 1, 2, 5, 3, 2],
-    expected: 2,
-  },
+strictEqual(repeatedNTimes([1, 2, 3, 3]), 3);
 
-  {
-    input: [5, 1, 5, 2, 5, 3, 5, 4],
-    expected: 5,
-  },
+strictEqual(repeatedNTimes([2, 1, 2, 5, 3, 2]), 2);
 
-  {
-    input: [1, 3, 5, 2, 2, 2],
-    expected: 2,
-  },
+strictEqual(repeatedNTimes([5, 1, 5, 2, 5, 3, 5, 4]), 5);
 
-  {
-    input: [0, 0, 0, 0, 8, 0, 2, 3, 0, 1, 7, 6, 0, 4, 9, 0],
-    expected: 0,
-  },
-];
+strictEqual(repeatedNTimes([1, 3, 5, 2, 2, 2]), 2);
 
-for ({ input, expected } of tests) {
-  const result = repeatedNTimes(input);
-  const name = JSON.stringify(input);
-  if (result === expected) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected ${expected} but got ${result}`);
-  }
-}
+strictEqual(
+  repeatedNTimes([0, 0, 0, 0, 8, 0, 2, 3, 0, 1, 7, 6, 0, 4, 9, 0]),
+  0,
+);
