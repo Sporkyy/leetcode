dc148167148afe3656f8160af9ef7db539abd62b
Todd Sayre <captain.friendly@gmail.com>
2020-02-13 13:55:04 -0500

    Converted test suite to Node.js assert

--- 937-reorder-log-files.js
+++ 937-reorder-log-files.js
@@ -1,6 +1,27 @@
 // 937. Reorder Log Files
 // https://leetcode.com/problems/reorder-data-in-log-files/
 
+/*
+
+You have an array of logs. Each log is a space delimited string of words.
+
+For each log, the first word in each log is an alphanumeric identifier.
+ Then, either:
+
+- Each word after the identifier will consist only of lowercase letters, or;
+- Each word after the identifier will consist only of digits.
+- We will call these two varieties of logs letter-logs and digit-logs.
+  It is guaranteed that each log has at least one word after its identifier.
+
+Reorder the logs so that all of the letter-logs come before any digit-log.
+The letter-logs are ordered lexicographically ignoring identifier,
+with the identifier used in case of ties.
+The digit-logs should be put in their original order.
+
+Return the final order of the logs.
+
+*/
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 68 ms, faster than 87.60% of JavaScript online submissions
