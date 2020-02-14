96d56ef763f0d831b27cf64aadee87b3cca0da9b
Todd Sayre <captain.friendly@gmail.com>
2020-02-14 08:45:45 -0500

    Converted test suite to Node.js assert

--- 933-number-of-recent-calls.js
+++ 933-number-of-recent-calls.js
@@ -1,14 +1,31 @@
 // 933. Number of Recent Calls
 // https://leetcode.com/problems/number-of-recent-calls/
 
+/*
+
+Write a class RecentCounter to count recent requests.
+
+It has only one method: ping(int t), where `t` represents some time
+in milliseconds.
+
+Return the number of pings that have been made from 3000 milliseconds
+ago until now.
+
+Any ping with time in [t - 3000, t] will count, including the current ping.
+
+It is guaranteed that every call to ping uses a strictly larger value
+of `t` than before.
+
+*/
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // var RecentCounter = function() {};
 
-/**
- * @param {number} t
- * @return {number}
- */
+// /**
+//  * @param {number} t
+//  * @return {number}
+//  */
 // RecentCounter.prototype.ping = function(t) {};
 
 /**
@@ -20,18 +37,16 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 1224 ms, faster than 5.11% of JavaScript online submissions
-// for Number of Recent Calls.
 // Memory Usage: 69.8 MB, less than 20.00% of JavaScript online submissions
-// for Number of Recent Calls.
 
 // const RecentCounter = function() {
 //   this.pings = [];
 // };
 
-/**
- * @param {number} t
- * @return {number}
- */
+// /**
+//  * @param {number} t
+//  * @return {number}
+//  */
 // RecentCounter.prototype.ping = function(t) {
 //   this.pings.push(t);
 //   const length = this.pings.filter(ping => null !== ping && t - 3000 <= ping).length;
@@ -41,9 +56,28 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 360 ms, faster than 28.76% of JavaScript online submissions
-// for Number of Recent Calls.
 // Memory Usage: 72.5 MB, less than 10.00% of JavaScript online submissions
-// for Number of Recent Calls.
+
+// const RecentCounter = function() {
+//   this.pings = [];
+// };
+
+// /**
+//  * @param {number} t
+//  * @return {number}
+//  */
+// RecentCounter.prototype.ping = function(t) {
+//   if (null !== t) this.pings.push(t);
+//   let offset = 0;
+//   while (offset < this.pings.length && this.pings[offset] < t - 3000) offset++;
+//   this.pings = this.pings.slice(offset);
+//   return this.pings.length || null;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 352 ms, faster than 24.55% of JavaScript online submissions
+// Memory Usage: 72.8 MB, less than 10.00% of JavaScript online submissions
 
 const RecentCounter = function() {
   this.pings = [];
@@ -63,50 +97,32 @@ RecentCounter.prototype.ping = function(t) {
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    input: {
-      pings: ['RecentCounter', 'ping', 'ping', 'ping', 'ping'],
-      times: [null, 1, 100, 3001, 3002],
-    },
-    expected: [null, 1, 2, 3, 3],
-  },
+import { deepStrictEqual } from 'assert';
 
-  {
-    input: {
-      pings: ['RecentCounter', 'ping', 'ping', 'ping', 'ping', 'ping'],
-      times: [null, 642, 1849, 4921, 5936, 5957],
-    },
-    expected: [null, 1, 2, 1, 2, 3],
-  },
-];
-
-for ({
-  input: { pings, times },
-  expected,
-} of tests) {
-  let rc;
-  let result = [];
-
+const runTest = (pings, times) => {
+  let [result, rc] = [[]];
   for (let i = 0; i < pings.length; i++) {
-    const ping = pings[i];
-    // console.log(ping);
-    const time = times[i];
-    // console.log(time);
-    if ('RecentCounter' === ping) {
-      rc = new RecentCounter();
-      // console.log(rc);
-    }
-    // console.log(time);
+    const [ping, time] = [pings[i], times[i]];
+    if ('RecentCounter' === ping) rc = new RecentCounter();
     result.push(rc.ping(time));
   }
+  return result;
+};
 
-  const name = `pings = ${JSON.stringify(pings)}, times = ${JSON.stringify(times)}`;
+let pings, times;
 
-  if (JSON.stringify(expected) === JSON.stringify(result)) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected "${expected}", but got "${result}"`);
-  }
-}
+deepStrictEqual(
+  runTest(
+    ['RecentCounter', 'ping', 'ping', 'ping', 'ping'],
+    [null, 1, 100, 3001, 3002],
+  ),
+  [null, 1, 2, 3, 3],
+);
+
+deepStrictEqual(
+  runTest(
+    ['RecentCounter', 'ping', 'ping', 'ping', 'ping', 'ping'],
+    [null, 642, 1849, 4921, 5936, 5957],
+  ),
+  [null, 1, 2, 1, 2, 3],
+);
