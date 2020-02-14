a8520d4efe490f10142c26830f68ffbb027bfcfd
Todd Sayre <captain.friendly@gmail.com>
2020-02-14 11:15:15 -0500

    Added problem description

--- 908-smallest-range-i.js
+++ 908-smallest-range-i.js
@@ -1,6 +1,18 @@
 // 908. Smallest Range I
 // https://leetcode.com/problems/smallest-range-i/
 
+/*
+
+Given an array A of integers, for each integer A[i] we may choose any x with
+-K <= x <= K, and add x to A[i].
+
+After this process, we have some array B.
+
+Return the smallest possible difference between the maximum value of B and
+the minimum value of B.
+
+*/
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 60 ms, faster than 65.08% of JavaScript online submissions
