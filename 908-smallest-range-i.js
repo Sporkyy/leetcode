0c2eaa4722fcc471c0b948e36071ac0e23a9c669
Todd Sayre <captain.friendly@gmail.com>
2020-02-14 10:56:55 -0500

    Converted test suite to Node.js assert

--- 908-smallest-range-i.js
+++ 908-smallest-range-i.js
@@ -4,53 +4,22 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 60 ms, faster than 65.08% of JavaScript online submissions
-// for Smallest Range I.
 // Memory Usage: 35.2 MB, less than 100.00% of JavaScript online submissions
-// for Smallest Range I.
 
 /**
  * @param {number[]} A
  * @param {number} K
  * @return {number}
  */
-const smallestRangeI = (A, K) => Math.max(Math.max(...A) - K - (Math.min(...A) + K), 0);
+const smallestRangeI = (A, K) =>
+  Math.max(Math.max(...A) - K - (Math.min(...A) + K), 0);
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    input: {
-      A: [1],
-      K: 0,
-    },
-    expected: 0,
-  },
-  {
-    input: {
-      A: [0, 10],
-      K: 2,
-    },
-    expected: 6,
-  },
-  {
-    input: {
-      A: [1, 3, 6],
-      K: 3,
-    },
-    expected: 0,
-  },
-];
+import { strictEqual } from 'assert';
 
-for ({
-  input: { A, K },
-  expected,
-} of tests) {
-  const name = `A: ${A}, K: ${K}`;
-  const result = smallestRangeI(A, K);
-  if (expected === result) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected "${expected}", but got "${result}"`);
-  }
-}
+strictEqual(smallestRangeI([1], 0), 0);
+
+strictEqual(smallestRangeI([0, 10], 2), 6);
+
+strictEqual(smallestRangeI([1, 3, 6], 3), 0);
