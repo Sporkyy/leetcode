1b8cdb23c81d0c2041a67d8b9116f9ac6b7e6de6
Todd Sayre <captain.friendly@gmail.com>
2020-02-14 14:24:13 -0500

    Optimized solution

--- 905-sort-array-by-parity.js
+++ 905-sort-array-by-parity.js
@@ -3,52 +3,45 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// I don't know what's wrong with this
-// const sortArrayByParity = A => A.sort((n1, n2) => (0 === n1 % 2 ? (0 === n2 % 2 ? -1 : 0) : 1));
-
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
 // Runtime: 68 ms, faster than 100.00% of JavaScript online submissions
-// for Sort Array By Parity.
 // Memory Usage: 37 MB, less than 69.01% of JavaScript online submissions
-// for Sort Array By Parity.
 
-/**
- * Filter+Concat
- *
- * @param {number[]} A
- * @return {number[]}
- */
-// const sortArrayByParity = A => A.filter(n => 0 === n % 2).concat(A.filter(n => 1 === n % 2));
+// /**
+//  * Filter+Concat
+//  *
+//  * @param {number[]} A
+//  * @return {number[]}
+//  */
+// const sortArrayByParity = A =>
+//   A.filter(n => 0 === n % 2).concat(A.filter(n => 1 === n % 2));
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 84 ms, faster than 92.13% of JavaScript online submissions
-// for Sort Array By Parity.
 // Memory Usage: 37.4 MB, less than 32.65% of JavaScript online submissions
-// for Sort Array By Parity.
 
-/**
- * Filter+Spread
- *
- * @param {number[]} A
- * @return {number[]}
- */
-// const sortArrayByParity = A => [...A.filter(n => 0 === n % 2), ...A.filter(n => 1 === n % 2)];
+// /**
+//  * Filter+Spread
+//  *
+//  * @param {number[]} A
+//  * @return {number[]}
+//  */
+// const sortArrayByParity = A => [
+//   ...A.filter(n => 0 === n % 2),
+//   ...A.filter(n => 1 === n % 2),
+// ];
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 76 ms, faster than 100.00% of JavaScript online submissions
-// for Sort Array By Parity.
 // Memory Usage: 37.4 MB, less than 35.54% of JavaScript online submissions
-// for Sort Array By Parity.
 
-/**
- * Filter + Push
- *
- * @param {number[]} A
- * @return {number[]}
- */
+// /**
+//  * Filter + Push
+//  *
+//  * @param {number[]} A
+//  * @return {number[]}
+//  */
 // const sortArrayByParity = A => {
 //   const result = A.filter(n => 0 === n % 2);
 //   const tmp = A.filter(n => 1 === n % 2);
@@ -63,16 +56,14 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 76 ms, faster than 100.00% of JavaScript online submissions
-// for Sort Array By Parity.
 // Memory Usage: 37.4 MB, less than 35.54% of JavaScript online submissions
-// for Sort Array By Parity.
 
-/**
- * Loop+Push
- *
- * @param {number[]} A
- * @return {number[]}
- */
+// /**
+//  * Loop+Push
+//  *
+//  * @param {number[]} A
+//  * @return {number[]}
+//  */
 // const sortArrayByParity = A => {
 //   const result = [];
 //   const secondary = [];
@@ -94,122 +85,155 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 80 ms, faster than 59.67% of JavaScript online submissions
-// for Sort Array By Parity.
 // Memory Usage: 37.9 MB, less than 8.00% of JavaScript online submissions
-// for Sort Array By Parity.
 
-/**
- * @param {number[]} A
- * @return {number[]}
- */
-const sortArrayByParity = A => A.sort((a, b) => (a & 1) - (b & 1));
+// /**
+//  * @param {number[]} A
+//  * @return {number[]}
+//  */
+// const sortArrayByParity = A => A.sort((a, b) => (a & 1) - (b & 1));
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    input: [3, 1, 2, 4],
-    expected: [2, 4, 3, 1],
-  },
+// Runtime: 84 ms, faster than 49.38% of JavaScript online submissions
+// Memory Usage: 38 MB, less than 8.00% of JavaScript online submissions
 
-  {
-    input: [2, 3, 1, 4],
-    expected: [2, 4, 3, 1],
-  },
+// /**
+//  * @param {number[]} A
+//  * @return {number[]}
+//  */
+// const sortArrayByParity = A => A.sort((a, b) => (a % 2) - (b % 2));
 
-  {
-    input: [0, 2, 4, 6],
-    expected: [0, 2, 4, 6],
-  },
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-  {
-    input: [1, 3, 5, 7],
-    expected: [1, 3, 5, 7],
-  },
+// Runtime: 84 ms, faster than 49.38% of JavaScript online submissions
+// Memory Usage: 37.3 MB, less than 24.00% of JavaScript online submissions
 
-  {
-    input: [1, 3, 2, 5, 7],
-    expected: [2, 1, 3, 5, 7],
-  },
+// /**
+//  * @param {number[]} a
+//  * @return {number[]}
+//  */
+// const sortArrayByParity = a => {
+//   const res = [];
+//   for (const n of a)
+//     if (0 === n % 2) res.unshift(n);
+//     else res.push(n);
+//   return res;
+// };
 
-  {
-    input: [1, 2, 3, 4, 5],
-    expected: [2, 4, 1, 3, 5],
-  },
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-  {
-    input: [
-      4016,
-      2240,
-      402,
-      1600,
-      2540,
-      4665,
-      545,
-      2756,
-      3533,
-      2737,
-      4073,
-      2701,
-      4820,
-      3016,
-      2938,
-      2225,
-      2982,
-      1924,
-      2878,
-      1451,
-    ],
-    expected: [
-      4016,
-      2240,
-      402,
-      1600,
-      2540,
-      2756,
-      4820,
-      3016,
-      2938,
-      2982,
-      1924,
-      2878,
-      4665,
-      545,
-      3533,
-      2737,
-      4073,
-      2701,
-      2225,
-      1451,
-    ],
-  },
-];
+// Runtime: 1096 ms, faster than 5.67% of JavaScript online submissions
+// Memory Usage: 387.7 MB, less than 8.00% of JavaScript online submissions
 
-// Gave up on an FP solution for this
-const evensFirst = a => {
-  let firstOddIndex;
-  for (let i = 0; i < a.length; i++) {
-    // Avoid inadvertent cheating
-    if ('undefined' === typeof a[i]) return false;
-    const isEven = 0 === a[i] % 2;
-    if ('undefined' === typeof firstOddIndex && !isEven) {
-      firstOddIndex = i;
-    }
-    if ('undefined' !== typeof firstOddIndex && isEven && firstOddIndex < i) {
-      return false;
-    }
+// /**
+//  * @param {number[]} a
+//  * @return {number[]}
+//  */
+// const sortArrayByParity = (a, sorted = []) =>
+//   !a.length
+//     ? sorted
+//     : sortArrayByParity(
+//         a.slice(1),
+//         0 === a[0] % 2 ? [a[0]].concat(sorted) : sorted.concat(a[0]),
+//       );
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 76 ms, faster than 81.56% of JavaScript online submissions
+// Memory Usage: 37.8 MB, less than 8.00% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} a
+//  * @return {number[]}
+//  */
+// const sortArrayByParity = a => {
+//   let [l, r] = [0, a.length - 1];
+//   while (l < r) {
+//     if (1 === a[l] % 2 && 0 === a[r] % 2)
+//       [a[l], a[r], l, r] = [a[r], a[l], l + 1, r - 1];
+//     else if (0 === a[l] % 2) l++;
+//     else r--;
+//   }
+//   return a;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 72 ms, faster than 92.02% of JavaScript online submissions
+// Memory Usage: 37.9 MB, less than 8.00% of JavaScript online submissions
+
+/**
+ * @param {number[]} a
+ * @return {number[]}
+ */
+const sortArrayByParity = a => {
+  let [l, r] = [0, a.length - 1];
+  while (l < r) {
+    if (1 === a[l] % 2 && 0 === a[r] % 2)
+      [a[l], a[r], l, r] = [a[r], a[l], l + 1, r - 1];
+    if (0 === a[l] % 2) l++;
+    if (1 === a[r] % 2) r--;
   }
-  return true;
+  return a;
 };
 
-for (let { input, expected } of tests) {
-  const result = sortArrayByParity(input);
-  const name = JSON.stringify(input);
-  if (expected.length === result.length && evensFirst(expected) && evensFirst(result)) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected "${expected}"`);
-    console.log(`But got "${result}"`);
-  }
-}
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+const checkParity = a => {
+  let i = 0;
+  while (0 === a[i] % 2 && i < a.length) i++;
+  while (1 === a[i] % 2 && i < a.length) i++;
+  return a.length === i;
+};
+
+// console.log(checkParity([1, 2, 3]));
+// console.log(checkParity([1, 3, 5]));
+// console.log(checkParity([2, 1, 3]));
+// console.log(checkParity([4, 0, 3]));
+// console.log(checkParity([1, 0, 3]));
+// console.log(checkParity([4, 2, 6, 0, 1, 3, 5]));
+// console.log(checkParity([1, 4, 2, 6, 0, 1, 3, 5]));
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+import { ok } from 'assert';
+
+ok(checkParity(sortArrayByParity([3, 1, 2, 4])));
+
+ok(checkParity(sortArrayByParity([2, 3, 1, 4])));
+
+ok(checkParity(sortArrayByParity([0, 2, 4, 6])));
+
+ok(checkParity(sortArrayByParity([1, 3, 5, 7])));
+
+ok(checkParity(sortArrayByParity([1, 3, 2, 5, 7])));
+
+ok(checkParity(sortArrayByParity([1, 2, 3, 4, 5])));
+
+ok(
+  checkParity(
+    sortArrayByParity([
+      4016,
+      2240,
+      402,
+      1600,
+      2540,
+      4665,
+      545,
+      2756,
+      3533,
+      2737,
+      4073,
+      2701,
+      4820,
+      3016,
+      2938,
+      2225,
+      2982,
+      1924,
+      2878,
+      1451,
+    ]),
+  ),
+);
