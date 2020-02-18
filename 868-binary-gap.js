2455febec0cee3d7c81b43156ab9c0698b21917a
Todd Sayre <captain.friendly@gmail.com>
2020-02-18 14:34:44 -0500

    Converted test suite to Node.js assert

--- 868-binary-gap.js
+++ 868-binary-gap.js
@@ -3,15 +3,17 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// Runtime: 76 ms, faster than 35.42% of JavaScript online submissions
-// for Binary Gap.
-// Memory Usage: 33.9 MB, less than 14.29% of JavaScript online submissions
-// for Binary Gap.
+import { strictEqual } from 'assert';
 
-/**
- * @param {number} N
- * @return {number}
- */
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 76 ms, faster than 35.42% of JavaScript online submissions
+// Memory Usage: 33.9 MB, less than 14.29% of JavaScript online submissions
+
+// /**
+//  * @param {number} N
+//  * @return {number}
+//  */
 // const binaryGap = N => {
 //   inBinary = (N >>> 0).toString(2);
 //   const gaps = [0];
@@ -36,86 +38,52 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 48 ms, faster than 95.29% of JavaScript online submissions
-// for Binary Gap.
 // Memory Usage: 33.9 MB, less than 100.00% of JavaScript online submissions
-// for Binary Gap.
 
 /**
  * @param {number} N
  * @return {number}
  */
-// const binaryGap = N =>
-//   Math.max(
-//     0,
-//     ...N.toString(2)
-//       .split('1')
-//       .slice(1, -1)
-//       .map(gap => gap.length + 1),
-//   );
+const binaryGap = N =>
+  Math.max(
+    0,
+    ...N.toString(2)
+      .split('1')
+      .slice(1, -1)
+      .map(gap => gap.length + 1),
+  );
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 52 ms, faster than 86.67% of JavaScript online submissions
-// for Binary Gap.
 // Memory Usage: 34 MB, less than 100.00% of JavaScript online submissions
-// for Binary Gap.
 
-/**
- * @param {number} N
- * @return {number}
- */
-const binaryGap = N => {
-  let maxGap = 0;
-  bin = (N >>> 0).toString(2);
-  for (let p1 = 0, p2 = 1; p2 < bin.length; p2++) {
-    if ('1' === bin[p2]) {
-      if (maxGap < p2 - p1) maxGap = p2 - p1;
-      p1 = p2;
-    }
-  }
-  return maxGap;
-};
+// /**
+//  * @param {number} N
+//  * @return {number}
+//  */
+// const binaryGap = N => {
+//   let maxGap = 0;
+//   const bin = (N >>> 0).toString(2);
+//   for (let p1 = 0, p2 = 1; p2 < bin.length; p2++) {
+//     if ('1' === bin[p2]) {
+//       if (maxGap < p2 - p1) maxGap = p2 - p1;
+//       p1 = p2;
+//     }
+//   }
+//   return maxGap;
+// };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    input: 0,
-    expected: 0,
-  },
+strictEqual(binaryGap(0), 0);
 
-  {
-    input: 5,
-    expected: 2,
-  },
+strictEqual(binaryGap(5), 2);
 
-  {
-    input: 6,
-    expected: 1,
-  },
+strictEqual(binaryGap(6), 1);
 
-  {
-    input: 8,
-    expected: 0,
-  },
+strictEqual(binaryGap(8), 0);
 
-  {
-    input: 13,
-    expected: 2,
-  },
+strictEqual(binaryGap(13), 2);
 
-  {
-    input: 22,
-    expected: 2,
-  },
-];
-
-for ({ input, expected } of tests) {
-  const output = binaryGap(input);
-  if (expected === output) {
-    console.log(`✅ ${input}`);
-  } else {
-    console.log(`🔴 ${input}`);
-    console.log(`Expected "${expected}", but got "${output}"`);
-  }
-}
+strictEqual(binaryGap(22), 2);
