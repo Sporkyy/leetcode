5dba4f0c85fc16d2c24afd51a8f8714e137ffb19
Todd Sayre <captain.friendly@gmail.com>
2020-02-18 09:17:11 -0500

    Still don't know what's wrong with my solutions

--- 297-serialize-and-deserialize-binary-tree.js
+++ 297-serialize-and-deserialize-binary-tree.js
@@ -1,151 +1,149 @@
 // 297. Serialize and Deserialize Binary Tree
 // https://leetcode.com/problems/serialize-and-deserialize-binary-tree/
 
+import { deepStrictEqual } from 'assert';
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-
-/**
- * Encodes a tree to a single string.
- *
- * @param {TreeNode} root
- * @return {string}
- */
-// const serialize = root => {
-//   if (!root || !root.val) return '[]';
-//   const a = [];
-//   const q = [root];
-//   while (q.length) {
-//     const { val, left, right } = q.shift();
-//     a.push(null === val ? 'null' : val);
-//     if (left) q.push(left);
-//     if (right) q.push(right);
-//   }
-//   return `[${a.join(',')}]`;
-// };
-
-/**
- * Decodes your encoded data to tree.
- *
- * @param {string} data
- * @return {TreeNode}
- */
-// const deserialize = data => {
-//   data = data.slice(1, data.length - 1).split(',');
-//   if (1 === data.length && 0 === data[0].length) return new TreeNode(null);
-//   data = data.map(v => ('null' === v ? null : parseInt(v)));
-//   const nodes = data.map(val => new TreeNode(val));
-//   for (let i = 0; i < nodes.length; i++) {
-//     if (!nodes[i]) continue;
-//     nodes[i].left = nodes[i * 2 + 1] || null;
-//     nodes[i].right = nodes[i * 2 + 2] || null;
-//   }
-//   return nodes[0];
-// };
-
-/**
- * Your functions will be called as such:
- * deserialize(serialize(root));
- */
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 84 ms, faster than 88.99% of JavaScript online submissions
-// for Serialize and Deserialize Binary Tree.
 // Memory Usage: 44.2 MB, less than 17.65% of JavaScript online submissions
-// for Serialize and Deserialize Binary Tree.
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
+// /**
+//  * Encodes a tree to a single string.
+//  *
+//  * @param {TreeNode} root
+//  * @return {string}
+//  */
+// const serialize = root => JSON.stringify(root);
 
-/**
- * Encodes a tree to a single string.
- *
- * @param {TreeNode} root
- * @return {string}
- */
-const serialize = root => JSON.stringify(root);
-
-/**
- * Decodes your encoded data to tree.
- *
- * @param {string} data
- * @return {TreeNode}
- */
-const deserialize = data => JSON.parse(data);
-
-/**
- * Your functions will be called as such:
- * deserialize(serialize(root));
- */
+// /**
+//  * Decodes your encoded data to tree.
+//  *
+//  * @param {string} data
+//  * @return {TreeNode}
+//  */
+// const deserialize = data => JSON.parse(data);
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = this.right = null;
-    return this;
+/**
+ * @param {object} root
+ */
+const serialize = root => {
+  if (null === root.val && null === root.left && null === root.right)
+    return '[]';
+  const queue = [root];
+  const vals = [];
+  while (queue.length) {
+    const curr = queue.shift();
+    vals.push(null === curr || null === curr.val ? 'null' : curr.val);
+    // vals.push(null === curr || null === curr.val ? null : curr.val);
+    if (null !== curr) queue.push(curr.left, curr.right);
   }
-}
+  while ('null' === vals[vals.length - 1]) vals.pop();
+  // return `[${vals.join(',')}]`;
+  return `[${vals.toString()}]`;
+};
 
-class Tree {
-  constructor(...vals) {
-    this.root = null;
-    if (0 === vals.length) return this;
-    const nodes = vals.map(v => new TreeNode(v));
-    for (let i = 0; i < nodes.length; i++) {
-      if (null === nodes[i]) continue;
-      nodes[i].left = nodes[i * 2 + 1] || null;
-      nodes[i].right = nodes[i * 2 + 2] || null;
-    }
-    this.root = nodes[0];
-    return this;
+/**
+ * @param {string} data
+ */
+const deserialize = data => {
+  if ('[]' === data) return new TreeNode(null);
+  data = data
+    .slice(1)
+    .slice(0, data.length - 2)
+    .split(',');
+  for (let i = 0; i < data.length; i++) {
+    if ('null' === data[i]) data[i] = null;
+    else data[i] = Number(data[i]);
+    data[i] = new TreeNode(data[i]);
   }
-}
-
-const tests = [
-  {
-    input: [],
-  },
-
-  {
-    input: [1, 2, 3, null, null, 4, 5],
-  },
-];
-
-for (const { input } of tests) {
-  const name = JSON.stringify(input);
-  const root = new Tree(...input).root;
-
-  // console.log(root);
-
-  // console.log(serialize(root));
-  // console.log(deserialize(serialize(input)));
-
-  // console.log(serialize(deserialize(input)));
-  // console.log(serialize(deserialize(serialize(deserialize(input)))));
-
-  const r1 = serialize(root);
-  // console.log(r1);
-  // console.log(deserialize(serialize(root)));
-  const r2 = serialize(deserialize(serialize(root)));
-  // console.log(r2);
-
-  if (r1 === r2) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
+  for (let i = 0; i < data.length; i++) {
+    if (null === data[i]) continue;
+    if (data[i * 2 + 1]) data[i].left = data[i * 2 + 1];
+    if (data[i * 2 + 2]) data[i].right = data[i * 2 + 2];
   }
-}
+  // console.log(data);
+  return data[0];
+};
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+//    1
+//  /  \
+// 2    3
+// console.log(deserialize('[1,2,3]'));
+// console.log(serialize(deserialize('[1,2,3]')));
+
+// 1
+//  \
+//   2
+// console.log(deserialize('[1,null,2]'));
+// console.log(serialize(deserialize('[1,null,2]')));
+
+//      1
+//    /
+//   2
+//  /
+// 3
+// console.log(deserialize('[1,2,null,3]'));
+// console.log(serialize(deserialize('[1,2,null,3]')));
+
+//   1
+//  / \
+// 2   3
+//    / \
+//   4   5
+// console.log(deserialize('[1,2,3,null,null,4,5]'));
+// console.log(serialize(deserialize('[1,2,3,null,null,4,5]')));
+
+//                   1
+//             /          \
+//          10             n
+//        /    \         /  \
+//      9      11       n     8
+//     / \     / \    / \    / \
+//    n   8  12   n  7   n  n   13
+//    et. al.
+// console.log(
+//   serialize(
+//     deserialize(
+//       '[1,10,null,9,11,null,8,12,null,7,null,null,13,null,6,14,null,5,null,null,15,null,4,16,null,3,null,null,17,null,2,18,null,1,null,null,19]',
+//     ),
+//   ),
+// );
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+deepStrictEqual(deserialize(serialize(deserialize('[]'))), deserialize('[]'));
+
+deepStrictEqual(
+  deserialize(serialize(deserialize('[1,null,2]'))),
+  deserialize('[1,null,2]'),
+);
+
+deepStrictEqual(
+  deserialize(serialize(deserialize('[1,2,3,null,null,4,5]'))),
+  deserialize('[1,2,3,null,null,4,5]'),
+);
+
+deepStrictEqual(
+  deserialize(
+    serialize(
+      deserialize(
+        '[1,10,null,9,11,null,8,12,null,7,null,null,13,null,6,14,null,5,null,null,15,null,4,16,null,3,null,null,17,null,2,18,null,1,null,null,19]',
+      ),
+    ),
+  ),
+  deserialize(
+    '[1,10,null,9,11,null,8,12,null,7,null,null,13,null,6,14,null,5,null,null,15,null,4,16,null,3,null,null,17,null,2,18,null,1,null,null,19]',
+  ),
+);
