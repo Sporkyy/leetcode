4fccc272c8e97bd98d45d98975533de7fa842ca7
Todd Sayre <captain.friendly@gmail.com>
2020-02-19 15:51:51 -0500

    Added 1-line solution

--- 832-flipping-an-image.js
+++ 832-flipping-an-image.js
@@ -8,25 +8,33 @@ import { deepStrictEqual } from 'assert';
 // Runtime: 60 ms, faster than 93.97% of JavaScript online submissions
 // Memory Usage: 36.6 MB, less than 8.33% of JavaScript online submissions
 
+// /**
+//  * @param {number[][]} A
+//  * @return {number[][]}
+//  */
+// const flipAndInvertImage = A => {
+//   const [rows, cols] = [A.length, A[0].length];
+//   for (let i = 0; i < rows; i++) {
+//     if (1 === cols % 2) {
+//       A[i][Math.trunc(cols / 2)] = 0 === A[i][Math.trunc(cols / 2)] ? 1 : 0;
+//     }
+//     for (let j = 0; j <= cols / 2 - 1; j++) {
+//       [A[i][j], A[i][cols - j - 1]] = [
+//         0 === A[i][cols - j - 1] ? 1 : 0,
+//         0 === A[i][j] ? 1 : 0,
+//       ];
+//     }
+//   }
+//   return A;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
 /**
- * @param {number[][]} A
+ * @param {number[][]} a
  * @return {number[][]}
  */
-const flipAndInvertImage = A => {
-  const [rows, cols] = [A.length, A[0].length];
-  for (let i = 0; i < rows; i++) {
-    if (1 === cols % 2) {
-      A[i][Math.trunc(cols / 2)] = 0 === A[i][Math.trunc(cols / 2)] ? 1 : 0;
-    }
-    for (let j = 0; j <= cols / 2 - 1; j++) {
-      [A[i][j], A[i][cols - j - 1]] = [
-        0 === A[i][cols - j - 1] ? 1 : 0,
-        0 === A[i][j] ? 1 : 0,
-      ];
-    }
-  }
-  return A;
-};
+const flipAndInvertImage = a => a.map(r => r.reverse().map(n => n ^ 1));
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
