540a346dcadf56c93f91de4b8e132f968cef09c1
Todd Sayre <captain.friendly@gmail.com>
2020-02-19 15:36:47 -0500

    Converted test suite to Node.js assert

--- 832-flipping-an-image.js
+++ 832-flipping-an-image.js
@@ -1,12 +1,12 @@
 // 832. Flipping an Image
 // https://leetcode.com/problems/flipping-an-image/
 
+import { deepStrictEqual } from 'assert';
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// Runtime: 68 ms, faster than 54.50% of JavaScript online submissions
-// for Flipping an Image.
-// Memory Usage: 36.7 MB, less than 5.80% of JavaScript online submissions
-// for Flipping an Image.
+// Runtime: 60 ms, faster than 93.97% of JavaScript online submissions
+// Memory Usage: 36.6 MB, less than 8.33% of JavaScript online submissions
 
 /**
  * @param {number[][]} A
@@ -14,12 +14,15 @@
  */
 const flipAndInvertImage = A => {
   const [rows, cols] = [A.length, A[0].length];
-  for (i = 0; i < rows; i++) {
+  for (let i = 0; i < rows; i++) {
     if (1 === cols % 2) {
       A[i][Math.trunc(cols / 2)] = 0 === A[i][Math.trunc(cols / 2)] ? 1 : 0;
     }
-    for (j = 0; j <= cols / 2 - 1; j++) {
-      [A[i][j], A[i][cols - j - 1]] = [0 === A[i][cols - j - 1] ? 1 : 0, 0 === A[i][j] ? 1 : 0];
+    for (let j = 0; j <= cols / 2 - 1; j++) {
+      [A[i][j], A[i][cols - j - 1]] = [
+        0 === A[i][cols - j - 1] ? 1 : 0,
+        0 === A[i][j] ? 1 : 0,
+      ];
     }
   }
   return A;
@@ -27,24 +30,30 @@ const flipAndInvertImage = A => {
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    input: [[1, 1, 0], [1, 0, 1], [0, 0, 0]],
-    expected: [[1, 0, 0], [0, 1, 0], [1, 1, 1]],
-  },
+deepStrictEqual(
+  flipAndInvertImage([
+    [1, 1, 0],
+    [1, 0, 1],
+    [0, 0, 0],
+  ]),
+  [
+    [1, 0, 0],
+    [0, 1, 0],
+    [1, 1, 1],
+  ],
+);
 
-  {
-    input: [[1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 1, 1], [1, 0, 1, 0]],
-    expected: [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 1], [1, 0, 1, 0]],
-  },
-];
-
-for (let { input, expected } of tests) {
-  const result = flipAndInvertImage(input);
-  const name = JSON.stringify(input);
-  if (JSON.stringify(expected) === JSON.stringify(result)) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-  }
-}
+deepStrictEqual(
+  flipAndInvertImage([
+    [1, 1, 0, 0],
+    [1, 0, 0, 1],
+    [0, 1, 1, 1],
+    [1, 0, 1, 0],
+  ]),
+  [
+    [1, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 1],
+    [1, 0, 1, 0],
+  ],
+);
