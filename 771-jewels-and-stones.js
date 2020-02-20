ee5beb4f22359e9c53f68272744f8ace6f7d39ee
Todd Sayre <captain.friendly@gmail.com>
2020-02-20 16:47:57 -0500

    Added 1-line solution

--- 771-jewels-and-stones.js
+++ 771-jewels-and-stones.js
@@ -1,13 +1,28 @@
 // 771. Jewels and Stones
 // https://leetcode.com/problems/jewels-and-stones/
 
+/*
+
+You're given strings J representing the types of stones that are jewels,
+and S representing the stones you have.
+Each character in S is a type of stone you have.
+You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are
+letters. Letters are case sensitive, so "a" is considered a different type
+of stone from "A".
+
+*/
+
+import { strictEqual } from 'assert';
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-/**
- * @param {string} J
- * @param {string} S
- * @return {number}
- */
+// /**
+//  * @param {string} J
+//  * @param {string} S
+//  * @return {number}
+//  */
 // const numJewelsInStones = (J, S) => {
 //   const occurrences = (n, h) => h.split('').filter(s => s === n).length;
 //   if (0 === J.length || 0 === S.length) {
@@ -41,11 +56,11 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-/**
- * @param {string} J
- * @param {string} S
- * @return {number}
- */
+// /**
+//  * @param {string} J
+//  * @param {string} S
+//  * @return {number}
+//  */
 // const numJewelsInStones = (J, S) => {
 //   let count = 0;
 //   for (let i = 0; i < J.length; i++) {
@@ -59,15 +74,13 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 64 ms, faster than 80.29% of JavaScript online submissions
-// for Jewels and Stones.
 // Memory Usage: 33.9 MB, less than 65.18% of JavaScript online submissions
-// for Jewels and Stones.
 
-/**
- * @param {string} J
- * @param {string} S
- * @return {number}
- */
+// /**
+//  * @param {string} J
+//  * @param {string} S
+//  * @return {number}
+//  */
 // const numJewelsInStones = (J, S) => {
 //   let count = 0;
 //   for (let i = S.length - 1; 0 <= i; --i) {
@@ -81,15 +94,13 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 60 ms, faster than 100.00% of JavaScript online submissions
-// for Jewels and Stones.
 // Memory Usage: 33.8 MB, less than 92.71% of JavaScript online submissions
-// for Jewels and Stones.
 
-/**
- * @param {string} J
- * @param {string} S
- * @return {number}
- */
+// /**
+//  * @param {string} J
+//  * @param {string} S
+//  * @return {number}
+//  */
 // const numJewelsInStones = (J, S) => {
 //   const jLen = J.length;
 //   const sLen = S.length;
@@ -104,15 +115,13 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 64 ms, faster than 80.29% of JavaScript online submissions
-// for Jewels and Stones.
 // Memory Usage: 33.8 MB, less than 90.08% of JavaScript online submissions
-// for Jewels and Stones.
 
-/**
- * @param {string} J
- * @param {string} S
- * @return {number}
- */
+// /**
+//  * @param {string} J
+//  * @param {string} S
+//  * @return {number}
+//  */
 // function numJewelsInStones(J, S) {
 //   let count = 0;
 //   for (let i = S.length - 1; 0 <= i; --i) {
@@ -124,15 +133,13 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 60 ms, faster than 100.00% of JavaScript online submissions
-// for Jewels and Stones.
 // Memory Usage: 33.8 MB, less than 81.17% of JavaScript online submissions
-// for Jewels and Stones.
 
-/**
- * @param {string} J
- * @param {string} S
- * @return {number}
- */
+// /**
+//  * @param {string} J
+//  * @param {string} S
+//  * @return {number}
+//  */
 // function numJewelsInStones(J, S) {
 //   if (0 === J.length || 0 === S.length) return 0;
 //   let count = 0;
@@ -145,15 +152,13 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 56 ms, faster than 80.72% of JavaScript online submissions
-// for Jewels and Stones.
 // Memory Usage: 33.8 MB, less than 94.45% of JavaScript online submissions
-// for Jewels and Stones.
 
-/**
- * @param {string} J
- * @param {string} S
- * @return {number}
- */
+// /**
+//  * @param {string} J
+//  * @param {string} S
+//  * @return {number}
+//  */
 // function numJewelsInStones(J, S) {
 //   let count = 0;
 //   let i = S.length;
@@ -163,11 +168,11 @@
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-/**
- * @param {string} J
- * @param {string} S
- * @return {number}
- */
+// /**
+//  * @param {string} J
+//  * @param {string} S
+//  * @return {number}
+//  */
 // function numJewelsInStones(J, S) {
 //   const dict = new Map();
 //   for (stone of S) {
@@ -182,97 +187,46 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 88 ms, faster than 6.92% of JavaScript online submissions
-// for Jewels and Stones.
 // Memory Usage: 34.7 MB, less than 30.23% of JavaScript online submissions
-// for Jewels and Stones.
+
+// /**
+//  * @param {string} J
+//  * @param {string} S
+//  * @return {number}
+//  */
+// function numJewelsInStones(J, S) {
+//   const a = new Array(127).fill(0);
+//   for (let i = 0; i < S.length; i++) a[S.charCodeAt(i)]++;
+//   let result = 0;
+//   for (let i = 0; i < J.length; i++) result += a[J.charCodeAt(i)];
+//   return result;
+// }
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 64 ms, faster than 44.14% of JavaScript online submissions
+// Memory Usage: 34.1 MB, less than 64.18% of JavaScript online submissions
 
 /**
  * @param {string} J
  * @param {string} S
  * @return {number}
  */
-function numJewelsInStones(J, S) {
-  const a = new Array(127).fill(0);
-  for (let i = 0; i < S.length; i++) a[S.charCodeAt(i)]++;
-  let result = 0;
-  for (let i = 0; i < J.length; i++) result += a[J.charCodeAt(i)];
-  return result;
-}
+const numJewelsInStones = (J, S) =>
+  [...S].reduce((acc, curr) => (J.includes(curr) ? acc + 1 : acc), 0);
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    input: {
-      J: 'aA',
-      S: 'aAAbbbb',
-    },
-    expected: 3,
-  },
+strictEqual(numJewelsInStones('aA', 'aAAbbbb'), 3);
 
-  {
-    input: {
-      J: 'z',
-      S: 'ZZ',
-    },
+strictEqual(numJewelsInStones('z', 'ZZ'), 0);
 
-    expected: 0,
-  },
+strictEqual(numJewelsInStones('', 'a'), 0);
 
-  {
-    input: {
-      J: '',
-      S: 'a',
-    },
+strictEqual(numJewelsInStones('a', ''), 0);
 
-    expected: 0,
-  },
+strictEqual(numJewelsInStones('', ''), 0);
 
-  {
-    input: {
-      J: 'a',
-      S: '',
-    },
+strictEqual(numJewelsInStones('A', 'AAA'), 3);
 
-    expected: 0,
-  },
-
-  {
-    input: {
-      J: '',
-      S: '',
-    },
-
-    expected: 0,
-  },
-
-  {
-    input: {
-      J: 'A',
-      S: 'AAA',
-    },
-    expected: 3,
-  },
-
-  {
-    input: {
-      J: 'z',
-      S: 'zzz',
-    },
-    expected: 3,
-  },
-];
-
-for ({
-  input: { J, S },
-  expected,
-} of tests) {
-  const output = numJewelsInStones(J, S);
-  const name = `J = "${J}" S = "${S}"`;
-  if (expected === output) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected "${expected}", but got "${output}"`);
-  }
-}
+strictEqual(numJewelsInStones('z', 'zzz'), 3);
