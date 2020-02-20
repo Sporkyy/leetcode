4cadbdb202e270430a39b6d203b8152aa596c5fa
Todd Sayre <captain.friendly@gmail.com>
2020-02-20 15:13:43 -0500

    Added 1-line Solution

--- 804-unique-morse-code-words.js
+++ 804-unique-morse-code-words.js
@@ -1,17 +1,67 @@
 // 804. Unique Morse Code Words
 // https://leetcode.com/problems/unique-morse-code-words/
 
+/*
+
+International Morse Code defines a standard encoding where each letter
+is mapped to a series of dots and dashes, as follows:
+  - "a" maps to ".-"
+  - "b" maps to "-..."
+  - "c" maps to "-.-."
+and so on.
+
+For convenience, the full table for the 26 letters of the English alphabet
+is given below:
+
+[
+  ".-",
+  "-...",
+  "-.-.",
+  "-..",
+  ".",
+  "..-.",
+  "--.",
+  "....",
+  "..",
+  ".---",
+  "-.-",
+  ".-..",
+  "--",
+  "-.",
+  "---",
+  ".--.",
+  "--.-",
+  ".-.",
+  "...",
+  "-",
+  "..-",
+  "...-",
+  ".--",
+  "-..-",
+  "-.--",
+  "--.."
+]
+
+Now, given a list of words, each word can be written as a concatenation
+of the Morse code of each letter. For example, "cba" can be written
+as "-.-..--...", (which is the concatenation "-.-." + "-..." + ".-").
+We'll call such a concatenation, the transformation of a word.
+
+Return the number of different transformations among all words we have.
+
+*/
+
+import { strictEqual } from 'assert';
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 96 ms, faster than 5.78% of JavaScript online submissions
-// for Unique Morse Code Words.
 // Memory Usage: 35.9 MB, less than 25.00% of JavaScript online submissions
-// for Unique Morse Code Words.
 
-/**
- * @param {string[]} words
- * @return {number}
- */
+// /**
+//  * @param {string[]} words
+//  * @return {number}
+//  */
 // var uniqueMorseRepresentations = function(words) {
 //   const codes = [
 //     '.-',
@@ -52,9 +102,7 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 60 ms, faster than 100.00% of JavaScript online submissions
-// for Unique Morse Code Words.
 // Memory Usage: 35.4 MB, less than 55.00% of JavaScript online submissions
-// for Unique Morse Code Words.
 
 // const codes = [
 //   '.-',
@@ -108,14 +156,12 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 52 ms, faster than 94.39% of JavaScript online submissions
-// for Unique Morse Code Words.
 // Memory Usage: 35.6 MB, less than 57.72% of JavaScript online submissions
-// for Unique Morse Code Words.
 
-/**
- * @param {string[]} words
- * @return {number}
- */
+// /**
+//  * @param {string[]} words
+//  * @return {number}
+//  */
 // const uniqueMorseRepresentations = words => {
 //   const codes = [
 //     '.-',
@@ -147,7 +193,8 @@
 //   ];
 //   m = new Array(words.length).fill('');
 //   for (i = 0; i < words.length; i++)
-//     for (j = 0; j < words[i].length; j++) m[i] += codes[words[i].charCodeAt(j) - 97];
+//     for (j = 0; j < words[i].length; j++)
+//       m[i] += codes[words[i].charCodeAt(j) - 97];
 //   o = {};
 //   for (const word of m) o[word] = 0;
 //   return Object.keys(o).length;
@@ -156,80 +203,102 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 56 ms, faster than 84.77% of JavaScript online submissions
-// for Unique Morse Code Words.
 // Memory Usage: 34.9 MB, less than 96.75% of JavaScript online submissions
-// for Unique Morse Code Words.
+
+// /**
+//  * @param {string[]} words
+//  * @return {number}
+//  */
+// const uniqueMorseRepresentations = words => {
+//   const codes = [
+//     '.-',
+//     '-...',
+//     '-.-.',
+//     '-..',
+//     '.',
+//     '..-.',
+//     '--.',
+//     '....',
+//     '..',
+//     '.---',
+//     '-.-',
+//     '.-..',
+//     '--',
+//     '-.',
+//     '---',
+//     '.--.',
+//     '--.-',
+//     '.-.',
+//     '...',
+//     '-',
+//     '..-',
+//     '...-',
+//     '.--',
+//     '-..-',
+//     '-.--',
+//     '--..',
+//   ];
+//   const set = new Set();
+//   for (let i = 0, word; i < words.length && !(word = ''); set.add(word), i++)
+//     for (let j = 0; j < words[i].length; j++)
+//       word += codes[words[i].charCodeAt(j) - 97];
+//   return set.size;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 68 ms, faster than 33.87% of JavaScript online submissions
+// Memory Usage: 36.6 MB, less than 27.78% of JavaScript online submissions
 
 /**
  * @param {string[]} words
  * @return {number}
  */
-const uniqueMorseRepresentations = words => {
-  const codes = [
-    '.-',
-    '-...',
-    '-.-.',
-    '-..',
-    '.',
-    '..-.',
-    '--.',
-    '....',
-    '..',
-    '.---',
-    '-.-',
-    '.-..',
-    '--',
-    '-.',
-    '---',
-    '.--.',
-    '--.-',
-    '.-.',
-    '...',
-    '-',
-    '..-',
-    '...-',
-    '.--',
-    '-..-',
-    '-.--',
-    '--..',
-  ];
-  set = new Set();
-  for (let i = 0, word; i < words.length && !(word = ''); set.add(word), i++)
-    for (j = 0; j < words[i].length; j++) word += codes[words[i].charCodeAt(j) - 97];
-  return set.size;
-};
+const uniqueMorseRepresentations = words =>
+  new Set(
+    words.map(word =>
+      [...word]
+        .map(
+          c =>
+            [
+              /* a */ '.-',
+              /* b */ '-...',
+              /* c */ '-.-.',
+              /* d */ '-..',
+              /* e */ '.',
+              /* f */ '..-.',
+              /* g */ '--.',
+              /* h */ '....',
+              /* i */ '..',
+              /* j */ '.---',
+              /* k */ '-.-',
+              /* l */ '.-..',
+              /* m */ '--',
+              /* n */ '-.',
+              /* o */ '---',
+              /* p */ '.--.',
+              /* q */ '--.-',
+              /* r */ '.-.',
+              /* s */ '...',
+              /* t */ '-',
+              /* u */ '..-',
+              /* v */ '...-',
+              /* w */ '.--',
+              /* x */ '-..-',
+              /* y */ '-.--',
+              /* z */ '--..',
+            ][c.charCodeAt(0) - 97],
+        )
+        .join(''),
+    ),
+  ).size;
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    name: 'Example',
-    input: ['gin', 'zen', 'gig', 'msg'],
-    expected: 2,
-  },
-  {
-    name: 'Empty array',
-    input: [],
-    expected: 0,
-  },
-  {
-    name: 'One word',
-    input: ['xyzzy'],
-    expected: 1,
-  },
-  {
-    name: 'Words different lengths',
-    input: ['larry', 'moe', 'curly'],
-    expected: 3,
-  },
-];
+strictEqual(uniqueMorseRepresentations(['gin', 'zen', 'gig', 'msg']), 2);
 
-tests.forEach(({ name, input, expected }) => {
-  const result = uniqueMorseRepresentations(input);
-  if (expected === result) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected ${expected} but got ${result}`);
-  }
-});
+strictEqual(uniqueMorseRepresentations([]), 0);
+
+strictEqual(uniqueMorseRepresentations(['xyzzy']), 1);
+
+strictEqual(uniqueMorseRepresentations(['larry', 'moe', 'curly']), 3);
