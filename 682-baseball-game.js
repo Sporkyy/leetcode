802623b8e19f9d57ba7b5fb216d219f1bd9785c6
Todd Sayre <captain.friendly@gmail.com>
2020-02-21 14:31:21 -0500

    Added one-line recursive solution

--- 682-baseball-game.js
+++ 682-baseball-game.js
@@ -27,34 +27,57 @@ C (an operation, which isn't a round's score)
 // Runtime: 56 ms, faster than 77.42% of JavaScript online submissions
 // Memory Usage: 35.3 MB, less than 50.00% of JavaScript online submissions
 
+// /**
+//  * @param {string[]} ops
+//  * @return {number}
+//  */
+// const calPoints = ops => {
+//   const rounds = [];
+//   for (let i = 0; i < ops.length; i++) {
+//     const op = ops[i];
+//     switch (op) {
+//       case 'C':
+//         rounds.pop();
+//         break;
+//       case 'D':
+//         rounds.push(2 * rounds[rounds.length - 1]);
+//         break;
+//       case '+':
+//         rounds.push(rounds[rounds.length - 1] + rounds[rounds.length - 2]);
+//         break;
+//       default:
+//         rounds.push(parseInt(op, 10));
+//         break;
+//     }
+//   }
+
+//   // console.log(rounds);
+
+//   return rounds.reduce((acc, curr) => acc + curr, 0);
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 84 ms, faster than 12.33% of JavaScript online submissions
+// Memory Usage: 62.3 MB, less than 100.00% of JavaScript online submissions
+
 /**
  * @param {string[]} ops
  * @return {number}
  */
-const calPoints = ops => {
-  const rounds = [];
-  for (let i = 0; i < ops.length; i++) {
-    const op = ops[i];
-    switch (op) {
-      case 'C':
-        rounds.pop();
-        break;
-      case 'D':
-        rounds.push(2 * rounds[rounds.length - 1]);
-        break;
-      case '+':
-        rounds.push(rounds[rounds.length - 1] + rounds[rounds.length - 2]);
-        break;
-      default:
-        rounds.push(parseInt(op, 10));
-        break;
-    }
-  }
-
-  // console.log(rounds);
-
-  return rounds.reduce((acc, curr) => acc + curr, 0);
-};
+const calPoints = (ops, rounds = []) =>
+  !ops.length
+    ? rounds.reduce((acc, curr) => acc + curr, 0)
+    : calPoints(
+        ops.slice(1),
+        'C' === ops[0]
+          ? rounds.slice(0, rounds.length - 1)
+          : 'D' === ops[0]
+          ? rounds.concat(2 * rounds[rounds.length - 1])
+          : '+' === ops[0]
+          ? rounds.concat(rounds[rounds.length - 1] + rounds[rounds.length - 2])
+          : rounds.concat(parseInt(ops[0], 10)),
+      );
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
