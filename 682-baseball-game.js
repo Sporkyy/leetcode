ae65a13b734cfdeb4648499a2468f678373b3fa7
Todd Sayre <captain.friendly@gmail.com>
2020-02-21 14:08:45 -0500

    Converted test suite to Node.js assert

--- 682-baseball-game.js
+++ 682-baseball-game.js
@@ -1,27 +1,31 @@
 // 682. Baseball Game
 // https://leetcode.com/problems/baseball-game/
 
-// Integer (one round's score):
-//   Directly represents the number of points you get in this round.
-//
-// + (one round's score):
-//   Represents that the points you get in this round are the sum of
-//   the last two valid round's points.
-//
-// D (one round's score):
-//   Represents that the points you get in this round are the
-//   doubled data of the last valid round's points.
-//
-// C (an operation, which isn't a round's score)
-//   Represents the last valid round's points you get were invalid
-//   and should be removed.
+import { strictEqual } from 'assert';
+
+/*
+
+Integer (one round's score):
+  Directly represents the number of points you get in this round.
+
++ (one round's score):
+  Represents that the points you get in this round are the sum of
+  the last two valid round's points.
+
+D (one round's score):
+  Represents that the points you get in this round are the
+  doubled data of the last valid round's points.
+
+C (an operation, which isn't a round's score)
+  Represents the last valid round's points you get were invalid
+  and should be removed.
+
+*/
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 56 ms, faster than 77.42% of JavaScript online submissions
-// for Baseball Game.
 // Memory Usage: 35.3 MB, less than 50.00% of JavaScript online submissions
-// for Baseball Game.
 
 /**
  * @param {string[]} ops
@@ -54,25 +58,6 @@ const calPoints = ops => {
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    name: 'Example 1',
-    input: ['5', '2', 'C', 'D', '+'],
-    expected: 30,
-  },
-  {
-    name: 'Example 2',
-    input: ['5', '-2', '4', 'C', 'D', '9', '+', '+'],
-    expected: 27,
-  },
-];
+strictEqual(calPoints(['5', '2', 'C', 'D', '+']), 30);
 
-tests.forEach(({ name, input, expected }) => {
-  const result = calPoints(input);
-  if (expected === result) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected "${expected}", but got "${result}"`);
-  }
-});
+strictEqual(calPoints(['5', '-2', '4', 'C', 'D', '9', '+', '+']), 27);
