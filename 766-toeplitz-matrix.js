b9fce9cec5a7b9315052f23fd90e3d9553f7078e
Todd Sayre <captain.friendly@gmail.com>
2020-02-21 08:50:02 -0500

    Converted test suite to Node.js assert

--- 766-toeplitz-matrix.js
+++ 766-toeplitz-matrix.js
@@ -1,12 +1,21 @@
 // 766. Toeplitz Matrix
 // https://leetcode.com/problems/toeplitz-matrix/
 
+/*
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the
+same element.
+
+Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+
+*/
+
+import { ok } from 'assert';
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 68 ms, faster than 60.57% of JavaScript online submissions
-// for Toeplitz Matrix.
 // Memory Usage: 35.7 MB, less than 34.54% of JavaScript online submissions
-// for Toeplitz Matrix.
 
 /**
  * @param {number[][]} matrix
@@ -21,73 +30,74 @@ const isToeplitzMatrix = matrix => {
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  // 1 2 3 4
-  // 5 1 2 3
-  // 9 5 1 2
+// 1 2 3 4
+// 5 1 2 3
+// 9 5 1 2
 
-  {
-    input: [[1, 2, 3, 4], [5, 1, 2, 3], [9, 5, 1, 2]],
-    expected: true,
-  },
+ok(
+  isToeplitzMatrix([
+    [1, 2, 3, 4],
+    [5, 1, 2, 3],
+    [9, 5, 1, 2],
+  ]),
+);
 
-  // 1 2
-  // 2 2
+// 1 2
+// 2 2
 
-  {
-    input: [[1, 2], [2, 2]],
-    expected: false,
-  },
+ok(
+  !isToeplitzMatrix([
+    [1, 2],
+    [2, 2],
+  ]),
+);
 
-  //  1 2 3
-  //  2 1 5
-  //  5 7 1
-  //  3 6 9
+//  1 2 3
+//  2 1 5
+//  5 7 1
+//  3 6 9
 
-  {
-    input: [[1, 2, 3], [2, 1, 5], [5, 7, 1], [3, 6, 9]],
-    expected: false,
-  },
+ok(
+  !isToeplitzMatrix([
+    [1, 2, 3],
+    [2, 1, 5],
+    [5, 7, 1],
+    [3, 6, 9],
+  ]),
+);
 
-  // 11 74  0 93
-  // 40 11 74  7
+// 11 74  0 93
+// 40 11 74  7
 
-  {
-    input: [[11, 74, 0, 93], [40, 11, 74, 7]],
-    expected: false,
-  },
+ok(
+  !isToeplitzMatrix([
+    [11, 74, 0, 93],
+    [40, 11, 74, 7],
+  ]),
+);
 
-  // 1 2 3 4
-  // 9 1 2 3
-  // 9 9 1 2
-  // 9 9 9 1
+// 1 2 3 4
+// 9 1 2 3
+// 9 9 1 2
+// 9 9 9 1
 
-  {
-    input: [[1, 2, 3, 4], [9, 1, 2, 3], [9, 9, 1, 2], [9, 9, 9, 1]],
-    expected: true,
-  },
+ok(
+  isToeplitzMatrix([
+    [1, 2, 3, 4],
+    [9, 1, 2, 3],
+    [9, 9, 1, 2],
+    [9, 9, 9, 1],
+  ]),
+);
 
-  // 36 59 71 15 26 82 87
-  // 56 36 59 71 15 26 82
-  // 15  0 36 59 71 15 26
+// 36 59 71 15 26 82 87
+// 56 36 59 71 15 26 82
+// 15  0 36 59 71 15 26
 
-  {
-    input: [
-      [36, 59, 71, 15, 26, 82, 87],
-      [56, 36, 59, 71, 15, 26, 82],
-      [15, 0, 36, 59, 71, 15, 26],
-    ],
-    expected: false,
-  },
-];
-
-for ({ input, expected } of tests) {
-  const result = isToeplitzMatrix(input);
-  const name = JSON.stringify(input);
-  if (result === expected) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected "${expected}", but got "${result}"`);
-  }
-}
+ok(
+  !isToeplitzMatrix([
+    [36, 59, 71, 15, 26, 82, 87],
+    [56, 36, 59, 71, 15, 26, 82],
+    [15, 0, 36, 59, 71, 15, 26],
+  ]),
+);
