9cfb604983e95ad0f526291be1372ea7893def72
Todd Sayre <captain.friendly@gmail.com>
2020-02-25 10:10:15 -0500

    Added new solutions

--- 590-n-ary-tree-postorder-traversal.js
+++ 590-n-ary-tree-postorder-traversal.js
@@ -1,60 +1,85 @@
 // 590. N-ary Tree Postorder Traversal
 // https://leetcode.com/problems/n-ary-tree-postorder-traversal/
 
+import { deepStrictEqual } from 'assert';
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const postorderNested = root =>
-  !root
-    ? []
-    : root.children.length === 0
-    ? [root.val]
-    : [...root.children.map(c => postorderNested(c)), root.val];
+// Definition for a Node.
+function Node(val, children) {
+  this.val = val;
+  this.children = children;
+}
 
-const flattened = a =>
-  a.reduce(
-    (acc, curr) => (Array.isArray(curr) ? [...acc, ...flattened(curr)] : [...acc, curr]),
-    [],
-  );
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-/**
- * // Definition for a Node.
- * function Node(val,children) {
- *    this.val = val;
- *    this.children = children;
- * };
- */
+// Runtime: 72 ms, faster than 81.36% of JavaScript online submissions
+// Memory Usage: 37.4 MB, less than 100.00% of JavaScript online submissions
+
+// /**
+//  * @param {Node} root
+//  * @return {number[]}
+//  */
+// const postorder = root => {
+//   const [res, stk] = [[], [root]];
+//   while (
+//     stk.length &&
+//     null !== stk[0] &&
+//     'undefined' !== typeof stk[0] &&
+//     'undefined' !== typeof stk[0].val
+//   ) {
+//     const curr = stk.pop();
+//     if (curr.children && curr.children.length) stk.push(...curr.children);
+//     res.unshift(curr.val);
+//   }
+//   return res;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 80 ms, faster than 48.87% of JavaScript online submissions
+// Memory Usage: 37.6 MB, less than 100.00% of JavaScript online submissions
+
 /**
  * @param {Node} root
  * @return {number[]}
  */
-const postorder = root => flattened(postorderNested(root));
+const postorder = root => {
+  const [res, stk] = [[], [root]];
+  while (stk.length && null !== stk[0]) {
+    const curr = stk.pop();
+    if (curr.children && curr.children.length) stk.push(...curr.children);
+    res.push(curr.val);
+  }
+  return res.reverse();
+};
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-class Node {
-  constructor(val = null, children = []) {
-    this.val = val;
-    this.children = children;
-    return this;
-  }
-}
+deepStrictEqual(
+  postorder(
+    new Node(1, [
+      new Node(3, [new Node(5), new Node(6)]),
+      new Node(2),
+      new Node(4),
+    ]),
+  ),
+  [5, 6, 3, 2, 4, 1],
+);
 
-const tests = [
-  {
-    name: 'Example',
-    input: new Node(1, [new Node(3, [new Node(5), new Node(6)]), new Node(2), new Node(4)]),
-    expected: [5, 6, 3, 2, 4, 1],
-  },
-];
+deepStrictEqual(
+  postorder(
+    new Node(1, [
+      new Node(2),
+      new Node(3, [
+        new Node(6, []),
+        new Node(7, [new Node(11, [new Node(14)])]),
+      ]),
+      new Node(4, [new Node(8, [new Node(12)])]),
+      new Node(5, [new Node(9, [new Node(13)]), new Node(10)]),
+    ]),
+  ),
+  [2, 6, 14, 11, 7, 3, 12, 8, 4, 13, 9, 10, 5, 1],
+);
 
-const areArraysEqual = (a1, a2) => a1.length === a2.length && a1.join() === a2.join();
-
-tests.forEach(({ name, input, expected }) => {
-  const result = postorder(input);
-  if (areArraysEqual(result, expected)) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected "${expected}", but got "${result}"`);
-  }
-});
+deepStrictEqual(postorder(null), []);
