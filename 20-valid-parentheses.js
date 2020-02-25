68e155e2297ec887a298837de4fef9b4dfb38bf8
Todd Sayre <captain.friendly@gmail.com>
2020-02-25 10:47:17 -0500

    Converted test suite to Node.js assert

--- 20-valid-parentheses.js
+++ 20-valid-parentheses.js
@@ -1,17 +1,17 @@
 // 20. Valid Parentheses
 // https://leetcode.com/problems/valid-parentheses/
 
+import { ok } from 'assert';
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 56 ms, faster than 87.22% of JavaScript online submissions
-// for Valid Parentheses.
 // Memory Usage: 33.9 MB, less than 67.84% of JavaScript online submissions
-// for Valid Parentheses.
 
-/**
- * @param {string} s
- * @return {boolean}
- */
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
 // var isValid = function(s) {
 //   const values = {
 //     '(': 1,
@@ -36,82 +36,95 @@
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 52 ms, faster than 94.35% of JavaScript online submissions
-// for Valid Parentheses.
 // Memory Usage: 35.1 MB, less than 25.74% of JavaScript online submissions
-// for Valid Parentheses.
+
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// var isValid = function(s) {
+//   const dict = {
+//     ']': '[',
+//     '}': '{',
+//     ')': '(',
+//   };
+//   const stack = [];
+//   for (let i = 0; i < s.length; i++) {
+//     if ('[' === s[i] || '{' === s[i] || '(' === s[i]) {
+//       stack.push(s[i]);
+//     } else {
+//       if (stack.pop() !== dict[s[i]]) return false;
+//     }
+//   }
+//   return 0 === stack.length;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 52 ms, faster than 87.47% of JavaScript online submissions
+// Memory Usage: 33.8 MB, less than 78.33% of JavaScript online submissions
+
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// const isValid = s => {
+//   if (1 === s.length % 2) return false;
+//   const dict = {
+//     ']': '[',
+//     '}': '{',
+//     ')': '(',
+//   };
+//   const stk = [];
+//   for (let i = 0; i < s.length; i++) {
+//     if ('[' === s[i] || '{' === s[i] || '(' === s[i]) stk.push(s[i]);
+//     else if (stk.pop() !== dict[s[i]]) return false;
+//   }
+//   return 0 === stk.length;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 52 ms, faster than 87.47% of JavaScript online submissions
+// Memory Usage: 37.1 MB, less than 6.67% of JavaScript online submissions
 
 /**
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-  const dict = {
-    ']': '[',
-    '}': '{',
-    ')': '(',
-  };
-  const stack = [];
-  for (i = 0; i < s.length; i++) {
-    if ('[' === s[i] || '{' === s[i] || '(' === s[i]) {
-      stack.push(s[i]);
-    } else {
-      if (stack.pop() !== dict[s[i]]) return false;
-    }
+const isValid = s => {
+  if (1 === s.length % 2) return false;
+  const dict = new Map([
+    [']', '['],
+    ['}', '{'],
+    [')', '('],
+  ]);
+  const stk = [];
+  for (const c of [...s]) {
+    if ([...dict.values()].includes(c)) stk.push(c);
+    else if (stk.pop() !== dict.get(c)) return false;
   }
-  return 0 === stack.length;
+  return 0 === stk.length;
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-tests = [
-  {
-    input: '()',
-    expected: true,
-  },
-  {
-    input: '()[]{}',
-    expected: true,
-  },
-  {
-    input: '(]',
-    expected: false,
-  },
-  {
-    input: '([])',
-    expected: true,
-  },
-  {
-    input: '([)]',
-    expected: false,
-  },
-  {
-    input: '{[]}',
-    expected: true,
-  },
-  {
-    input: '',
-    expected: true,
-  },
-  {
-    input: '({)',
-    expected: false,
-  },
-  {
-    input: '([{}]){[()]}',
-    expected: true,
-  },
-  {
-    input: '[])',
-    expected: false,
-  },
-];
+ok(isValid('()'));
 
-tests.forEach(({ input, expected }) => {
-  const output = isValid(input);
-  if (expected === output) {
-    console.log(`✅ ${input}`);
-  } else {
-    console.log(`🔴 ${input}`);
-    console.log(`Expected "${expected}" but got "${output}"`);
-  }
-});
+ok(isValid('()[]{}'));
+
+ok(!isValid('(]'));
+
+ok(isValid('([])'));
+
+ok(!isValid('([)]'));
+
+ok(isValid('{[]}'));
+
+ok(isValid(''));
+
+ok(!isValid('({)'));
+
+ok(isValid('([{}]){[()]}'));
+
+ok(!isValid('[])'));
