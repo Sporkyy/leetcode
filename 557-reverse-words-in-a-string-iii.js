30f037e91fe59a26906ca233a0fd5eb3b932fa4e
Todd Sayre <captain.friendly@gmail.com>
2020-02-26 08:58:31 -0500

    Converted test suite to Node.js assert

--- 557-reverse-words-in-a-string-iii.js
+++ 557-reverse-words-in-a-string-iii.js
@@ -1,12 +1,12 @@
 // 557. Reverse Words in a String III
 // https://leetcode.com/problems/reverse-words-in-a-string-iii/
 
+import { strictEqual } from 'assert';
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 84 ms, faster than 40.02% of JavaScript online submissions
-// for Reverse Words in a String III.
 // Memory Usage: 42.2 MB, less than 45.07% of JavaScript online submissions
-// for Reverse Words in a String III.
 
 /**
  * @param {string} s
@@ -25,19 +25,7 @@ const reverseWords = s =>
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    input: "Let's take LeetCode contest",
-    expected: "s'teL ekat edoCteeL tsetnoc",
-  },
-];
-
-tests.forEach(({ input, expected }) => {
-  const result = reverseWords(input);
-  if (expected === result) {
-    console.log(`✅ ${input}`);
-  } else {
-    console.log(`🔴 ${input}`);
-    console.log(`Expected "${expected}", but got "${result}"`);
-  }
-});
+strictEqual(
+  reverseWords("Let's take LeetCode contest"),
+  "s'teL ekat edoCteeL tsetnoc",
+);
