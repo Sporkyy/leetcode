ee556e8914e7fc95eca55f66212ded1b1c05a338
Todd Sayre <captain.friendly@gmail.com>
2020-02-26 09:51:30 -0500

    Converted test suite to Node.js assert

--- 559-maximum-depth-of-n-ary-tree.js
+++ 559-maximum-depth-of-n-ary-tree.js
@@ -1,33 +1,34 @@
 // 559. Maximum Depth of N-ary Tree
 // https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
 
+import { strictEqual } from 'assert';
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Definition for a Node.
+function Node(val, children) {
+  this.val = val;
+  this.children = children;
+}
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 612 ms, faster than 37.87% of JavaScript online submissions
-// for Maximum Depth of N-ary Tree.
 // Memory Usage: 81 MB, less than 28.95% of JavaScript online submissions
-// for Maximum Depth of N-ary Tree.
 
-/**
- * // Definition for a Node.
- * function Node(val,children) {
- *    this.val = val;
- *    this.children = children;
- * };
- */
 /**
  * @param {Node} root
  * @return {number}
  */
 // const maxDepth = root =>
-//   !root ? 0 : root.children.reduce((acc, curr) => Math.max(acc, maxDepth(curr)), 0) + 1;
+//   !root
+//     ? 0
+//     : root.children.reduce((acc, curr) => Math.max(acc, maxDepth(curr)), 0) + 1;
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-// Runtime: 612 ms, faster than 37.55% of JavaScript online submissions
-// for Maximum Depth of N-ary Tree.
-// Memory Usage: 80.4 MB, less than 52.63% of JavaScript online submissions
-// for Maximum Depth of N-ary Tree.
+// Runtime: 64 ms, faster than 87.04% of JavaScript online submissions
+// Memory Usage: 39.1 MB, less than 100.00% of JavaScript online submissions
 
 /**
  * @param {Node} root
@@ -38,7 +39,7 @@ const maxDepth = root => {
   let max = 0;
   const stk = [[root, 1]];
   while (stk.length) {
-    [curr, depth] = stk.pop();
+    const [curr, depth] = stk.pop();
     max = Math.max(max, depth);
     if (curr.children && 0 < curr.children.length) {
       stk.push(...curr.children.map(child => [child, depth + 1]));
@@ -49,96 +50,42 @@ const maxDepth = root => {
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-class Node {
-  constructor(val, children = []) {
-    this.val = val;
-    this.children = children;
-    return this;
-  }
-}
+const createTree = (n, ...vals) => {
+  if (!vals || (1 === vals.length && !vals[0])) return null;
+  const nodes = vals.map(v => new Node(v));
+  for (let i = 0; nodes[i * n + 1]; i++)
+    nodes[i].children = nodes.slice(i * n + 1, i * n + n);
+  return nodes[0];
+};
 
-class NaryTree {
-  constructor(n, ...vals) {
-    if (!vals || (1 === vals.length && !vals[0])) return null;
-    const nodes = vals.map(v => new Node(v));
-    // console.log(nodes);
-    for (let i = 0; nodes[i * n + 1]; i++) {
-      nodes[i].children = nodes.slice(i * n + 1, i * n + n);
-    }
-    // console.log(nodes);
-    this.root = nodes[0];
-  }
-}
+// 0 = 1, 2
+// =-=-=-=-=-=-=-
+// 1 = 3, 4
+// 2 = 5, 6
+// =-=-=-=-=-=-=-
+// 3 = 7
 
-const tests = [
-  // 0 = 1, 2
-  // =-=-=-=-=-=-=-
-  // 1 = 3, 4
-  // 2 = 5, 6
-  // =-=-=-=-=-=-=-
-  // 3 = 7
+strictEqual(maxDepth(createTree(2, 0, 1, 2, 3, 4, 5, 6, 7)), 4);
 
-  {
-    input: {
-      vals: '0,1,2,3,4,5,6,7',
-      n: 2,
-    },
-    expected: 4,
-  },
+// 0 =  1,  2,  3
+// =-=-=-=-=-=-=-
+// 1 =  4,  5,  6
+// 2 =  7,  8,  9
+// 3 = 10, 11, 12
+// =-=-=-=-=-=-=-
+// 4 = 13
 
-  // 0 =  1,  2,  3
-  // =-=-=-=-=-=-=-
-  // 1 =  4,  5,  6
-  // 2 =  7,  8,  9
-  // 3 = 10, 11, 12
-  // =-=-=-=-=-=-=-
-  // 4 = 13
+strictEqual(
+  maxDepth(createTree(3, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)),
+  4,
+);
 
-  {
-    input: {
-      vals: '0,1,2,3,4,5,6,7,8,9,10,11,12,13',
-      n: 3,
-    },
-    expected: 4,
-  },
+// 1 = 3, 2, 4
+// =-=-=-=-=-=-=-
+// 3 = 5, 6
 
-  // 1 = 3, 2, 4
-  // =-=-=-=-=-=-=-
-  // 3 = 5, 6
+strictEqual(maxDepth(createTree(3, 1, 3, 2, 4, 5, 6)), 3);
 
-  {
-    input: {
-      vals: '1,3,2,4,5,6',
-      n: 3,
-    },
-    expected: 3,
-  },
+strictEqual(maxDepth(createTree(2, null)), 0);
 
-  {
-    input: {
-      vals: '',
-      n: 2,
-    },
-    expected: 0,
-  },
-
-  {
-    input: {
-      vals: '1',
-      n: 2,
-    },
-    expected: 1,
-  },
-];
-
-tests.forEach(({ input: { vals, n }, expected }) => {
-  const root = new NaryTree(n, ...vals.split(',')).root;
-  const result = maxDepth(root);
-  const name = `${n} 👉 ${vals}`;
-  if (expected === result) {
-    console.log(`✅ ${name}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected "${expected}", but got "${result}"`);
-  }
-});
+strictEqual(maxDepth(createTree(2, 1)), 1);
