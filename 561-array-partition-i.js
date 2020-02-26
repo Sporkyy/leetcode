54daa6727ebe3521302b132e741968ed0807616b
Todd Sayre <captain.friendly@gmail.com>
2020-02-26 10:18:04 -0500

    Converted test suite to Node.js assert

--- 561-array-partition-i.js
+++ 561-array-partition-i.js
@@ -1,12 +1,12 @@
 // 561. Array Partition I
 // https://leetcode.com/problems/array-partition-i/
 
+import { strictEqual } from 'assert';
+
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 116 ms, faster than 70.08% of JavaScript online submissions
-// for Array Partition I.
 // Memory Usage: 39.1 MB, less than 49.68% of JavaScript online submissions
-// for Array Partition I.
 
 // const getN = a => a.length / 2;
 
@@ -14,21 +14,22 @@
 
 // const sortAsc = a => a.sort((n1, n2) => ufo(n1, n2));
 
-/**
- * @param {number[]} nums
- * @return {number}
- */
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
 // const arrayPairSum = nums => {
 //   const n = getN(nums);
-//   return sortAsc(nums).reduce((acc, curr, i) => (0 === i % 2 ? acc + curr : acc), 0);
+//   return sortAsc(nums).reduce(
+//     (acc, curr, i) => (0 === i % 2 ? acc + curr : acc),
+//     0,
+//   );
 // };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Runtime: 120 ms, faster than 56.49% of JavaScript online submissions
-// for Array Partition I.
 // Memory Usage: 40.4 MB, less than 10.83% of JavaScript online submissions
-// for Array Partition I.
 
 /**
  * @param {number[]} nums
@@ -42,45 +43,12 @@ const arrayPairSum = nums =>
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-const tests = [
-  {
-    name: 'Example 1',
-    input: [1, 4, 3, 2],
-    expected: 4,
-  },
+strictEqual(arrayPairSum([1, 4, 3, 2]), 4);
 
-  {
-    name: 'N = 1',
-    input: [2, 1],
-    expected: 1,
-  },
+strictEqual(arrayPairSum([2, 1]), 1);
 
-  {
-    name: 'N = 3',
-    input: [2, 2, 6, 6, 8, 8],
-    expected: 16,
-  },
+strictEqual(arrayPairSum([2, 2, 6, 6, 8, 8]), 16);
 
-  {
-    name: 'All the same',
-    input: [1, 1, 1, 1, 1, 1],
-    expected: 3,
-  },
+strictEqual(arrayPairSum([1, 1, 1, 1, 1, 1]), 3);
 
-  {
-    name: 'Negative numbers',
-    input: [-1, -4, -3, -2],
-    expected: -6,
-  },
-];
-
-tests.forEach(({ input, expected }) => {
-  const result = arrayPairSum(input);
-  let name = JSON.stringify(input);
-  if (result === expected) {
-    console.log(`✅ ${name} = ${result}`);
-  } else {
-    console.log(`🔴 ${name}`);
-    console.log(`Expected "${expected}", but got "${result}"`);
-  }
-});
+strictEqual(arrayPairSum([-1, -4, -3, -2]), -6);
