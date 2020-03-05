b96f3aa255582997327738e6c8fda40f29e3da39
Todd Sayre <captain.friendly@gmail.com>
2020-03-05 16:43:02 -0500

    Math solution

--- 1252-cells-with-odd-values-in-a-matrix.js
+++ 1252-cells-with-odd-values-in-a-matrix.js
@@ -19,6 +19,69 @@ import { strictEqual } from 'assert';
 // Runtime: 72 ms, faster than 26.62% of JavaScript online submissions
 // Memory Usage: 38.5 MB, less than 100.00% of JavaScript online submissions
 
+// /**
+//  * @param {number} n
+//  * @param {number} m
+//  * @param {number[][]} indices
+//  * @return {number}
+//  */
+// const oddCells = (n, m, indices) => {
+//   const matrix = new Array(n).fill().map(_ => new Array(m).fill(0));
+//   for (const [ri, ci] of indices)
+//     for (let i = 0; i < matrix.length; i++)
+//       for (let j = 0; j < matrix[i].length; j++) {
+//         if (ri === i) matrix[i][j]++;
+//         if (ci === j) matrix[i][j]++;
+//       }
+//   return matrix
+//     .reduce((acc, curr) => acc.concat(curr), [])
+//     .filter(n => 1 === n % 2).length;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 48 ms, faster than 98.11% of JavaScript online submissions
+// Memory Usage: 35.7 MB, less than 100.00% of JavaScript online submissions
+
+// /**
+//  * @param {number} n
+//  * @param {number} m
+//  * @param {number[][]} indices
+//  * @return {number}
+//  */
+// const oddCells = (n, m, indices) => {
+//   const [rows, cols] = [new Array(n).fill(0), new Array(m).fill(0)];
+//   for (const [ri, ci] of indices) [rows[ri]++, cols[ci]++];
+//   // console.log(rows.join(''));
+//   // console.log(cols.join(''));
+//   let res = 0;
+//   const rowOdds = rows.filter(n => 1 === n % 2).length;
+//   // console.log(rowOdds);
+//   const rowEvens = rows.length - rowOdds;
+//   // console.log(rowEvens);
+//   const colOdds = cols.filter(n => 1 === n % 2).length;
+//   // console.log(colOdds);
+//   const colEvens = cols.length - colOdds;
+//   // console.log(colEvens);
+//   return rowOdds * colEvens + rowEvens * colOdds;
+//   // rowEvens + colEvens -
+//   // console.log(res);
+//   // res += rows.filter(n => 1 === n % 2).length * cols.length;
+//   // console.log(res);
+//   // res += cols.filter(n => 1 === n % 2).length * rows.length;
+//   // console.log(res);
+//   // // res -= rows.filter(n => 1 === n % 2).length;
+//   // console.log(res);
+//   // // res -= cols.filter(n => 1 === n % 2).length;
+//   // console.log(res);
+//   return res;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 60 ms, faster than 69.81% of JavaScript online submissions
+// Memory Usage: 35.9 MB, less than 100.00% of JavaScript online submissions
+
 /**
  * @param {number} n
  * @param {number} m
@@ -26,16 +89,13 @@ import { strictEqual } from 'assert';
  * @return {number}
  */
 const oddCells = (n, m, indices) => {
-  const matrix = new Array(n).fill().map(_ => new Array(m).fill(0));
-  for (const [ri, ci] of indices)
-    for (let i = 0; i < matrix.length; i++)
-      for (let j = 0; j < matrix[i].length; j++) {
-        if (ri === i) matrix[i][j]++;
-        if (ci === j) matrix[i][j]++;
-      }
-  return matrix
-    .reduce((acc, curr) => acc.concat(curr), [])
-    .filter(n => 1 === n % 2).length;
+  const [rows, cols] = [new Array(n).fill(0), new Array(m).fill(0)];
+  for (const [ri, ci] of indices) [rows[ri]++, cols[ci]++];
+  const rowOdds = rows.filter(n => 1 === n % 2).length;
+  const rowEvens = rows.length - rowOdds;
+  const colOdds = cols.filter(n => 1 === n % 2).length;
+  const colEvens = cols.length - colOdds;
+  return rowOdds * colEvens + rowEvens * colOdds;
 };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
@@ -61,3 +121,13 @@ strictEqual(
   0,
 );
 // Explanation: Final matrix = [[2,2],[2,2]]. There is no odd number in the final matrix.
+
+strictEqual(oddCells(48, 37, [[40, 5]]), 83);
+
+strictEqual(
+  oddCells(2, 3, [
+    [0, 1],
+    [1, 1],
+  ]),
+  6,
+);
