442478873f3c80746d48004dde4b64c6f7934701
Todd Sayre <captain.friendly@gmail.com>
2020-03-10 10:35:45 -0400

    1-line solution

--- 1374-generate-a-string-with-characters-that-have-odd-counts.js
+++ 1374-generate-a-string-with-characters-that-have-odd-counts.js
@@ -22,22 +22,36 @@ import { ok } from 'assert';
 // Runtime: 48 ms, faster than 97.33% of JavaScript online submissions
 // Memory Usage: 33.8 MB, less than 100.00% of JavaScript online submissions
 
+// /**
+//  * @param {number} n
+//  * @return {string}
+//  */
+// const generateTheString = n => {
+//   const stock = new Array(26).fill(0);
+//   for (let i = 0; i < 26 && 0 < n; i++, n--) stock[i]++;
+//   stock[25] += n;
+//   if (0 === stock[25] % 2) {
+//     stock[24] += stock[25];
+//     stock[25] = 0;
+//   }
+//   return stock
+//     .map((n, idx) => String.fromCharCode(97 + idx).repeat(n))
+//     .join('');
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 56 ms, faster than 70.35% of JavaScript online submissions
+// Memory Usage: 33.8 MB, less than 100.00% of JavaScript online submissions
+
 /**
  * @param {number} n
  * @return {string}
  */
-const generateTheString = n => {
-  const stock = new Array(26).fill(0);
-  for (let i = 0; i < 26 && 0 < n; i++, n--) stock[i]++;
-  stock[25] += n;
-  if (0 === stock[25] % 2) {
-    stock[24] += stock[25];
-    stock[25] = 0;
-  }
-  return stock
-    .map((n, idx) => String.fromCharCode(97 + idx).repeat(n))
-    .join('');
-};
+const generateTheString = n =>
+  0 === n % 2
+    ? `a${'b'.repeat(n - 1)}`
+    : `a${1 < n ? 'b' : ''}${2 < n ? 'c'.repeat(n - 2) : ''}`;
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
@@ -46,10 +60,16 @@ console.log(generateTheString(2));
 console.log(generateTheString(3));
 console.log(generateTheString(4));
 console.log(generateTheString(5));
+console.log(generateTheString(6));
+console.log(generateTheString(7));
+console.log(generateTheString(8));
+console.log(generateTheString(9));
+console.log(generateTheString(10));
 console.log(generateTheString(26));
 console.log(generateTheString(27));
 console.log(generateTheString(28));
-console.log(generateTheString(500));
+console.log(generateTheString(29));
+console.log(generateTheString(30));
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
