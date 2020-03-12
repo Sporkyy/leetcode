b9dc13c5ef2d1132ff30790f75b34930fcb0e529
Todd Sayre <captain.friendly@gmail.com>
2020-03-12 16:21:32 -0400

    Even worse soution

--- 1170-compare-strings-by-frequency-of-the-smallest-character.js
+++ 1170-compare-strings-by-frequency-of-the-smallest-character.js
@@ -47,32 +47,82 @@ import { deepStrictEqual } from 'assert';
 // Runtime: 224 ms, faster than 35.51% of JavaScript online submissions
 // Memory Usage: 43.5 MB, less than 100.00% of JavaScript online submissions
 
-const memo = new Map();
+// const memo = new Map();
+
+// /**
+//  * @param {string} s
+//  * @returns
+//  */
+// const f = s => {
+//   if (memo.has(s)) return memo.get(s);
+//   const cnts = new Array(26).fill(0);
+//   for (const c of s) cnts[c.charCodeAt(0) - 97]++;
+//   for (const cnt of cnts)
+//     if (0 < cnt) {
+//       memo.set(s, cnt);
+//       return cnt;
+//     }
+// };
+
+// /**
+//  * @param {string[]} queries
+//  * @param {string[]} words
+//  * @return {number[]}
+//  */
+// const numSmallerByFrequency = (queries, words) => {
+//   [queries, words] = [queries.map(f), words.map(f)];
+//   return queries.map(q => words.filter(w => q < w).length);
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 236 ms, faster than 31.40% of JavaScript online submissions
+// Memory Usage: 42.5 MB, less than 100.00% of JavaScript online submissions
+
+// /**
+//  * @param {string} s
+//  * @returns
+//  */
+// const f = s => {
+//   const cnts = new Array(26).fill(0);
+//   for (const c of s) cnts[c.charCodeAt(0) - 97]++;
+//   for (const cnt of cnts) if (0 < cnt) return cnt;
+// };
+
+// /**
+//  * @param {string[]} queries
+//  * @param {string[]} words
+//  * @return {number[]}
+//  */
+// const numSmallerByFrequency = (queries, words) => {
+//   [queries, words] = [queries.map(f), words.map(f)];
+//   return queries.map(q => words.filter(w => q < w).length);
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 5312 ms, faster than 5.08% of JavaScript online submissions
+// Memory Usage: 67.5 MB, less than 100.00% of JavaScript online submissions
 
 /**
  * @param {string} s
  * @returns
  */
-const f = s => {
-  if (memo.has(s)) return memo.get(s);
-  const cnts = new Array(26).fill(0);
-  for (const c of s) cnts[c.charCodeAt(0) - 97]++;
-  for (const cnt of cnts)
-    if (0 < cnt) {
-      memo.set(s, cnt);
-      return cnt;
-    }
-};
+const f = s =>
+  s.match(
+    new RegExp(
+      `(${String.fromCharCode(Math.min(...[...s].map(c => c.charCodeAt(0))))})`,
+      'g',
+    ),
+  ).length;
 
 /**
  * @param {string[]} queries
  * @param {string[]} words
  * @return {number[]}
  */
-const numSmallerByFrequency = (queries, words) => {
-  [queries, words] = [queries.map(f), words.map(f)];
-  return queries.map(q => words.filter(w => q < w).length);
-};
+const numSmallerByFrequency = (queries, words) =>
+  queries.map(f).map(q => words.map(f).filter(w => q < w).length);
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
