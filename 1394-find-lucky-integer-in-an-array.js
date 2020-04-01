8f8d6970abc36c6e05ac5126da239d8cf5c0882c
Todd Sayre <captain.friendly@gmail.com>
2020-04-01 11:32:54 -0400

    It works but it's slow

---
+++ 1394-find-lucky-integer-in-an-array.js
@@ -0,0 +1,54 @@
+// 1394. Find Lucky Integer in an Array
+// https://leetcode.com/problems/find-lucky-integer-in-an-array/
+
+/*
+
+Given an array of integers arr, a lucky integer is an integer which has a
+frequency in the array equal to its value.
+
+Return a lucky integer in the array. If there are multiple lucky integers return
+the largest of them. If there is no lucky integer return -1.
+
+Constraints:
+- 1 <= arr.length <= 500
+- 1 <= arr[i] <= 500
+
+*/
+
+import { strictEqual } from 'assert';
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 116 ms, faster than 18.18% of JavaScript online submissions
+// Memory Usage: 35.4 MB, less than 100.00% of JavaScript online submissions
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+const findLucky = arr => {
+  const cnts = new Array(arr.length + 1).fill(0);
+  for (const num of arr) if (0 < num && num < arr.length + 1) cnts[num]++;
+  for (let i = cnts.length - 1; 0 < i; i--) if (i === cnts[i]) return i;
+  return -1;
+};
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Example 1:
+strictEqual(findLucky([2, 2, 3, 4]), 2);
+// Explanation: The only lucky number in the array is 2 because frequency[2] == 2.
+
+// Example 2:
+strictEqual(findLucky([1, 2, 2, 3, 3, 3]), 3);
+// Explanation: 1, 2 and 3 are all lucky numbers, return the largest of them.
+
+// Example 3:
+strictEqual(findLucky([2, 2, 2, 3, 3]), -1);
+// Explanation: There are no lucky numbers in the array.
+
+// Example 4:
+strictEqual(findLucky([5]), -1);
+
+// Example 5:
+strictEqual(findLucky([7, 7, 7, 7, 7, 7, 7]), 7);
