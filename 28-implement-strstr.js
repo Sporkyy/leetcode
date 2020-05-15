a5134962a5597d1ee15d51329d9be23e5b16e86e
Todd Sayre <captain.friendly@gmail.com>
2020-05-15 11:58:38 -0400

    Better

--- 28-implement-strstr.js
+++ 28-implement-strstr.js
@@ -25,16 +25,38 @@ import { strictEqual } from 'assert';
 // Runtime: 3184 ms, faster than 5.15% of JavaScript online submissions
 // Memory Usage: 35.1 MB, less than 39.29% of JavaScript online submissions
 
+// /**
+//  * @param {string} haystack
+//  * @param {string} needle
+//  * @return {number}
+//  */
+// const strStr = (haystack, needle) => {
+//   if (0 === needle.length) return 0;
+//   if (haystack.length < needle.length) return -1;
+//   let [hI, nI] = [0, 0];
+//   while (hI < haystack.length) {
+//     if (nI === needle.length) return hI;
+//     if (haystack[hI + nI] === needle[nI]) nI++;
+//     else hI++, (nI = 0);
+//   }
+//   return -1;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 3184 ms, faster than 5.15% of JavaScript online submissions
+// Memory Usage: 35.1 MB, less than 39.29% of JavaScript online submissions
+
 /**
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
 const strStr = (haystack, needle) => {
-  if (0 === needle.length) return 0;
   if (haystack.length < needle.length) return -1;
+  if (0 === needle.length) return 0;
   let [hI, nI] = [0, 0];
-  while (hI < haystack.length) {
+  while (hI <= haystack.length - needle.length) {
     if (nI === needle.length) return hI;
     if (haystack[hI + nI] === needle[nI]) nI++;
     else hI++, (nI = 0);
