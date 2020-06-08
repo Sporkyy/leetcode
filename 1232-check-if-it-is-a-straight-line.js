fdae30373abfd076c11f11b905a1def42d94a261
Todd Sayre <captain.friendly@gmail.com>
2020-06-08 10:56:07 -0400

    Slower

--- 1232-check-if-it-is-a-straight-line.js
+++ 1232-check-if-it-is-a-straight-line.js
@@ -22,22 +22,41 @@ import { ok } from 'assert';
 // Runtime: 100 ms, faster than 5.20% of JavaScript online submissions
 // Memory Usage: 38.9 MB, less than 5.02% of JavaScript online submissions
 
+// /**
+//  * @param {number[][]} coordinates
+//  * @return {boolean}
+//  */
+// const checkStraightLine = coordinates => {
+//   if (2 === coordinates.length) return true;
+//   for (let i = 0; i < coordinates.length - 2; i++) {
+//     const [ax, ay, bx, by, cx, cy] = [
+//       coordinates[i][0],
+//       coordinates[i][1],
+//       coordinates[i + 1][0],
+//       coordinates[i + 1][1],
+//       coordinates[i + 2][0],
+//       coordinates[i + 2][1],
+//     ];
+//     if ((ax - bx) * (by - cy) !== (bx - cx) * (ay - by)) return false;
+//   }
+//   return true;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 112 ms, faster than 5.20% of JavaScript online submissions
+// Memory Usage: 38.6 MB, less than 5.02% of JavaScript online submissions
+
 /**
  * @param {number[][]} coordinates
  * @return {boolean}
  */
 const checkStraightLine = coordinates => {
-  if (2 === coordinates.length) return true;
-  for (let i = 0; i < coordinates.length - 2; i++) {
-    const [ax, ay, bx, by, cx, cy] = [
-      coordinates[i][0],
-      coordinates[i][1],
-      coordinates[i + 1][0],
-      coordinates[i + 1][1],
-      coordinates[i + 2][0],
-      coordinates[i + 2][1],
-    ];
-    if ((ax - bx) * (by - cy) !== (bx - cx) * (ay - by)) return false;
+  const [ix, iy] = [coordinates[0][0], coordinates[0][1]];
+  for (let i = 1; i < coordinates.length - 1; i++) {
+    const [ax, ay] = [coordinates[i][0], coordinates[i][1]];
+    const [bx, by] = [coordinates[i + 1][0], coordinates[i + 1][1]];
+    if ((ix - ax) * (ay - by) !== (ax - bx) * (iy - ay)) return false;
   }
   return true;
 };
@@ -97,5 +116,13 @@ ok(
     [1, 3],
     [-4, -7],
     [5, 11],
+  ]),
+);
+
+ok(
+  !checkStraightLine([
+    [1, 2],
+    [2, 3],
+    [3, 5],
   ]),
 );
