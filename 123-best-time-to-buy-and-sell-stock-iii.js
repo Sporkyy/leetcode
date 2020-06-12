a1b5faced7fc83ed6fed1c31570b121107c084c1
Todd Sayre <captain.friendly@gmail.com>
2020-06-12 09:57:52 -0400

    Will get back to it later

--- 123-best-time-to-buy-and-sell-stock-iii.js
+++ 123-best-time-to-buy-and-sell-stock-iii.js
@@ -82,53 +82,65 @@ FATAL ERROR: Committing semi space failed. Allocation failed - JavaScript heap o
 
 */
 
-/**
- * @param {number[]} prices
- * @return {number}
- */
-const leetcode121 = prices =>
-  prices.reduce(
-    ([min, profit], curr) => [
-      Math.min(min, curr),
-      Math.max(profit, curr - min),
-    ],
-    [Infinity, 0],
-  )[1];
+// /**
+//  * @param {number[]} prices
+//  * @return {number}
+//  */
+// const leetcode121 = prices =>
+//   prices.reduce(
+//     ([min, profit], curr) => [
+//       Math.min(min, curr),
+//       Math.max(profit, curr - min),
+//     ],
+//     [Infinity, 0],
+//   )[1];
 
-/**
- * @param {number[]} prices
- * @return {number}
- */
-const maxProfit = prices => {
-  // Partition the prices into two arrays
-  // const partitions = prices.map((_, idx) => [
-  //   prices.slice(0, idx),
-  //   prices.slice(idx, prices.length),
-  // ]);
-  // console.log(partitions);
-  const partitions = new Array(prices.length).fill().map(_ => [[], []]);
-  for (let i = 0; i < prices.length; i++) {
-    for (let j = 0; j < prices.length; j++) {
-      // console.log(i, j);
-      if (j < i) partitions[i][0].push(prices[j]);
-      else partitions[i][1].push(prices[j]);
-    }
-  }
-  // console.log(partitions);
-  // Get the maximum profit from each half
-  const maxes = partitions.map(([l, r]) => [leetcode121(l), leetcode121(r)]);
-  // console.log(maxes);
-  const sums = maxes.map(([l, r]) => l + r);
-  // console.log(sums);
-  return Math.max(0, ...sums);
-};
+// /**
+//  * @param {number[]} prices
+//  * @return {number}
+//  */
+// const maxProfit = prices => {
+//   // Partition the prices into two arrays
+//   // const partitions = prices.map((_, idx) => [
+//   //   prices.slice(0, idx),
+//   //   prices.slice(idx, prices.length),
+//   // ]);
+//   // console.log(partitions);
+//   const partitions = new Array(prices.length).fill().map(_ => [[], []]);
+//   for (let i = 0; i < prices.length; i++) {
+//     for (let j = 0; j < prices.length; j++) {
+//       // console.log(i, j);
+//       if (j < i) partitions[i][0].push(prices[j]);
+//       else partitions[i][1].push(prices[j]);
+//     }
+//   }
+//   // console.log(partitions);
+//   // Get the maximum profit from each half
+//   const maxes = partitions.map(([l, r]) => [leetcode121(l), leetcode121(r)]);
+//   // console.log(maxes);
+//   const sums = maxes.map(([l, r]) => l + r);
+//   // console.log(sums);
+//   return Math.max(0, ...sums);
+// };
 
 // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // Example 1:
 strictEqual(maxProfit([3, 3, 5, 0, 0, 3, 1, 4]), 6);
-// Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
-//              Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+/*
+| 3 | 3 | 5 | 0 | (0) | [3] | (1) | [4] |
+|   | 0 | 2 | 2 |  2  | {3} |  3  | {3} | = 6
+
+| 3 | 3 | 5 | 0 | 0 | 3 | 1 | 4 |
+|   | 3 | 3 | 5 | 0 | 0 | 3 | 1 |
+|   |   | 3 | 3 | 5 | 0 | 0 | 3 |
+|   |   |   | 3 | 3 | 5 | 0 | 0 |
+|   |   |   |   | 3 | 3 | 5 | 0 |
+|   |   |   |   |   | 3 | 3 | 5 |
+|   |   |   |   |   |   | 3 | 3 |
+|   |   |   |   |   |   |   | 3 |
+
+*/
 
 // Example 2:
 strictEqual(maxProfit([1, 2, 3, 4, 5]), 4);
@@ -142,23 +154,16 @@ strictEqual(maxProfit([7, 6, 4, 3, 1]), 0);
 
 strictEqual(maxProfit([6, 1, 3, 2, 4, 7]), 7);
 /*
+| 6 |  1 | 3 |  2 | 4 | 7 |
+|   | -5 | 2 | -1 | 2 | 3 |
 
 | Buy | Sell | Profit    |
 |   1 |    2 | 3 - 1 = 2 |
 |   3 |    5 | 7 - 2 = 5 |
 
-| 6 | 1 | 3 | 2 | 4 | 7 |
-| = | = | = | = | = | = |
-| 7 | 3 | 4 | 4 | 7 |   |
-|   | 2 | 7 | 7 |   |   |
-|   | 4 |   |   |   |   |
-|   | 7 |   |   |   |   |
-
-| 6 | 1 | 3 | 2 | 4 | 7 |
-| 1 |   |   |   |   |   |
-| 3 | 3 |   |   |   |   |
-| 2 | 2 | 2 |   |   |   |
-| 4 | 4 | 4 | 4 |   |   |
-| 7 | 7 | 7 | 7 | 7 |   |
+|  6 |  1 |  3 |  2 |  4 |  7 |
+| == | == | == | == | == | == |
+|  0 | -5 |  2 |  2 |  3 |  6 |
+|  6 |  6 |  5 |  5 |  3 |  0 |
 
 */
