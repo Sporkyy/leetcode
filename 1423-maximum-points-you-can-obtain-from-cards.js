fa497bfb42e0b64342f65b644f8e06c7998fab68
Todd Sayre <captain.friendly@gmail.com>
2020-07-16 12:16:58 -0400

    It works

---
+++ 1423-maximum-points-you-can-obtain-from-cards.js
@@ -0,0 +1,93 @@
+// 1423. Maximum Points You Can Obtain from Cards
+// https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+
+/*
+
+There are several cards arranged in a row, and each card has an associated
+number of points The points are given in the integer array cardPoints.
+
+In one step, you can take one card from the beginning or from the end of the
+row. You have to take exactly k cards.
+
+Your score is the sum of the points of the cards you have taken.
+
+Given the integer array cardPoints and the integer k, return the maximum score
+you can obtain.
+
+Constraints:
+- 1 <= cardPoints.length <= 10^5
+- 1 <= cardPoints[i] <= 10^4
+- 1 <= k <= cardPoints.length
+
+*/
+
+import { strictEqual } from 'assert';
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 9220 ms, faster than 5.00% of JavaScript online submissions
+// Memory Usage: 46.5 MB, less than 9.68% of JavaScript online submissions
+
+/**
+ * @param {number[]} cardPoints
+ * @param {number} k
+ * @return {number}
+ */
+const maxScore = (cardPoints, k) => {
+  const window = cardPoints.slice(0, k);
+  // console.log(window);
+  let maxSum = Math.max(
+    window.reduce((acc, curr) => acc + curr),
+    cardPoints.slice(-k).reduce((acc, curr) => acc + curr),
+  );
+  for (let i = 0; i < k; i++) {
+    window.pop();
+    window.unshift(cardPoints.pop());
+    // console.log(window);
+    maxSum = Math.max(
+      maxSum,
+      window.reduce((acc, curr) => acc + curr),
+    );
+  }
+  return maxSum;
+};
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Example 1:
+strictEqual(maxScore([1, 2, 3, 4, 5, 6, 1], 3), 12);
+// Explanation: After the first step, your score will always be However,
+// choosing the rightmost card first will maximize your total score. The optimal
+// strategy is to take the three cards on the right, giving a final score of 1 +
+// 6 + 5 = 12.
+
+// Example 2:
+strictEqual(maxScore([2, 2, 2], 2), 4);
+// Explanation: Regardless of which two cards you take, your score will always
+// be 4.
+
+// Example 3:
+strictEqual(maxScore([9, 7, 7, 9, 7, 7, 9], 7), 55);
+// Explanation: You have to take all the cards. Your score is the sum of points
+// of all cards.
+
+// Example 4:
+strictEqual(maxScore([1, 1000, 1], 1), 1);
+// Explanation: You cannot take the card in the middle. Your best score is 1.
+
+// Example 5:
+strictEqual(maxScore([1, 79, 80, 1, 1, 1, 200, 1], 3), 202);
+
+strictEqual(maxScore([1, 2, 1, 1, 1, 3], 3), 6);
+
+strictEqual(maxScore([100, 40, 17, 9, 73, 75], 3), 248);
+
+/*
+
+0,1,2,3,4,5
+
+0,1,2
+
+0,4,5
+
+*/
