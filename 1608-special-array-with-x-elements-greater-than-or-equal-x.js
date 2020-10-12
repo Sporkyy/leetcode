5b2370ae2a04268e0c87b143fdc5177687f9427c
Todd Sayre <captain.friendly@gmail.com>
2020-10-12 16:26:33 -0400

    1-line solutions

--- 1608-special-array-with-x-elements-greater-than-or-equal-x.js
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x.js
@@ -100,32 +100,199 @@ Constraints
 // Runtime: 76 ms, faster than 88.02% of JavaScript online submissions
 // Memory Usage: 38.2 MB, less than 6.45% of JavaScript online submissions
 
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// const specialArray = nums => {
+//   outer: for (let i = 1, x = 0; i <= nums.length; i++, x = 0) {
+//     for (const n of nums)
+//       if (i <= n) {
+//         x++;
+//         if (i < x) continue outer;
+//       }
+//     if (i === x) return i;
+//   }
+//   return -1;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 76 ms, faster than 88.02% of JavaScript online submissions
+// Memory Usage: 38.9 MB, less than 6.45% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// const specialArray = nums => {
+//   let min = nums.length;
+//   let max = nums.length;
+//   for (const n of nums) {
+//     if (n < min) min--;
+//     if (max < n) max++;
+//   }
+//   // console.log(min, max);
+//   outer: for (let i = min, x = 0; i <= max; i++, x = 0) {
+//     for (const n of nums)
+//       if (i <= n) {
+//         x++;
+//         if (i < x) continue outer;
+//       }
+//     if (i === x) return i;
+//   }
+//   return -1;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// const specialArray = nums => {
+//   outer: for (let i = 1, x = 0; i <= nums.length; i++, x = 0) {
+//     for (const n of nums)
+//       if (i <= n) {
+//         x++;
+//         if (i < x) continue outer;
+//       }
+//     if (i === x) return i;
+//   }
+//   return -1;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 76 ms, faster than 88.02% of JavaScript online submissions
+// Memory Usage: 38.9 MB, less than 6.45% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// const specialArray = nums => {
+//   let min = nums.length;
+//   let max = nums.length;
+//   for (const n of nums) {
+//     if (n < min) min--;
+//     if (max < n) max++;
+//   }
+//   // console.log(min, max);
+//   outer: for (let i = min, x = 0; i <= max; i++, x = 0) {
+//     for (const n of nums)
+//       if (i <= n) {
+//         x++;
+//         if (i < x) continue outer;
+//       }
+//     if (i === x) return i;
+//   }
+//   return -1;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// const specialArray = nums => {
+//   let x = nums.length;
+//   while (1 < x) {
+//     let [ltCnt, gteCnt] = [0, 0];
+//     for (const n of nums) {
+//       if (n < x) ltCnt++;
+//       else gteCnt++;
+//     }
+//     console.log(ltCnt, gteCnt, x);
+//     // if (ltCnt === x || gteCnt === x) return x;
+//     x--;
+//   }
+//   return -1;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 84 ms, faster than 55.76% of JavaScript online submissions
+// Memory Usage: 39 MB, less than 6.45% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// const specialArray = nums => {
+//   outer: for (let x = 1, cntGteX = 0; x <= nums.length; x++, cntGteX = 0) {
+//     for (const n of nums) {
+//       if (x <= n) cntGteX++;
+//       if (x < cntGteX) continue outer;
+//     }
+//     if (x === cntGteX) return x;
+//   }
+//   return -1;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 80 ms, faster than 72.35% of JavaScript online submissions
+// Memory Usage: 38.8 MB, less than 6.45% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// const specialArray = nums => {
+//   for (let x = 1, cntGteX = 0; x <= nums.length; x++, cntGteX = 0) {
+//     for (const n of nums) {
+//       if (x <= n) cntGteX++;
+//       if (x < cntGteX) break;
+//     }
+//     if (x === cntGteX) return x;
+//   }
+//   return -1;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 80 ms, faster than 72.17% of JavaScript online submissions
+// Memory Usage: 38.8 MB, less than 6.09% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// const specialArray = (nums, x = nums.length) =>
+//   0 === x
+//     ? -1
+//     : x === nums.reduce((cnt, n) => cnt + (x <= n ? 1 : 0), 0)
+//     ? x
+//     : specialArray(nums, x - 1);
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 88 ms, faster than 37.39% of JavaScript online submissions
+// Memory Usage: 39.9 MB, less than 6.09% of JavaScript online submissions
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
-const specialArray = nums => {
-  outer: for (let i = 1, x = 0; i <= nums.length; i++, x = 0) {
-    for (const n of nums)
-      if (i <= n) {
-        x++;
-        if (i < x) continue outer;
-      }
-    if (i === x) return i;
-  }
-  return -1;
-};
+const specialArray = (nums, x = nums.length) =>
+  0 === x
+    ? -1
+    : x === nums.filter(n => x <= n).length
+    ? x
+    : specialArray(nums, x - 1);
 
 // 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
 
 import { strictEqual } from 'assert';
 
 // Example 1:
-// strictEqual(specialArray([3, 5]), 2);
+strictEqual(specialArray([3, 5]), 2);
 // Explanation: There are 2 values (3 and 5) that are greater than or equal to 2.
 
 // Example 2:
-// strictEqual(specialArray([0, 0]), -1);
+strictEqual(specialArray([0, 0]), -1);
 // Explanation: No numbers fit the criteria for x.
 // If x = 0, there should be 0 numbers >= x, but there are 2.
 // If x = 1, there should be 1 number >= x, but there are 0.
@@ -133,10 +300,12 @@ import { strictEqual } from 'assert';
 // x cannot be greater since there are only 2 numbers in nums.
 
 // Example 3:
-// strictEqual(specialArray([0, 4, 3, 0, 4]), 3);
+strictEqual(specialArray([0, 4, 3, 0, 4]), 3);
 // Explanation: There are 3 values that are greater than or equal to 3.
 
-// Example 4:
-// strictEqual(specialArray([3, 6, 7, 7, 0]), -1);
+// // Example 4:
+strictEqual(specialArray([3, 6, 7, 7, 0]), -1);
 
 strictEqual(specialArray([3, 9, 7, 8, 3, 8, 6, 6]), 6);
+
+strictEqual(specialArray([0, 5, 0, 1, 8, 3, 0, 1]), 3);
