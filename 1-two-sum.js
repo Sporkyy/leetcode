c28fcf0f3904b90480dd23db8cd14c4af863a6a2
Todd Sayre <captain.friendly@gmail.com>
2020-12-11 11:06:59 -0500

    Good bad 1-line solution

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -1,7 +1,19 @@
 // 1. Two Sum
 // https://leetcode.com/problems/two-sum/
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+/*
+〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+Given an array of integers nums and an integer target, return indices of the two
+numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not
+use the same element twice.
+
+You can return the answer in any order.
+
+〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+*/
 
 // /**
 //  * @param {number[]} nums
@@ -21,12 +33,10 @@
 //   return answer;
 // };
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
 
 // Runtime: 360 ms, faster than 5.14% of JavaScript online submissions
-// for Two Sum.
 // Memory Usage: 36.1 MB, less than 13.64% of JavaScript online submissions
-// for Two Sum.
 
 // /**
 //  * @param {number[]} nums
@@ -44,12 +54,10 @@
 //       undefined,
 //     );
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
 
 // Runtime: 68 ms, faster than 60.17% of JavaScript online submissions
-// for Two Sum.
 // Memory Usage: 35.8 MB, less than 16.12% of JavaScript online submissions
-// for Two Sum.
 
 // /**
 //  * @param {number[]} nums
@@ -65,29 +73,100 @@
 //   }
 // };
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 76 ms, faster than 54.02% of JavaScript online submissions
+// Memory Usage: 40 MB, less than 6.20% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// const twoSum = (nums, target) => {
+//   const map = new Map([...nums.entries()].map(e => e.reverse()));
+//   for (const [i, n] of nums.entries())
+//     if (map.has(target - n) && map.get(target - n) !== i)
+//       return [i, map.get(target - n)];
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 76 ms, faster than 90.70% of JavaScript online submissions
+// Memory Usage: 41.1 MB, less than 28.16% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// const twoSum = (nums, target) =>
+//   nums
+//     .reduce((acc, curr, idx) => {
+//       if (acc.has(target - curr)) {
+//         acc.set('res', [idx, acc.get(target - curr)]);
+//       }
+//       acc.set(curr, idx);
+//       return acc;
+//     }, new Map())
+//     .get('res');
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 76 ms, faster than 90.70% of JavaScript online submissions
+// Memory Usage: 38.9 MB, less than 69.50% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// const twoSum = (nums, target) =>
+//   nums
+//     .reduce(
+//       (acc, curr, idx, arr) =>
+//         acc.has(target - curr)
+//           ? arr.splice(0) && acc.set('res', [idx, acc.get(target - curr)])
+//           : acc.set(curr, idx),
+//       new Map(),
+//     )
+//     .get('res');
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 76 ms, faster than 90.70% of JavaScript online submissions
+// Memory Usage: 38.8 MB, less than 69.50% of JavaScript online submissions
 
 /**
- * Runtime: 76 ms, faster than 54.02% of JavaScript online submissions
- * Memory Usage: 40 MB, less than 6.20% of JavaScript online submissions
- *
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-const twoSum = (nums, target) => {
-  const map = new Map([...nums.entries()].map(e => e.reverse()));
-  for (const [i, n] of nums.entries())
-    if (map.has(target - n) && map.get(target - n) !== i)
-      return [i, map.get(target - n)];
-};
+const twoSum = (nums, target) =>
+  nums.reduce(
+    (acc, curr, idx, arr) =>
+      acc.has(target - curr)
+        ? arr.splice(0) /* NO! BAD! */ && [idx, acc.get(target - curr)]
+        : acc.set(curr, idx),
+    new Map(),
+  );
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
 
 import { deepStrictEqual } from 'assert';
 
-deepStrictEqual(twoSum([2, 7, 11, 15], 9).sort(), [0, 1].sort());
-deepStrictEqual(twoSum([15, 11, 7, 2], 9).sort(), [2, 3].sort());
-deepStrictEqual(twoSum([2, 7], 9).sort(), [0, 1].sort());
-deepStrictEqual(twoSum([0, 1, 2, 0], 0).sort(), [0, 3].sort());
-deepStrictEqual(twoSum([3, 2, 4], 6).sort(), [1, 2].sort());
+deepStrictEqual(
+  ...[twoSum([2, 7, 11, 15], 9), [0, 1]].map(a => a.sort((a, b) => a - b)),
+);
+deepStrictEqual(
+  ...[twoSum([15, 11, 7, 2], 9), [2, 3]].map(a => a.sort((a, b) => a - b)),
+);
+deepStrictEqual(
+  ...[twoSum([2, 7], 9), [0, 1]].map(a => a.sort((a, b) => a - b)),
+);
+deepStrictEqual(
+  ...[twoSum([0, 1, 2, 0], 0), [0, 3]].map(a => a.sort((a, b) => a - b)),
+);
+deepStrictEqual(
+  ...[twoSum([3, 2, 4], 6), [1, 2]].map(a => a.sort((a, b) => a - b)),
+);
