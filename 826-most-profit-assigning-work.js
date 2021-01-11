a6aa6ec3be428a720524618379d81846f46c96e4
Todd Sayre <captain.friendly@gmail.com>
2021-01-11 15:25:36 -0500

    Better

--- 826-most-profit-assigning-work.js
+++ 826-most-profit-assigning-work.js
@@ -27,6 +27,81 @@ What is the most profit we can make?
 
 // 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
 
+// Time Limit Exceeded
+
+// /**
+//  * @param {number[]} difficulties
+//  * @param {number[]} profits
+//  * @param {number[]} workers
+//  * @return {number}
+//  */
+// const maxProfitAssignment = (difficulties, profits, workers) => {
+//   const tmp = new Map();
+//   let min = Infinity;
+//   for (let i = 0; i < difficulties.length; i++) {
+//     const [difficulty, profit] = [difficulties[i], profits[i]];
+//     tmp.set(difficulty, Math.max(profit, tmp.get(difficulty) || 0));
+//     min = Math.min(min, difficulty);
+//   }
+//   // console.log(tmp);
+//   const sorted = [...tmp].sort(([a], [b]) => a - b);
+//   // console.log(sorted);
+//   let max = -Infinity;
+//   const x = sorted.map(([difficulty, profit]) => [
+//     difficulty,
+//     (max = Math.max(max, profit)),
+//   ]);
+//   // console.log(x);
+//   const y = new Map(x);
+//   // console.log(y);
+//   let sum = 0;
+//   outer: for (const ability of workers) {
+//     // console.log(ability);
+//     if (ability < min) continue;
+//     if (y.has(ability)) sum += y.get(ability);
+//     else {
+//       for (const [difficulty, profit] of [...y].sort(([a], [b]) => b - a)) {
+//         // console.log(ability, difficulty, profit);
+//         if (difficulty < ability) {
+//           sum += profit;
+//           y.set(ability, profit);
+//           continue outer;
+//         }
+//       }
+//     }
+//   }
+//   return sum;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 4808 ms, faster than 7.14% of JavaScript online submissions
+// Memory Usage: 43.5 MB, less than 78.57% of JavaScript online submissions
+
+// /**
+//  * @param {number[]} difficulties
+//  * @param {number[]} profits
+//  * @param {number[]} workers
+//  * @return {number}
+//  */
+// const maxProfitAssignment = (difficulties, profits, workers) => {
+//   let sum = 0;
+//   for (const ability of workers) {
+//     let max = 0;
+//     for (let i = 0; i < difficulties.length; i++) {
+//       const [difficulty, profit] = [difficulties[i], profits[i]];
+//       if (difficulty <= ability) max = Math.max(max, profit);
+//     }
+//     sum += max;
+//   }
+//   return sum;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 3528 ms, faster than 7.14% of JavaScript online submissions
+// Memory Usage: 43 MB, less than 92.86% of JavaScript online submissions
+
 /**
  * @param {number[]} difficulties
  * @param {number[]} profits
@@ -34,17 +109,12 @@ What is the most profit we can make?
  * @return {number}
  */
 const maxProfitAssignment = (difficulties, profits, workers) => {
-  const tmp = new Map();
-  for (let i = 0; i < difficulties.length; i++) {
-    const [difficulty, profit] = [difficulties[i], profits[i]];
-    tmp.set(difficulty, Math.max(profit, tmp.get(difficulty) || 0));
-  }
-  console.log(tmp);
-  let max = 0;
-  for (const [difficulty, profit] of [...tmp].sort(([a], [b]) => a - b)) {
-    tmp.set(difficulty, (max = Math.max(max, profit)));
-  }
-  console.log(tmp);
+  const wages = new Array(workers.length).fill(0);
+  for (let i = 0; i < difficulties.length; i++)
+    for (let j = 0; j < workers.length; j++)
+      if (difficulties[i] <= workers[j] && wages[j] < profits[i])
+        wages[j] = profits[i];
+  return wages.reduce((acc, curr) => acc + curr);
 };
 
 // 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
@@ -58,3 +128,19 @@ strictEqual(
 );
 // Explanation: Workers are assigned jobs of difficulty [4,4,6,6] and they get
 // profit of [20,20,30,30] separately.
+
+strictEqual(
+  maxProfitAssignment(
+    [68, 35, 52, 47, 86],
+    [67, 17, 1, 81, 3],
+    [92, 10, 85, 84, 82],
+  ),
+  324,
+);
+
+/*
+
+| 35 | 47 | 52 | 68 | 86 |
+| 17 | 81 |  1 | 67 |  3 |
+
+*/
