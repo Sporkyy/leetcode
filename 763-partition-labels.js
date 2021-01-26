c84852cadf10dec492bcf2e5b36f2a5a8e14203c
Todd Sayre <captain.friendly@gmail.com>
2021-01-26 14:17:36 -0500

    Optimized versions slower than unoptimized

--- 763-partition-labels.js
+++ 763-partition-labels.js
@@ -66,36 +66,164 @@ part, and return a list of integers representing the size of these parts.
 
 // 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
 
+// Runtime: 112 ms, faster than 17.41% of JavaScript online submissions
+// Memory Usage: 43.5 MB, less than 6.99% of JavaScript online submissions
+
+// /**
+//  * @param {string} s
+//  * @return {number[]}
+//  */
+// const partitionLabels = s => {
+//   let indCnts = new Array(26).fill().map(() => [-1, -1, 0]);
+//   for (let i = 0; i < s.length; i++) {
+//     const code = s.charCodeAt(i) - 97;
+//     if (-1 === indCnts[code][0]) indCnts[code][0] = i;
+//     indCnts[code][1] = i;
+//     indCnts[code][2]++;
+//   }
+//   indCnts.sort(([a, c], [b, d]) => a - b || c - d);
+//   // console.log(indices);
+//   let [partMin, partMax, partCnt] = [Infinity, -Infinity, 0];
+//   // for (let i = 0; i < indices.length; i++) {
+//   //   console.log(indices[i]);
+//   //   min = Math.min(min, indices[i][0]); // ?
+//   //   max = Math.max(max, indices[i][1]); // ?
+//   // }
+//   const res = [];
+//   for (const [charMin, charMax, charCnt] of indCnts) {
+//     // console.log([charMin, charMax, charCnt]);
+//     if (partMax < charMin) {
+//       // console.log(`here ${partCnt}`);
+//       res.push(partCnt);
+//       partCnt = 0;
+//     }
+//     partMin = Math.min(partMin, charMin);
+//     partMax = Math.max(partMax, charMax);
+//     partCnt += charCnt;
+//   }
+//   res.push(partCnt);
+//   console.log(res);
+//   while (0 === res[0]) res.shift();
+//   return res;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 168 ms, faster than 5.15% of JavaScript online submissions
+// Memory Usage: 48.2 MB, less than 5.16% of JavaScript online submissions
+
+// /**
+//  * @param {string} s
+//  * @return {number[]}
+//  */
+// const partitionLabels = s => {
+//   const cMap = new Map();
+//   for (let i = 0; i < s.length; i++) {
+//     const [minIdx, , cnt] = cMap.get(s[i]) || [Infinity, , 0];
+//     cMap.set(s[i], [Math.min(i, minIdx), i, cnt + 1]);
+//   }
+//   let [partMin, partMax] = [Infinity, -Infinity];
+//   const res = [];
+//   for (const [charMin, charMax, charCnt] of cMap.values()) {
+//     if (partMax < charMin) res.push(0);
+//     partMin = Math.min(partMin, charMin);
+//     partMax = Math.max(partMax, charMax);
+//     res[res.length - 1] += charCnt;
+//   }
+//   return res;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 152 ms, faster than 5.15% of JavaScript online submissions
+// Memory Usage: 45.9 MB, less than 5.16% of JavaScript online submissions
+
+// /**
+//  * @param {string} s
+//  * @return {number[]}
+//  */
+// const partitionLabels = s => {
+//   const cMap = new Map();
+//   for (const [idx, char] of [...s].entries()) {
+//     const [minIdx, , cnt] = cMap.get(char) || [Infinity, , 0];
+//     cMap.set(char, [Math.min(idx, minIdx), idx, cnt + 1]);
+//   }
+//   let [partMin, partMax] = [Infinity, -Infinity];
+//   const res = [];
+//   for (const [charMin, charMax, charCnt] of cMap.values()) {
+//     if (partMax < charMin) res.push(0);
+//     partMin = Math.min(partMin, charMin);
+//     partMax = Math.max(partMax, charMax);
+//     res[res.length - 1] += charCnt;
+//   }
+//   return res;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 104 ms, faster than 21.99% of JavaScript online submissions
+// Memory Usage: 41.7 MB, less than 19.36% of JavaScript online submissions
+
+// /**
+//  * @param {string} s
+//  * @return {number[]}
+//  */
+// const partitionLabels = s => {
+//   const indices = new Array(26);
+//   for (let i = 0; i < s.length; i++) {
+//     const [fi] = indices[s.charCodeAt(i) - 97] || [i];
+//     indices[s.charCodeAt(i) - 97] = [fi, i];
+//   }
+//   indices.sort(([a, c], [b, d]) => a - b || c - d);
+//   let [partMin, partMax] = [0, 0];
+//   const res = [];
+//   for (let i = 0; i < indices.length; i++) {
+//     if (!indices[i]) break;
+//     const [charMin, charMax] = indices[i];
+//     if (partMax < charMin) {
+//       res.push(partMax - partMin + 1);
+//       partMin = Infinity;
+//     }
+//     partMax = Math.max(partMax, charMax);
+//     partMin = Math.min(partMin, charMin);
+//     if (charMax === s.length - 1) {
+//       res.push(s.length - partMin);
+//       break;
+//     }
+//   }
+//   return res;
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 144 ms, faster than 5.84% of JavaScript online submissions
+// Memory Usage: 42 MB, less than 13.97% of JavaScript online submissions
+
 /**
  * @param {string} s
  * @return {number[]}
  */
 const partitionLabels = s => {
-  let indices = new Array(26).fill().map(() => [-1, -1]);
+  const liMap = new Map();
+  const cnts = new Array(26).fill(0);
   for (let i = 0; i < s.length; i++) {
-    const code = s.charCodeAt(i) - 97;
-    if (-1 === indices[code][0]) indices[code][0] = i;
-    indices[code][1] = i;
+    liMap.set(s[i], i);
+    cnts[s.charCodeAt(i) - 97]++;
   }
-  console.log(indices);
+  let [pMin, pMax] = [Infinity, -Infinity];
   const res = [];
-  const stk = [];
-  let [l, r] = [Infinity, -Infinity];
-  while (indices.length) {
-    const [sl, sr] = indices.pop();
-    if (-1 === sl) continue;
-    console.log(l, r);
-    // console.log(sl, sr);
-    if (l < sl && sr < r) continue;
-    stk.push([sl, sr]);
-    if (!indices.length && stk.length) {
-      // console.log(l, r);
-      indices = stk;
-    }
+  for (let i = 0; i < s.length && liMap.size; i++) {
+    if (!liMap.has(s[i])) continue;
+    const [cMin, cMax] = [i, liMap.get(s[i])];
+    if (pMax < cMin) res.push(0);
+    liMap.delete(s[i]);
+    res[res.length - 1] += cnts[s.charCodeAt(i) - 97];
+    [pMin, pMax] = [Math.min(pMin, cMin), Math.max(pMax, cMax)];
   }
+  return res;
 };
 
-// // 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
 
 import { deepStrictEqual } from 'assert';
 
@@ -125,3 +253,10 @@ deepStrictEqual(partitionLabels('eaaaabaaec'), [9, 1]);
 // |0|1|2|3|4|5|6|7|8|9|
 // |e|a|a|a|a|b|a|a|e|c|
 // |8|7|7|7|7|5|7|7|8|9|
+
+deepStrictEqual(
+  partitionLabels(
+    'ntswuqqbidunnixxpoxxuuupotaatwdainsotwvpxpsdvdbwvbtdiptwtxnnbtqbdvnbowqitudutpsxsbbsvtipibqpvpnivottsxvoqqaqdxiviidivndvdtbvadnxboiqivpusuxaaqnqaobutdbpiosuitdnopoboivopaapadvqwwnnwvxndpxbapixaspwxxxvppoptqxitsvaaawxwaxtbxuixsoxoqdtopqqivaitnpvutzchkygjjgjkcfzjzrkmyerhgkglcyffezmehjcllmlrjghhfkfylkgyhyjfmljkzglkklykrjgrmzjyeyzrrkymccefggczrjflykclfhrjjckjlmglrmgfzlkkhffkjrkyfhegyykrzgjzcgjhkzzmzyejycfrkkekmhzjgggrmchkeclljlyhjkchmhjlehhejjyccyegzrcrerfzczfelzrlfylzleefgefgmzzlggmejjjygehmrczmkrc',
+  ),
+  [246, 254],
+);
