5a84ee49e4d7bafce98a6df0182d99927a17fec9
Todd Sayre <captain.friendly@gmail.com>
2021-03-23 13:14:25 -0400

    Most simple solution so far

--- 1790-check-if-one-string-swap-can-make-strings-equal.js
+++ 1790-check-if-one-string-swap-can-make-strings-equal.js
@@ -73,20 +73,133 @@ one string swap on exactly one of the strings. Otherwise, return false.
 // Runtime: 80 ms, faster than 100.00% of JavaScript online submissions
 // Memory Usage: 39.6 MB, less than 100.00% of JavaScript online submissions
 
+// /**
+//  * @param {string} s1
+//  * @param {string} s2
+//  * @return {boolean}
+//  */
+// const areAlmostEqual = (s1, s2) => {
+//   const [x, y] = [new Array(26).fill(0), new Array(26).fill(0)];
+//   for (let i = 0, j = 0; i < s1.length && i < s2.length; i++) {
+//     if (s1[i] !== s2[i]) j++;
+//     if (2 < j) return false;
+//     x[s1.charCodeAt(i) - 97]++;
+//     y[s2.charCodeAt(i) - 97]++;
+//   }
+//   return x.join('') === y.join('');
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 84 ms, faster than 100.00% of JavaScript online submissions
+// Memory Usage: 39.7 MB, less than 100.00% of JavaScript online submissions
+
+// /**
+//  * @param {string} s1
+//  * @param {string} s2
+//  * @return {boolean}
+//  */
+// const areAlmostEqual = (s1, s2) => {
+//   const cnt = new Array(26).fill(0);
+//   let sum = 0;
+//   for (let i = 0, cntDiff = 0; i < s1.length; i++) {
+//     if (s1[i] !== s2[i]) cntDiff++;
+//     if (2 < cntDiff) return false;
+//     const [c1, c2] = [s1.charCodeAt(i) - 97, s2.charCodeAt(i) - 97];
+//     cnt[c1]++;
+//     cnt[c2]--;
+//     sum += c1 - c2;
+//   }
+//   if (sum) return false;
+//   return cnt.every(c => !c);
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// /**
+//  * @param {string} s1
+//  * @param {string} s2
+//  * @return {boolean}
+//  */
+// const areAlmostEqual = (s1, s2) => {
+//   const cnt = new Array(26).fill(0);
+//   let sum = 0;
+//   for (let i = 0, cntDiff = 0; i < s1.length; i++) {
+//     if (s1[i] !== s2[i]) cntDiff++;
+//     if (2 < cntDiff) return false;
+//     const [c1, c2] = [s1.charCodeAt(i) - 97, s2.charCodeAt(i) - 97];
+//     cnt[c1]++;
+//     cnt[c2]--;
+//     sum += c1 - c2;
+//   }
+//   return !sum || cnt.every(c => !c);
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 96 ms, faster than 100.00% of JavaScript online submissions
+// Memory Usage: 39.4 MB, less than 100.00% of JavaScript online submissions
+
+// /**
+//  * @param {string} s1
+//  * @param {string} s2
+//  * @return {boolean}
+//  */
+// const areAlmostEqual = (s1, s2) => {
+//   const cnt = new Array(26).fill(0);
+//   const cnt2 = new Array(s1.length).fill(0);
+//   for (let i = 0, cntDiff = 0; i < s1.length; i++) {
+//     cnt[s1.charCodeAt(i) - 97]++;
+//     cnt[s2.charCodeAt(i) - 97]--;
+//     cnt2[i] += s1.charCodeAt(i);
+//     cnt2[i] -= s2.charCodeAt(i);
+//   }
+//   return cnt2.reduce((acc, curr) => acc + !!curr, 0) < 3 && cnt.every(c => !c);
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 76 ms, faster than 100.00% of JavaScript online submissions
+// Memory Usage: 38.5 MB, less than 100.00% of JavaScript online submissions
+
+// /**
+//  * @param {string} s1
+//  * @param {string} s2
+//  * @return {boolean}
+//  */
+// const areAlmostEqual = (s1, s2) => {
+//   const cntChars = new Array(26).fill(0);
+//   for (let i = 0, cntDiff = 0; i < s1.length; i++) {
+//     if (s1[i] !== s2[i]) {
+//       cntChars[s1.charCodeAt(i) - 97]++;
+//       cntChars[s2.charCodeAt(i) - 97]--;
+//       cntDiff++;
+//     }
+//     if (2 < cntDiff) return false;
+//   }
+//   return cntChars.every(c => !c);
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 80 ms, faster than 100.00% of JavaScript online submissions
+// Memory Usage: 39 MB, less than 100.00% of JavaScript online submissions
+
 /**
  * @param {string} s1
  * @param {string} s2
  * @return {boolean}
  */
 const areAlmostEqual = (s1, s2) => {
-  const [x, y] = [new Array(26).fill(0), new Array(26).fill(0)];
-  for (let i = 0, j = 0; i < s1.length && i < s2.length; i++) {
-    if (s1[i] !== s2[i]) j++;
-    if (2 < j) return false;
-    x[s1.charCodeAt(i) - 97]++;
-    y[s2.charCodeAt(i) - 97]++;
+  const [a1, a2] = [[], []];
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i] !== s2[i]) {
+      a1.push(s1[i]);
+      a2.push(s2[i]);
+    }
+    if (2 < a1.length) return false;
   }
-  return x.join('') === y.join('');
+  return a1[0] === a2[1] && a1[1] === a2[0];
 };
 
 // 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
@@ -113,3 +226,5 @@ ok(!areAlmostEqual('abcd', 'dcba'));
 ok(!areAlmostEqual('aa', 'ac'));
 
 ok(!areAlmostEqual('caa', 'aaz'));
+
+ok(!areAlmostEqual('bf', 'dd'));
