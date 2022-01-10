9a31e047d156abe155e676bd3ac083cea452ba7e
Todd Sayre <captain.friendly@gmail.com>
2022-01-10 17:07:42 -0500

    1260 | Better code

--- 1260-shift-2d-grid.js
+++ 1260-shift-2d-grid.js
@@ -110,6 +110,35 @@ import { deepStrictEqual } from 'assert';
 // Runtime: 84 ms, faster than 97.03% of JavaScript online submissions
 // Memory Usage: 42 MB, less than 100.00% of JavaScript online submissions
 
+// /**
+//  * @param {number[][]} grid
+//  * @param {number} k
+//  * @return {number[][]}
+//  */
+// const shiftGrid = (grid, k) => {
+//   // Store the width and the height of the grid
+//   const [w, h] = [grid[0].length, grid.length];
+//   // Calculate the total items in the grid
+//   const t = w * h;
+//   // Use the total to avoid extraneous whole-grid shifts
+//   k %= t;
+//   // Flatten the grid for easy shifting
+//   const flattened = grid.reduce((acc, curr) => acc.concat(curr), []);
+//   // Shift the grid using slice for speed
+//   const sFlattened = [...flattened.slice(-k), ...flattened.slice(0, -k)];
+//   // Make a new two-dimensional shifted grid
+//   const shifted = []; // The second dimension is added on the next line
+//   // Store the flat shifted grid in the new two-dimensional shifted grid
+//   for (let i = 0; i < t; i += w) shifted.push(sFlattened.slice(i, i + w));
+//   // Return the new two-dimensional shifted grid
+//   return shifted;
+// };
+
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+// Runtime: 92 ms, faster than 100.00% of JavaScript online submissions
+// Memory Usage: 45.3 MB, less than 35.62% of JavaScript online submissions
+
 /**
  * @param {number[][]} grid
  * @param {number} k
@@ -123,7 +152,7 @@ const shiftGrid = (grid, k) => {
   // Use the total to avoid extraneous whole-grid shifts
   k %= t;
   // Flatten the grid for easy shifting
-  const flattened = grid.reduce((acc, curr) => acc.concat(curr), []);
+  const flattened = grid.flat();
   // Shift the grid using slice for speed
   const sFlattened = [...flattened.slice(-k), ...flattened.slice(0, -k)];
   // Make a new two-dimensional shifted grid
