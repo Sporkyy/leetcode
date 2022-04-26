a2f74757f298e43b8d0e0f767afc0b1d0451044a
Todd Sayre <captain.friendly@gmail.com>
2022-04-26 16:59:42 -0400

    Solution works with 2 now

--- 1304-find-n-unique-integers-sum-up-to-zero.js
+++ 1304-find-n-unique-integers-sum-up-to-zero.js
@@ -8,9 +8,9 @@ add up to 0.
 
 */
 
-import { strictEqual } from 'assert';
+import { ok, strictEqual } from 'assert';
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
 
 // Runtime: 60 ms, faster than 69.72% of JavaScript online submissions
 // Memory Usage: 35.1 MB, less than 100.00% of JavaScript online submissions
@@ -26,7 +26,7 @@ import { strictEqual } from 'assert';
 //   return res;
 // };
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
 
 // Runtime: 64 ms, faster than 44.12% of JavaScript online submissions
 // Memory Usage: 35.6 MB, less than 100.00% of JavaScript online submissions
@@ -38,37 +38,88 @@ import { strictEqual } from 'assert';
 // const sumZero = n =>
 //   [...new Array(n - 1).keys()].concat([0 - ((n - 1) * (n - 2)) / 2]);
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
 
 // Runtime: 60 ms, faster than 69.72% of JavaScript online submissions
 // Memory Usage: 35.7 MB, less than 100.00% of JavaScript online submissions
+
+// /**
+//  * @param {number} n
+//  * @return {number[]}
+//  */
+// const sumZero = n => [...new Array(n - 1).keys(), 0 - ((n - 1) * (n - 2)) / 2];
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// /**
+//  * @param {number} n
+//  * @return {number[]}
+//  */
+// const sumZero = n => {
+//   // const x = [...new Array(n - 1).keys()]; //?
+//   const x = [...new Array(n).keys()].slice(1); //?
+//   // const y = 0 - ((n - 1) * (n - 2)) / 2; //?
+//   const y = 0 - ((n - 1) * n) / 2; //?
+//   return [...x, y];
+// };
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 62 ms, faster than 93.42% of JavaScript online submissions
+// Memory Usage: 44.5 MB, less than 5.61% of JavaScript online submissions
+
+// /**
+//  * @param {number} n
+//  * @return {number[]}
+//  */
+// const sumZero = n => [...new Array(n).keys(), ((-n + 1) * n) / 2].slice(1);
+
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+// Runtime: 74 ms, faster than 69.44% of JavaScript online submissions
+// Memory Usage: 44.3 MB, less than 5.61% of JavaScript online submissions
 
 /**
  * @param {number} n
  * @return {number[]}
  */
-const sumZero = n => [...new Array(n - 1).keys(), 0 - ((n - 1) * (n - 2)) / 2];
+const sumZero = n => [...new Array(n).keys(), ((1 - n) * n) / 2].slice(1);
 
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// 〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰
+
+const areUnique = arr => arr.length === new Set(arr).size;
+const sum = arr => arr.reduce((acc, curr) => acc + curr, 0);
+
+// Example 3:
+(arr => {
+  ok(areUnique(arr));
+  strictEqual(sum(arr), 0);
+})(sumZero(1));
+// Example: [0]
+
+// Someone in the comments mentioned "2" didn't work with my solution:
+(arr => {
+  ok(areUnique(arr));
+  strictEqual(sum(arr), 0);
+})(sumZero(2));
+
+// Example 2:
+(arr => {
+  ok(areUnique(arr));
+  strictEqual(sum(arr), 0);
+})(sumZero(3));
+// Example: [-1,0,1]
 
 // Example 1:
-strictEqual(
-  sumZero(5).reduce((acc, curr) => acc + curr),
-  0,
-);
+(arr => {
+  ok(areUnique(arr));
+  strictEqual(sum(arr), 0);
+})(sumZero(5));
 // Example: [-3,-1,2,-2,4]
 // Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
 
-// Example 2:
-strictEqual(
-  sumZero(3).reduce((acc, curr) => acc + curr),
-  0,
-);
-// Example: [-1,0,1]
-
-// Example 3:
-strictEqual(
-  sumZero(1).reduce((acc, curr) => acc + curr),
-  0,
-);
-// Example: [0]
+// Just a bigger example:
+(arr => {
+  ok(areUnique(arr));
+  strictEqual(sum(arr), 0);
+})(sumZero(8));
