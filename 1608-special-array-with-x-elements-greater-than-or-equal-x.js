538e6ddb88d50a05562061a86bf28e7b003f3f09
Todd Sayre <captain.friendly@gmail.com>
2022-08-01 12:33:13 -0400

    Update 1608-special-array-with-x-elements-greater-than-or-equal-x.js

--- 1608-special-array-with-x-elements-greater-than-or-equal-x.js
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x.js
@@ -3,14 +3,14 @@
 
 /*
 
-You are given an array nums of non-negative integers. nums is considered special
-if there exists a number x such that there are exactly x numbers in nums that
-are greater than or equal to x.
+You are given an array nums of non-negative integers. nums is considered
+special if there exists a number x such that there are exactly x numbers in
+nums that are greater than or equal to x.
 
 Notice that x does not have to be an element in nums.
 
-Return x if the array is special, otherwise, return -1. It can be proven that if
-nums is special, the value for x is unique.
+Return x if the array is special, otherwise, return -1. It can be proven that
+if nums is special, the value for x is unique.
 
 Constraints
 - 1 <= nums.length <= 100
